fbece341c50fd80eee3b1d9d3a22e85d1ea92641
Harmon <Harmon758@gmail.com>
2016-05-07 02:00:40 -0500

    v0-0.10
    Former-commit-id: 9ca0a473e0187e71ec283958681e18a71bb7205a

---
+++ modules/documentation.py
@@ -0,0 +1,77 @@
+
+commands = "Commands: !8ball (!eightball) !add !audiodefine !avatar !calc !cat !chess !choose !coin !color (!colour) !commands !date\
+ !define !fancify !google (!search) !guess !haveibeenpwned !imdb !imfeelinglucky !insult !joke !lmbtfy !lmgtfy !longurl !map !math !maze !mycolor (!mycolour) !number !randomidea\
+ !randomlocation !randomword !rng !rolecolor (!rolecolour) !shorturl !spotifyinfo !spotifytoyoutube (!sptoyt) !steam !streetview !trivia !urbandictionary !wiki !xkcd !year\
+ !youtubeinfo !youtubesearch\n\
+Conversions: temperature unit conversions (![c, f, k, r, de]to[c, f, k, r, de, n, re, ro]), weight unit conversions (![amu, me, bagc, bagpc, barge, kt, ct, \
+clove, crith, da, drt, drav, ev, gamma, gr, gv, longcwt, cwt, shcwt, kg, kip, mark, mite, mitem, ozt, ozav, oz, dwt, pwt, point, lb, lbav, lbm, lbt, \
+quarterimp, quarterinf, quarterlinf, q, sap, sheet, slug, st, atl, ats, longtn, ton, shtn, t, wey, g]to[amu, me, bagc, bagpc, barge, kt, ct, clove, crith, \
+da, drt, drav, ev, gamma, gr, gv, longcwt, cwt, shcwt, kg, kip, mark, mite, mitem, ozt, ozav, oz, dwt, pwt, point, lb, lbav, lbm, lbt, quarterimp, \
+quarterinf, quarterlinf, q, sap, sheet, slug, st, atl, ats, longtn, ton, shtn, t, wey, g])\n\
+In Progress: !adventure !everyone !gifvtogif !giphy !gofish !help (!whatis) !radio !tempchannel !timer !youtube (!audio !soundcloud !yt) !war !webmtogif\n\
+Misc/Admin Only: !channel !delete !discriminator !eval !idtoname !libraryversion !load !nametoid !owner !servericon !serverowner !test !tts !updateavatar\n\
+@Harmon758 (!cleverbot) (!talk) (!ask), f (F)\n\
+Use !help or !whatis *command* for more information on a command"
+
+commands_info = {
+	"!8ball" : "Ask !8ball a yes or no question", "!eightball" : "Ask !eightball a yes or no question",
+	"!add" : "Use !add to add numbers",
+	"!adventure" : "!adventure",
+	"!audiodefine" : "Use !audiodefine to find out how to pronounce a word",
+	"!avatar" : "Use !avatar to see someone's avatar",
+	"!calc" : "!calc",
+	"!cat" : "Use !cat for a random image of a cat. !cat categories for different categories you can choose from. !cat <category> for a random image of a cat from that category",
+	"!channel" : "!channel",
+	"!chess" : "!chess",
+	"!choose" : "Use !choose and I will choose between multiple options (e.g. !choose <option1> <option2> <...>)",
+	"!cleverbot" : "!cleverbot", "!talk" : "!talk", "!ask" : "!ask",
+	"!coin" : "Use !coin to flip a coin",
+	"!color" : "!color", "!colour" : "!colour",
+	"!commands" : "!commands",
+	"!date" : "!date",
+	"!define" : "!define",
+	"!delete" : "!delete",
+	"!everyone" : "Use !everyone to see if you can mention everyone in that channel",
+	"!gofish" : "!gofish",
+	"!google" : "!google", "!search" : "!search",
+	"!guess" : "!guess",
+	"!haveibeenpwned" : "!haveibeenpwned",
+	"!help" : "!help", "!whatis" : "!whatis",
+	"!idtoname" : "!idtoname",
+	"!imdb" : "!imdb",
+	"!imfeelinglucky" : "!imfeelinglucky",
+	"!insult" : "!insult",
+	"!joke" : "!joke",
+	"!lmgtfy" : "!lmgtfy",
+	"!longurl" : "!longurl",
+	"!map" : "!map",
+	"!math" : "!math",
+	"!mycolor" : "!mycolor", "!mycolour" : "!mycolour",
+	"!nametoid" : "!nametoid",
+	"!number" : "!number",
+	"!owner" : "Use !owner to find out the owner of the server",
+	"!randomidea" : "!randomidea",
+	"!randomlocation" : "!randomlocation",
+	"!randomword" : "!randomword",
+	"!rng" : "!rng",
+	"!rolecolor" : "!rolecolor", "!rolecolour" : "!rolecolour",
+	"!servericon" : "!servericon",
+	"!shorturl" : "!shorturl",
+	"!spotifyinfo" : "!spotifyinfo",
+	"!spotifytoyoutube" : "!spotifytoyoutube", "!sptoyt" : "!sptoyt",
+	"!steam" : "!steam",
+	"!streetview" : "!streetview",
+	"!tempchannel" : "!tempchannel",
+	"!test" : "!test",
+	"!timer" : "!timer",
+	"!trivia" : "!trivia",
+	"!tts" : "!tts",
+	"!war" : "!war",
+	"!wiki" : "Use !wiki to look something up on Wikipedia",
+	"!xkcd" : "!xkcd",
+	"!year" : "!year",
+	"!youtube" : "!youtube",
+	"!youtubeinfo" : "!youtubeinfo",
+	"!youtubesearch" : "!youtubesearch",
+}
+
