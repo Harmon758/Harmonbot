9b077073ee9cf48b22320546ac50b79871843751
Harmon <Harmon758@gmail.com>
2016-06-10 08:07:13 -0500

    Added 8ball emote response

--- cogs/games.py
+++ cogs/games.py
@@ -131,7 +131,7 @@ class Games:
 		responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely",
 			"Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predit now",
 			"Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-		await client.reply(random.choice(responses))
+		await client.reply(":8ball: {}".format(random.choice(responses)))
 	
 	@commands.group(hidden = True, pass_context = True)
 	async def gofish(self, ctx): #WIP
