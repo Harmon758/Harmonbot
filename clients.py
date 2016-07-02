479178446d65972d2e7c69209619ba51ca7eaf23
Harmon758 <Harmon758@gmail.com>
2016-07-01 22:44:25 -0500

    Made commands case insensitive
    (overrode process_commands)
    Also updated overriden reply

--- clients.py
+++ clients.py
@@ -1,5 +1,9 @@
 
+import discord
 from discord.ext import commands
+from discord.ext.commands.view import StringView
+from discord.ext.commands.context import Context
+from discord.ext.commands.errors import CommandNotFound, CommandError
 import aiohttp
 import cleverbot
 import datetime
@@ -8,7 +12,7 @@ import json
 import os
 from utilities.help_formatter import CustomHelpFormatter
 
-version = "0.30.8"
+version = "0.30.9"
 wait_time = 15.0
 code_block = "```\n{}\n```"
 py_code_block = "```py\n{}\n```"
@@ -23,8 +27,43 @@ class Bot(commands.Bot):
 	def reply(self, content, *args, **kwargs):
 		author = commands.bot._get_variable('_internal_author')
 		destination = commands.bot._get_variable('_internal_channel')
-		fmt = '{0.mention}: {1}'.format(author, str(content))
-		return self.send_message(destination, fmt, *args, **kwargs)
+		fmt = '{0.mention}: {1}'.format(author, str(content)) # , -> :
+		extensions = ('delete_after',)
+		params = {k: kwargs.pop(k, None) for k in extensions}
+		coro = self.send_message(destination, fmt, *args, **kwargs)
+		return self._augmented_msg(coro, **params)
+	
+	async def process_commands(self, message):
+		_internal_channel = message.channel
+		_internal_author = message.author
+		view = StringView(message.content)
+		if self._skip_check(message.author, self.user):
+			return
+		prefix = self._get_prefix(message)
+		invoked_prefix = prefix
+		if not isinstance(prefix, (tuple, list)):
+			if not view.skip_string(prefix):
+				return
+		else:
+			invoked_prefix = discord.utils.find(view.skip_string, prefix)
+			if invoked_prefix is None:
+				return
+		invoker = view.get_word().lower() # case insensitive commands
+		tmp = {'bot': self, 'invoked_with': invoker, 'message': message, 'view': view, 'prefix': invoked_prefix}
+		ctx = Context(**tmp)
+		del tmp
+		if invoker in self.commands:
+			command = self.commands[invoker]
+			self.dispatch('command', command, ctx)
+			try:
+				await command.invoke(ctx)
+			except CommandError as e:
+				ctx.command.dispatch_error(e, ctx)
+			else:
+				self.dispatch('command_completion', command, ctx)
+		elif invoker:
+			exc = CommandNotFound('Command "{}" is not found'.format(invoker))
+			self.dispatch('command_error', exc, ctx)
 
 try:
 	with open("data/prefixes.json", "x") as prefixes_file:
