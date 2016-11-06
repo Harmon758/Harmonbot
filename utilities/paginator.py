135f709e9a244c1397378e8e082ba29c59008a93
Harmon <Harmon758@gmail.com>
2016-11-06 08:02:50 -0600

    Implemented custom paginator with seperator option

---
+++ utilities/paginator.py
@@ -0,0 +1,29 @@
+
+from discord.ext.commands.formatter import Paginator
+
+class CustomPaginator(Paginator):
+	
+	def __init__(self, seperator = "\n", prefix='```', suffix='```', max_size=2000):
+		super().__init__(prefix, suffix, max_size)
+		self.seperator = seperator
+		self._current_page = []
+
+	def add_section(self, section='', *, empty=False):
+		if len(section) > self.max_size - len(self.prefix) - 2:
+			raise RuntimeError('Section exceeds maximum page size %s' % (self.max_size - len(self.prefix) - 2))
+
+		if self._count + len(section) + len(self.seperator) > self.max_size:
+			self.close_page()
+
+		self._count += len(section) + len(self.seperator)
+		self._current_page.append(section)
+
+		if empty:
+			self._current_page.append('')
+			self._count += len(self.seperator)
+
+	def close_page(self):
+		self._pages.append(self.prefix + "\n" + self.seperator.join(self._current_page) + "\n" + self.suffix)
+		self._current_page = []
+		self._count = len(self.prefix) + len(self.seperator)
+
