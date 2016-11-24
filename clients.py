5199061af52b174b7268982606341e7d5f859a47
Harmon <Harmon758@gmail.com>
2016-11-24 01:35:19 -0600

    Updated help command to accept lowercase cog names
    Given that cog name is not also name of a command

--- clients.py
+++ clients.py
@@ -17,7 +17,7 @@ from utilities.help_formatter import CustomHelpFormatter
 from modules import utilities
 import credentials
 
-version = "0.34.23.24"
+version = "0.34.23.25"
 changelog = "https://discord.gg/a2rbZPu"
 wait_time = 15.0
 code_block = "```\n{}\n```"
