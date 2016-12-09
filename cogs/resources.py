6644163e95abd8f8562c070b230955da77088b9a
Harmon <Harmon758@gmail.com>
2016-12-09 06:16:37 -0600

    Improved utility function usage

--- cogs/resources.py
+++ cogs/resources.py
@@ -78,14 +78,14 @@ class Resources:
 					data = await resp.text()
 				root = xml.etree.ElementTree.fromstring(data)
 				if root.find(".//url") is not None:
-					await self.bot.embed_reply_image(root.find(".//url").text)
+					await self.bot.embed_reply(None, image_url = root.find(".//url").text)
 				else:
 					await self.bot.embed_reply(":no_entry: Error: Category not found")
 		else:
 			async with aiohttp_session.get("http://thecatapi.com/api/images/get?format=xml&results_per_page=1") as resp:
 				data = await resp.text()
 			root = xml.etree.ElementTree.fromstring(data)
-			await self.bot.embed_reply_image(root.find(".//url").text)
+			await self.bot.embed_reply(None, image_url = root.find(".//url").text)
 	
 	@commands.command(aliases = ["colour"])
 	@checks.not_forbidden()
@@ -696,7 +696,7 @@ class Resources:
 	async def streetview(self, *, location : str):
 		'''Generate street view of a location'''
 		image_url = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location={0}".format(location.replace(' ', '+'))
-		await self.bot.embed_reply_image(image_url)
+		await self.bot.embed_reply(None, image_url = image_url)
 	
 	@streetview.command(name = "random")
 	@checks.not_forbidden()
@@ -705,7 +705,7 @@ class Resources:
 		latitude = random.uniform(-90, 90)
 		longitude = random.uniform(-180, 180)
 		image_url = "https://maps.googleapis.com/maps/api/streetview?size=400x400&location={0},{1}".format(latitude, longitude)
-		await self.bot.embed_reply_image(image_url)
+		await self.bot.embed_reply(None, image_url = image_url)
 	
 	@commands.command()
 	@checks.not_forbidden()
