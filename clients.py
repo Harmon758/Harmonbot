b99a4b798fb8ebab9a743dabd3a0e3ee4e2f4c22
Harmon <Harmon758@gmail.com>
2017-02-01 03:40:11 -0600

    Updated version command
    Also unhid unnecessarily hidden owner only commands

--- clients.py
+++ clients.py
@@ -20,7 +20,7 @@ from modules import utilities
 from utilities.help_formatter import CustomHelpFormatter
 import credentials
 
-version = "0.34.23-3.14"
+version = "0.34.23-3.15"
 changelog = "https://discord.gg/a2rbZPu"
 stream_url = "https://www.twitch.tv/harmonbot"
 listener_id = "180994984038760448"
