8048afa45866bb5b63bef0519ce2666bd0871fa8
Harmon <Harmon758@gmail.com>
2017-02-08 08:43:56 -0600

    Created initial search cog
    Also updated wolframalpha command

--- cogs/resources.py
+++ cogs/resources.py
@@ -30,12 +30,6 @@ class Resources:
 		self.lichess_user_data, self.lichess_tournaments_data = None, None
 		# spotify = spotipy.Spotify()
 	
-	@commands.command()
-	@checks.not_forbidden()
-	async def amazon(self, *search : str):
-		'''Search with Amazon'''
-		await self.bot.embed_reply("[Amazon search for \"{}\"](https://www.amazon.com/s/?field-keywords={})".format(' '.join(search), '+'.join(search)))
-	
 	@commands.command(aliases = ["antonyms"])
 	@checks.not_forbidden()
 	async def antonym(self, word : str):
@@ -45,24 +39,6 @@ class Resources:
 			await self.bot.embed_reply(":no_entry: Word or antonyms not found")
 			return
 		await self.bot.embed_reply(', '.join(antonyms[0].words), title = "Antonyms of {}".format(word.capitalize()))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def aol(self, *search : str):
-		'''Search with AOL'''
-		await self.bot.embed_reply("[AOL search for \"{}\"](https://search.aol.com/aol/search?q={})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command(name = "ask.com")
-	@checks.not_forbidden()
-	async def ask_com(self, *search : str):
-		'''Search with Ask.com'''
-		await self.bot.embed_reply("[Ask.com search for \"{}\"](http://www.ask.com/web?q={})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def baidu(self, *search : str):
-		'''Search with Baidu'''
-		await self.bot.embed_reply("[Baidu search for \"{}\"](http://www.baidu.com/s?wd={})".format(' '.join(search), '+'.join(search)))
 	
 	@commands.group(aliases = ["blizzard"], invoke_without_command = True)
 	@checks.not_forbidden()
@@ -87,12 +63,6 @@ class Resources:
 			await self.bot.embed_reply(":no_entry: Game not found")
 			return
 		await self.bot.embed_reply("[Launch {}](battlenet://{})".format(game, abbrev))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def bing(self, *search : str):
-		'''Search with Bing'''
-		await self.bot.embed_reply("[Bing search for \"{}\"](http://www.bing.com/search?q={})".format(' '.join(search), '+'.join(search)))
 	
 	@commands.group(aliases = ["colour"], pass_context = True, invoke_without_command = True)
 	@checks.not_forbidden()
@@ -153,12 +123,6 @@ class Resources:
 			url = "https://www.dotabuff.com/players/{}".format(int(data["response"]["steamid"]) - 76561197960265728)
 		await self.bot.embed_reply(None, title = "{}'s Dotabuff profile".format(account), title_url = url)
 	
-	@commands.command()
-	@checks.not_forbidden()
-	async def duckduckgo(self, *search : str):
-		'''Search with DuckDuckGo'''
-		await self.bot.embed_reply("[DuckDuckGo search for \"{}\"](https://www.duckduckgo.com/?q={})".format(' '.join(search), '+'.join(search)))
-	
 	@commands.group(invoke_without_command = True)
 	@checks.not_forbidden()
 	async def giphy(self, *, search : str):
@@ -186,12 +150,6 @@ class Resources:
 			data = await resp.json()
 		data = data["data"]
 		await self.bot.embed_reply(None, image_url = data[0]["images"]["original"]["url"])
-	
-	@commands.command(aliases = ["search", "googlesearch"])
-	@checks.not_forbidden()
-	async def google(self, *, search : str):
-		'''Google search'''
-		await self.bot.embed_reply("[Google search for \"{}\"](https://www.google.com/search?q={})".format(search, search.replace(' ', '+')))
 	
 	@commands.command(aliases = ["imagesearch", "googleimages"])
 	@checks.not_forbidden()
@@ -255,12 +213,6 @@ class Resources:
 			"Plot: {0[Plot]}```".format(data))
 			if data["Poster"] and data["Poster"] != "N/A": embed.set_thumbnail(url = data["Poster"])
 			await self.bot.say(embed = embed)
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def imfeelinglucky(self, *search : str):
-		'''First Google result of a search'''
-		await self.bot.embed_reply("[First Google result of \"{}\"](https://www.google.com/search?btnI&q={})".format(' '.join(search), '+'.join(search)))
 	
 	@commands.group(pass_context = True, invoke_without_command = True)
 	@checks.not_forbidden()
@@ -381,56 +333,6 @@ class Resources:
 	async def lichess_tournament(self):
 		'''WIP'''
 		pass
-	
-	@commands.command(name = "lma.ctfy")
-	@checks.not_forbidden()
-	async def lma_ctfy(self, *search : str):
-		'''Let Me Ask.Com That For You'''
-		await self.bot.embed_reply("[LMA.CTFY: \"{}\"](http://lmgtfy.com/?s=k&q={})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def lmaoltfy(self, *search : str):
-		'''Let Me AOL That For You'''
-		await self.bot.embed_reply("[LMAOLTFY: \"{}\"](http://lmgtfy.com/?s=a&q={})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def lmatfy(self, *search : str):
-		'''Let Me Amazon That For You'''
-		await self.bot.embed_reply("[LMATFY: \"{}\"](http://lmatfy.co/?q={})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def lmbdtfy(self, *search : str):
-		'''Let Me Baidu That For You'''
-		await self.bot.embed_reply("[LMBDTFY: \"{}\"](https://lmbtfy.cn/?{})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def lmbtfy(self, *search : str):
-		'''Let Me Bing That For You'''
-		output = "[LMBTFY: \"{}\"](http://lmbtfy.com/?s=b&q={})\n".format(' '.join(search), '+'.join(search))
-		output += "[LMBTFY: \"{}\"](http://letmebingthatforyou.com/q={})".format(' '.join(search), '+'.join(search))
-		await self.bot.embed_reply(output)
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def lmdtfy(self, *search : str):
-		'''Let Me DuckDuckGo That For You'''
-		await self.bot.embed_reply("[LMDTFY: \"{}\"](http://lmgtfy.com/?s=d&q={})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def lmgtfy(self, *search : str):
-		'''Let Me Google That For You'''
-		await self.bot.embed_reply("[LMGTFY: \"{}\"](http://lmgtfy.com/?q={})".format(' '.join(search), '+'.join(search)))
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def lmytfy(self, *search : str):
-		'''Let Me Yahoo That For You'''
-		await self.bot.embed_reply("[LMYTFY: \"{}\"](http://lmgtfy.com/?s=y&q={})".format(' '.join(search), '+'.join(search)))
 	
 	@commands.command()
 	@checks.not_forbidden()
@@ -1067,56 +969,6 @@ class Resources:
 		'''Look something up on Wikipedia'''
 		await self.bot.reply("https://en.wikipedia.org/wiki/{0}".format("_".join(search)))
 	
-	@commands.group(aliases = ["wa"], pass_context = True, invoke_without_command = True)
-	@checks.not_forbidden()
-	async def wolframalpha(self, ctx, *, search : str):
-		'''
-		Wolfram|Alpha
-		http://www.wolframalpha.com/examples/
-		'''
-		await self._wolframalpha(ctx, search)
-	
-	@wolframalpha.command(name = "location", pass_context = True)
-	@checks.not_forbidden()
-	async def wolframalpha_location(self, ctx, location: str, *, search : str):
-		'''Input location'''
-		await self._wolframalpha(ctx, search, location = location)
-	
-	async def _wolframalpha(self, ctx, search, location = "Fort Yukon, Alaska"):
-		ip = "nice try"
-		search = search.strip('`')
-		result = clients.wolfram_alpha_client.query(search, ip = ip, location = location) # options
-		if not hasattr(result, "pods") and hasattr(result, "didyoumeans"):
-			if result.didyoumeans["@count"] == '1':
-				didyoumean = result.didyoumeans["didyoumean"]["#text"]
-			else:
-				didyoumean = result.didyoumeans["didyoumean"][0]["#text"]
-			await self.bot.reply("Using closest Wolfram|Alpha interpretation: `{}`".format(didyoumean))
-			result = clients.wolfram_alpha_client.query(didyoumean, ip = ip, location = location)
-		if hasattr(result, "pods"):
-			for pod in result.pods:
-				output = ("**{}**".format(pod.title))
-				link_output, text_output = [], []
-				for subpod in pod.subpods:
-					image = next(subpod.img)
-					short_url = await self._shorturl(image.src)
-					link_output.append("{}".format(short_url))
-					if subpod.plaintext and subpod.plaintext.replace('\n', ' ') not in (image.title, image.alt, image.title.strip(' '), image.alt.strip(' ')) or not ctx.message.server.me.permissions_in(ctx.message.channel).embed_links:
-						print(image.title)
-						print(image.alt)
-						print(subpod.plaintext.replace('\n', ' '))
-						text_output.append("\n{}".format(subpod.plaintext))
-				output += " ({})".format(', '.join(link_output))
-				output += "".join(text_output)
-				await self.bot.reply(output)
-			if result.timedout:
-				await self.bot.reply("Some results timed out: {}".format(result.timedout.replace(',', ", ")))
-		elif result.timedout:
-			await self.bot.reply("Standard computation time exceeded")
-		else:
-			await self.bot.reply("No results found")
-		# await self.bot.reply(next(result.results).text)
-	
 	@commands.group(pass_context = True, invoke_without_command = True)
 	@checks.not_forbidden()
 	async def xkcd(self, ctx, number : int = 0):
@@ -1150,12 +1002,6 @@ class Resources:
 		embed.set_footer(text = data["alt"])
 		embed.timestamp = datetime.datetime(int(data["year"]), int(data["month"]), int(data["day"]))
 		await self.bot.say(embed = embed)
-	
-	@commands.command()
-	@checks.not_forbidden()
-	async def yahoo(self, *search : str):
-		'''Search with Yahoo'''
-		await self.bot.embed_reply("[Yahoo search for \"{}\"](https://search.yahoo.com/search?q={})".format(' '.join(search), '+'.join(search)))
 	
 	@commands.command(aliases = ["ytinfo"])
 	@checks.not_forbidden()
