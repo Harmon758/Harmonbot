4defde7d709ad0ee5fcab80a2e5ba8ac2c41cac3
Harmon <Harmon758@gmail.com>
2017-04-19 00:49:39 -0500

    Removed group monkeypatching

--- Discord/clients.py
+++ Discord/clients.py
@@ -181,44 +181,7 @@ class Bot(commands.Bot):
 		except discord.errors.NotFound:
 			pass
 	
-	# TODO: Case-Insenstivie commands
-
-# Make Subcommands Case-Insensitive
-
-class Group(commands.GroupMixin, commands.Command):
-
-	def __init__(self, **attrs):
-		self.invoke_without_command = attrs.pop('invoke_without_command', False)
-		super().__init__(**attrs)
-
-	async def invoke(self, ctx):
-		early_invoke = not self.invoke_without_command
-		if early_invoke:
-			await self.prepare(ctx)
-		view = ctx.view
-		previous = view.index
-		view.skip_ws()
-		trigger = view.get_word().lower() # case-insensitive subcommands
-		if trigger:
-			ctx.subcommand_passed = trigger
-			ctx.invoked_subcommand = self.commands.get(trigger, None)
-		if early_invoke:
-			injected = commands.core.inject_context(ctx, self.callback)
-			await injected(*ctx.args, **ctx.kwargs)
-		if trigger and ctx.invoked_subcommand:
-			ctx.invoked_with = trigger
-			await ctx.invoked_subcommand.invoke(ctx)
-		elif not early_invoke:
-			# undo the trigger parsing
-			view.index = previous
-			view.previous = previous
-			await super().invoke(ctx)
-
-def group(name=None, **attrs):
-    return commands.command(name=name, cls=Group, **attrs)
-
-commands.Group = Group
-commands.group = group
+	# TODO: Case-Insenstivie commands + subcommands
 
 
 # Create Folders
