df66b10f4df4fd8461e229d3b996a490ba87bdbe
Harmon <Harmon758@gmail.com>
2017-04-23 21:25:34 -0500

    Ameliorated go fish and war modules to use classes
    Also improved consistency of variable names

--- Discord/modules/gofish.py
+++ Discord/modules/gofish.py
@@ -1,38 +1,43 @@
 
 import pydealer
 
-started = False
-
-def start(number):
-	'''Start a round of Go Fish'''
-	global started, number_of_players, played, hands, deck
-	started = True
-	number_of_players = number
-	played = [False] * number_of_players
-	deck = pydealer.Deck()
-	deck.shuffle()
-	hands = {}
-	for i in range(number_of_players):
-		if number_of_players <= 4:
-			hands[i] = deck.deal(7)
+class GoFishRound:
+	
+	def __init__(self):
+		self.started = False
+		self.number_of_players = 0
+		self.played = []
+		self.hands = {}
+		self.deck = pydealer.Deck()
+		self.deck.shuffle()
+	
+	def start(number):
+		'''Start a round of Go Fish'''
+		self.started = True
+		self.number_of_players = number
+		self.played = [False] * self.number_of_players
+		for i in range(self.number_of_players):
+			if self.number_of_players <= 4:
+				self.hands[i] = self.deck.deal(7)
+			else:
+				self.hands[i] = self.deck.deal(5)
+			self.hands[i].sort()
+	
+	def hand(player_number):
+		hand_string = ""
+		if player_number <= self.number_of_players:
+			for card in hands[player_number].cards:
+				hand_string += card.value + " of " + card.suit + ", "
 		else:
-			hands[i] = deck.deal(5)
-		hands[i].sort()
+			return
+		return hand_string[:-2]
 
-def hand(player_number):
-	hand_string = ""
-	if player_number <= number_of_players:
-		for card in hands[player_number].cards:
-			hand_string += card.value + " of " + card.suit + ", "
-	else:
-		return
-	return hand_string[:-2]
+	def ask(player_number, asked_player, card):
+		if self.played[player_number - 1]:
+			return -1
+		if player_number <= self.number_of_players and asked_player <= self.number_of_players:
+			if len(self.hands[player_number].get(card)):
+				self.played[player_number - 1] = True
+			else:
+				return -2
 
-def ask(player_number, asked_player, card):
-	if played[player_number - 1]:
-		return -1
-	if player_number <= number_of_players and asked_player <= number_of_players:
-		if len(hands[player_number].get(card)):
-			played[player_number - 1] = True
-		else:
-			return -2
