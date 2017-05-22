cf963e8f9f28bb3af8ec9176782bb59b2103da36
Harmon <Harmon758@gmail.com>
2017-05-22 16:55:11 -0500

    Changed GroupMixin.commands to all_commands

--- Discord/cogs/permissions.py
+++ Discord/cogs/permissions.py
@@ -24,8 +24,8 @@ class Permissions:
 	@setpermission.command(name = "everyone")
 	@checks.is_permitted()
 	async def setpermission_everyone(self, ctx, permission : str, setting : bool = None):
-		if permission not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
-		command = self.bot.commands[permission].name
+		if permission not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
+		command = self.bot.all_commands[permission].name
 		with open(clients.data_path + "/permissions/{}.json".format(ctx.guild.id), 'r') as permissions_file:
 			permissions_data = json.load(permissions_file)
 		permissions_data.setdefault("everyone", {})
@@ -37,8 +37,8 @@ class Permissions:
 	@setpermission.command(name = "role")
 	@checks.is_permitted()
 	async def setpermission_role(self, ctx, role : str, permission : str, setting : bool = None):
-		if permission not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission))) 
-		command = self.bot.commands[permission].name
+		if permission not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
+		command = self.bot.all_commands[permission].name
 		matches = [_role for _role in ctx.guild.roles if _role.name == role]
 		if len(matches) > 1: return (await ctx.embed_reply("Error: multiple roles with the name, {}".format(role)))
 		elif len(matches) == 0: return (await ctx.embed_reply('Error: role with name, "{}", not found'.format(role)))
@@ -55,8 +55,8 @@ class Permissions:
 	@setpermission.command(name = "user")
 	@checks.is_permitted()
 	async def setpermission_user(self, ctx, user : str, permission : str, setting : bool = None):
-		if permission not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission))) 
-		command = self.bot.commands[permission].name
+		if permission not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
+		command = self.bot.all_commands[permission].name
 		_user = await utilities.get_user(ctx, user)
 		if not _user: return (await ctx.embed_reply("Error: user not found"))
 		with open(clients.data_path + "/permissions/{}.json".format(ctx.guild.id), 'r') as permissions_file:
@@ -73,8 +73,8 @@ class Permissions:
 	async def getpermission(self, ctx, *options : str):
 		'''Get a permission'''
 		if len(options) == 2:
-			if options[1] not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(options[1]))) 
-			command = self.bot.commands[options[1]].name
+			if options[1] not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(options[1])))
+			command = self.bot.all_commands[options[1]].name
 			user = await utilities.get_user(ctx, options[0])
 			if not user: return (await ctx.embed_reply("Error: user not found"))
 			setting = utilities.get_permission(ctx, command, id = user.id)
@@ -85,16 +85,16 @@ class Permissions:
 	@getpermission.command(name = "everyone")
 	@checks.is_permitted()
 	async def getpermission_everyone(self, ctx, permission : str):
-		if permission not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission))) 
-		command = self.bot.commands[permission].name
+		if permission not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
+		command = self.bot.all_commands[permission].name
 		setting = utilities.get_permission(ctx, command, type = "everyone")
 		await ctx.embed_reply("{} is set to {} for everyone".format(permission, setting))
 	
 	@getpermission.command(name = "role")
 	@checks.is_permitted()
 	async def getpermission_role(self, ctx, role : str, permission : str):
-		if permission not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
-		command = self.bot.commands[permission].name
+		if permission not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
+		command = self.bot.all_commands[permission].name
 		matches = [_role for _role in ctx.guild.roles if _role.name == role]
 		if len(matches) > 1: return (await ctx.embed_reply("Error: multiple roles with the name, {}".format(role)))
 		elif len(matches) == 0: return (await ctx.embed_reply('Error: role with name, "{}", not found'.format(role)))
@@ -105,8 +105,8 @@ class Permissions:
 	@getpermission.command(name = "user")
 	@checks.is_permitted()
 	async def getpermission_user(self, ctx, user : str, permission : str):
-		if permission not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
-		command = self.bot.commands[permission].name
+		if permission not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
+		command = self.bot.all_commands[permission].name
 		_user = await utilities.get_user(ctx, user)
 		if not _user: return (await ctx.embed_reply("Error: user not found"))
 		setting = utilities.get_permission(ctx, command, id = _user.id)
@@ -149,7 +149,7 @@ class Permissions:
 	async def getpermissions_user(self, ctx, user : str):
 		_user = await utilities.get_user(ctx, user)
 		if not _user: return (await ctx.embed_reply("Error: user not found"))
-		with open(clients.data_path + "/permissions/{}.json".format(ctx.guild.id), "r") as permissions_file:
+		with open(clients.data_path + "/permissions/{}.json".format(ctx.guild.id), 'r') as permissions_file:
 			permissions_data = json.load(permissions_file)
 		user_settings = permissions_data.get("users", {}).get(_user.id, {})
 		output = "__Permissions for {}__\n".format(_user.name)
@@ -161,7 +161,7 @@ class Permissions:
 	@getpermissions.command(name = "command")
 	@checks.is_permitted()
 	async def getpermissions_command(self, ctx, command : str):
-		if command not in self.bot.commands: return (await ctx.embed_reply("Error: {} is not a command".format(command)))
+		if command not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(command)))
 		with open(clients.data_path + "/permissions/{}.json".format(ctx.guild.id), 'r') as permissions_file:
 			permissions_data = json.load(permissions_file)
 		output = "__Permissions for {}__\n".format(command)
