b43295fea1f2f9f3688ee09dfa5a3d4649a13d8f
Harmon <Harmon758@gmail.com>
2017-06-01 20:14:24 -0500

    Changed amazon command url to smile

--- Discord/cogs/search.py
+++ Discord/cogs/search.py
@@ -76,7 +76,7 @@ class Search:
 	@checks.not_forbidden()
 	async def amazon(self, *search : str):
 		'''Search with Amazon'''
-		await self.bot.embed_reply("[Amazon search for \"{}\"](https://www.amazon.com/s/?field-keywords={})".format(' '.join(search), '+'.join(search)))
+		await self.bot.embed_reply("[Amazon search for \"{}\"](https://smile.amazon.com/s/?field-keywords={})".format(' '.join(search), '+'.join(search)))
 	
 	@commands.command()
 	@checks.not_forbidden()
