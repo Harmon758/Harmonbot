bac84cc4ab88a3ac72a2ff39968b7a6d119bd7b2
Harmon <Harmon758@gmail.com>
2018-07-05 02:15:32 -0500

    Improved osu cog get user function style

--- Discord/cogs/osu.py
+++ Discord/cogs/osu.py
@@ -62,11 +62,28 @@ class Osu:
 		await self.get_user(ctx, user, 2)
 	
 	async def get_user(self, ctx, user, mode = 0):
-		async with clients.aiohttp_session.get("https://osu.ppy.sh/api/get_user", params = {'k': credentials.osu_api_key, 'u': user, 'm': mode}) as resp:
+		url = "https://osu.ppy.sh/api/get_user"
+		params = {'k': credentials.osu_api_key, 'u': user, 'm': mode}
+		async with clients.aiohttp_session.get(url, params = params) as resp:
 			data = await resp.json()
 		if not data:
 			await ctx.embed_reply(":no_entry: Error: User not found")
 			return
 		data = data[0]
-		await ctx.embed_reply(title = data["username"], title_url = "https://osu.ppy.sh/users/{}".format(data["user_id"]), fields = (("Ranked Score", "{:,}".format(int(data["ranked_score"]))), ("Hit Accuracy", data["accuracy"]), ("Play Count", data["playcount"]), ("Total Score", "{:,}".format(int(data["total_score"]))), ("Performance", "{:,}pp".format(float(data["pp_raw"]))), ("Rank", "#{:,}".format(int(data["pp_rank"]))), ("Level", data["level"]), ("Country Rank", "{} #{:,}".format(pycountry.countries.get(alpha_2 = data["country"]).name, int(data["pp_country_rank"]))), ("Total Hits", "{:,}".format(int(data["count300"]) + int(data["count100"]) + int(data["count50"]))), ("300 Hits", "{:,}".format(int(data["count300"]))), ("100 Hits", "{:,}".format(int(data["count100"]))), ("50 Hits", "{:,}".format(int(data["count50"]))), (self.ssh_emoji, data["count_rank_ssh"]), (self.ss_emoji, data["count_rank_ss"]), (self.sh_emoji, data["count_rank_sh"]), (self.s_emoji, data["count_rank_s"]), (self.a_emoji, data["count_rank_a"])))
+		title_url = "https://osu.ppy.sh/users/{}".format(data["user_id"])
+		country_name = pycountry.countries.get(alpha_2 = data["country"]).name
+		fields = (("Ranked Score", "{:,}".format(int(data["ranked_score"]))), 
+					("Hit Accuracy", data["accuracy"]), ("Play Count", data["playcount"]), 
+					("Total Score", "{:,}".format(int(data["total_score"]))), 
+					("Performance", "{:,}pp".format(float(data["pp_raw"]))), 
+					("Rank", "#{:,}".format(int(data["pp_rank"]))), ("Level", data["level"]), 
+					("Country Rank", "{} #{:,}".format(country_name, int(data["pp_country_rank"]))), 
+					("Total Hits", "{:,}".format(int(data["count300"]) + int(data["count100"]) + int(data["count50"]))), 
+					("300 Hits", "{:,}".format(int(data["count300"]))), 
+					("100 Hits", "{:,}".format(int(data["count100"]))), 
+					("50 Hits", "{:,}".format(int(data["count50"]))), 
+					(self.ssh_emoji, data["count_rank_ssh"]), (self.ss_emoji, data["count_rank_ss"]), 
+					(self.sh_emoji, data["count_rank_sh"]), (self.s_emoji, data["count_rank_s"]), 
+					(self.a_emoji, data["count_rank_a"]))
+		await ctx.embed_reply(title = data["username"], title_url = title_url, fields = fields)
 
