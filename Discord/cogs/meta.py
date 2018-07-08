e3ad488e410f56958292e2428909252ca3693ec3
Harmon <Harmon758@gmail.com>
2018-07-08 04:19:22 -0500

    Improved servers command efficiency

--- Discord/cogs/meta.py
+++ Discord/cogs/meta.py
@@ -202,12 +202,12 @@ class Meta:
 		'''Every server I'm in'''
 		for guild in self.bot.guilds:
 			embed = discord.Embed(color = clients.bot_color)
-			embed.description = f"```Name: {guild.name}\n"
-			embed.description += f"ID: {guild.id}\n"
-			embed.description += f"Owner: {guild.owner} ({guild.owner.id})\n"
-			embed.description += f"Server Region: {guild.region}\n"
-			embed.description += f"Members: {guild.member_count}\n"
-			embed.description += f"Created at: {guild.created_at}\n```"
+			embed.description = (f"```Name: {guild.name}\n"
+									f"ID: {guild.id}\n"
+									f"Owner: {guild.owner} ({guild.owner.id})\n"
+									f"Server Region: {guild.region}\n"
+									f"Members: {guild.member_count}\n"
+									f"Created at: {guild.created_at}\n```")
 			embed.set_thumbnail(url = guild.icon_url)
 			await ctx.whisper(embed = embed)
 	
