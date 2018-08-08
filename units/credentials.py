b08988c61299e41a693322b7bc93b159c57f7365
Harmon <Harmon758@gmail.com>
2018-08-08 05:01:39 -0500

    Fix location unit credentials for Travis CI build

--- units/credentials.py
+++ units/credentials.py
@@ -1,9 +1,10 @@
 
+import os
 import sys
 
-from . import _credentials
-
-for variable in _credentials.__dir__():
-	if not variable.startswith("__"):
-		setattr(sys.modules[__name__], variable, getattr(_credentials, variable))
+if not os.getenv("TRAVIS") or not os.getenv("CI"):
+	from . import _credentials
+	for variable in _credentials.__dir__():
+		if not variable.startswith("__"):
+			setattr(sys.modules[__name__], variable, getattr(_credentials, variable))
 
