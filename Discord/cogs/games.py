191e139b3f9cb08512e709fa5461b129f7a1da3a
Harmon <Harmon758@gmail.com>
2018-08-12 06:26:51 -0500

    [Discord] Use f-string for 8ball command

--- Discord/cogs/games.py
+++ Discord/cogs/games.py
@@ -584,7 +584,7 @@ class Games:
 		Also triggers on \N{BILLIARDS} without prefix
 		'''
 		responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-		await ctx.embed_reply("\N{BILLIARDS} {}".format(random.choice(responses)))
+		await ctx.embed_reply(f"\N{BILLIARDS} {random.choice(responses)}")
 	
 	@commands.group(hidden = True)
 	@checks.not_forbidden()
