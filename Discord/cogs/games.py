05d1642a51802bd27614a38a58ae27d2e4af5f35
Harmon <Harmon758@gmail.com>
2018-08-12 06:37:55 -0500

    [Discord] Use games unit for 8ball command

--- Discord/cogs/games.py
+++ Discord/cogs/games.py
@@ -28,6 +28,10 @@ from modules import utilities
 # from modules import war
 from utilities import checks
 
+sys.path.insert(0, "..")
+from units import games
+sys.path.pop(0)
+
 def setup(bot):
 	bot.add_cog(Games(bot))
 
@@ -583,8 +587,7 @@ class Games:
 		Ask 8ball a yes or no question
 		Also triggers on \N{BILLIARDS} without prefix
 		'''
-		responses = ["It is certain", "It is decidedly so", "Without a doubt", "Yes, definitely", "You may rely on it", "As I see it, yes", "Most likely", "Outlook good", "Yes", "Signs point to yes", "Reply hazy try again", "Ask again later", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no", "Outlook not so good", "Very doubtful"]
-		await ctx.embed_reply(f"\N{BILLIARDS} {random.choice(responses)}")
+		await ctx.embed_reply(f"\N{BILLIARDS} {games.eightball()}")
 	
 	@commands.group(hidden = True)
 	@checks.not_forbidden()
