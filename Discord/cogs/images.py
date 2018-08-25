2c8927339d4aaf0eec7986d891bc04c7b87775a1
Harmon <Harmon758@gmail.com>
2018-08-25 09:59:03 -0500

    [Discord] Fix imgur upload command error handling

--- Discord/cogs/images.py
+++ Discord/cogs/images.py
@@ -131,6 +131,7 @@ class Images:
 		'''Upload images to Imgur'''
 		if not (url or ctx.message.attachments):
 			await ctx.embed_reply(":no_entry: Please input an image and/or url")
+			return
 		image = url or ctx.message.attachments[0].url
 		try:
 			await ctx.embed_reply(self.bot.imgur_client.upload_from_url(image)["link"])
