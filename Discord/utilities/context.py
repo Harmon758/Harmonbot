efea15dc1d5aa93091908fdd5c79051d232da26c
Harmon <Harmon758@gmail.com>
2018-09-04 00:06:24 -0500

    [Discord] Use newline instead of space for Context reply method

--- Discord/utilities/context.py
+++ Discord/utilities/context.py
@@ -39,7 +39,7 @@ class Context(commands.Context):
 		return message
 	
 	def reply(self, content, *args, **kwargs):
-		return self.send("{0.display_name}: {1}".format(self.author, content), **kwargs)
+		return self.send("{0.display_name}:\n{1}".format(self.author, content), **kwargs)
 	
 	def whisper(self, *args, **kwargs):
 		return self.author.send(*args, **kwargs)
