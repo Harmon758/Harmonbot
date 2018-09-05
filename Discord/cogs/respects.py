55287d584c9e47e6dd3dc2b840ba30ae36843b4d
Harmon <Harmon758@gmail.com>
2018-09-04 19:06:44 -0500

    [Discord] Use get user method for respects top command
    Before falling back to get user info method

--- Discord/cogs/respects.py
+++ Discord/cogs/respects.py
@@ -145,7 +145,9 @@ class Respects:
 				# Postgres requires non-scrollable cursors to be created
 				# and used in a transaction.
 				async for record in connection.cursor("SELECT * FROM respect.users ORDER BY respects DESC LIMIT 10"):
-					user = await ctx.bot.get_user_info(record["user_id"])
+					user = ctx.bot.get_user(record["user_id"])
+					if not user:
+						user = await ctx.bot.get_user_info(record["user_id"])
 					fields.append((str(user), f"{record['respects']:,}"))
 		await ctx.embed_reply(title = "Top Respects Paid", fields = fields)
 
