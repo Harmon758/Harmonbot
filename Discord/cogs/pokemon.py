7805b57c5b84b7c6912236129e0cc0d2a929e9f5
Harmon <Harmon758@gmail.com>
2018-11-15 14:55:08 -0600

    [Discord] Optimize pokemon berry command

--- Discord/cogs/pokemon.py
+++ Discord/cogs/pokemon.py
@@ -47,8 +47,7 @@ class Pokemon:
 		async with clients.aiohttp_session.get("http://pokeapi.co/api/v2/berry/" + id_or_name) as resp:
 			data = await resp.json()
 			if resp.status == 404:
-				await ctx.embed_reply(f":no_entry: Error: {data['detail']}")
-				return
+				return await ctx.embed_reply(f":no_entry: Error: {data['detail']}")
 		await ctx.embed_reply(title = f"{data['name'].capitalize()} ({data['id']})", fields = (("Item Name", data["item"]["name"].capitalize()), ("Growth Time", f"{data['growth_time']}h*4"), ("Max Harvest", data["max_harvest"]), ("Size", f"{data['size']} mm"), ("Smoothness", data["smoothness"]), ("Soil Dryness", data["soil_dryness"]), ("Firmness", data["firmness"]["name"]), ("Natural Gift Power", data["natural_gift_power"]), ("Natural Gift Type", data["natural_gift_type"]["name"]), ("Flavors (Potency)", ", ".join(f"{f['flavor']['name']} ({f['potency']})" for f in data["flavors"]))))
 	
 	@pokemon.command()
