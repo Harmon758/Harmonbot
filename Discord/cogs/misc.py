9fdacffa56a5dde9d71b557e36a2b5af3dfa19dd
Harmon <Harmon758@gmail.com>
2019-01-27 11:18:03 -0600

    [Discord] Improve poke command formatting and efficiency

--- Discord/cogs/misc.py
+++ Discord/cogs/misc.py
@@ -126,20 +126,20 @@ class Misc:
 	async def poke(self, ctx, *, user : discord.Member):
 		'''Poke someone'''
 		if user == self.bot.user:
-			await ctx.embed_reply(f"!poke {ctx.author.mention}")
-		else:
-			clients.create_folder(f"{clients.data_path}/user_data/{ctx.author.id}")
-			clients.create_file(f"user_data/{ctx.author.id}/pokes")
-			with open(f"{clients.data_path}/user_data/{ctx.author.id}/pokes.json", 'r') as pokes_file:
-				pokes_data = json.load(pokes_file)
-			pokes_data[str(user.id)] = pokes_data.get(str(user.id), 0) + 1
-			with open(f"{clients.data_path}/user_data/{ctx.author.id}/pokes.json", 'w') as pokes_file:
-				json.dump(pokes_data, pokes_file, indent = 4)
-			embed = discord.Embed(color = ctx.bot.bot_color)
-			embed.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
-			embed.description = f"Poked you for the {clients.inflect_engine.ordinal(pokes_data[str(user.id)])} time!"
-			await user.send(embed = embed)
-			await ctx.embed_reply(f"You have poked {user.mention} for the {clients.inflect_engine.ordinal(pokes_data[str(user.id)])} time!")
+			return await ctx.embed_reply(f"!poke {ctx.author.mention}")
+		clients.create_folder(f"{clients.data_path}/user_data/{ctx.author.id}")
+		clients.create_file(f"user_data/{ctx.author.id}/pokes")
+		with open(f"{clients.data_path}/user_data/{ctx.author.id}/pokes.json", 'r') as pokes_file:
+			pokes_data = json.load(pokes_file)
+		pokes_data[str(user.id)] = pokes_data.get(str(user.id), 0) + 1
+		with open(f"{clients.data_path}/user_data/{ctx.author.id}/pokes.json", 'w') as pokes_file:
+			json.dump(pokes_data, pokes_file, indent = 4)
+		times = clients.inflect_engine.ordinal(pokes_data[str(user.id)])
+		embed = discord.Embed(color = ctx.bot.bot_color)
+		embed.set_author(name = ctx.author, icon_url = ctx.author.avatar_url)
+		embed.description = f"Poked you for the {times} time!"
+		await user.send(embed = embed)
+		await ctx.embed_reply(f"You have poked {user.mention} for the {times} time!")
 	
 	@commands.command()
 	@checks.not_forbidden()
