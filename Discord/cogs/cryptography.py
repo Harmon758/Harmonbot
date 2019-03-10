49158d6e16dc102a304165954f5dd139666fe154
Harmon <Harmon758@gmail.com>
2019-03-10 18:38:22 -0500

    [Discord] Use f-string for encode gost 28147-89 cfb command

--- Discord/cogs/cryptography.py
+++ Discord/cogs/cryptography.py
@@ -252,7 +252,7 @@ class Cryptography(commands.Cog):
 		try:
 			await ctx.embed_reply(pygost.gost28147.cfb_encrypt(key.encode("utf-8"), data.encode("utf-8")).hex())
 		except ValueError as e:
-			await ctx.embed_reply(":no_entry: Error: {}".format(e))
+			await ctx.embed_reply(f":no_entry: Error: {e}")
 	
 	@encode_gost_28147_89.command(name = "cnt")
 	@checks.not_forbidden()
