ccaca2ef153e56d18b9aa3ce4625d05d591640fa
Harmon <Harmon758@gmail.com>
2019-03-10 18:39:19 -0500

    [Discord] Standardize encoding case in encode gost 28147-89 cfb command

--- Discord/cogs/cryptography.py
+++ Discord/cogs/cryptography.py
@@ -250,7 +250,7 @@ class Cryptography(commands.Cog):
 	async def encode_gost_28147_89_cfb(self, ctx, key : str, *, data : str):
 		'''Magma with CFB mode of operation'''
 		try:
-			await ctx.embed_reply(pygost.gost28147.cfb_encrypt(key.encode("utf-8"), data.encode("utf-8")).hex())
+			await ctx.embed_reply(pygost.gost28147.cfb_encrypt(key.encode("UTF-8"), data.encode("UTF-8")).hex())
 		except ValueError as e:
 			await ctx.embed_reply(f":no_entry: Error: {e}")
 	
