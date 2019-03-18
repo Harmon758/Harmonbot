b9d6022f13e894569d651f57f04b0be6f0870edb
Harmon <Harmon758@gmail.com>
2019-03-17 19:27:59 -0500

    [Discord] Use Optional converter for image recognition command

--- Discord/cogs/images.py
+++ Discord/cogs/images.py
@@ -3,6 +3,7 @@ from discord.ext import commands
 
 import inspect
 import re
+from typing import Optional
 
 import clarifai.rest
 import imgurpython
@@ -80,7 +81,7 @@ class Images(commands.Cog):
 		# TODO: handle 403 daily limit exceeded error
 	
 	@image.command(name = "recognition")
-	async def image_recognition(self, ctx, image_url : str = ""):
+	async def image_recognition(self, ctx, image_url : Optional[str]):
 		'''Image recognition'''
 		if not image_url:
 			if not ctx.message.attachments:
