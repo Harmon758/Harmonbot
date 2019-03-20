d0eee72469a2672d735cf188c78c8d6689ce2109
Harmon <Harmon758@gmail.com>
2019-03-20 08:22:59 -0500

    [Discord] Remove now unnecessary Command import from Help Command

--- Discord/utilities/help_command.py
+++ Discord/utilities/help_command.py
@@ -1,7 +1,7 @@
 
 import discord
 from discord.ext import commands
-from discord.ext.commands import Cog, Command, Group, Paginator
+from discord.ext.commands import Cog, Group, Paginator
 
 import difflib
 import itertools
