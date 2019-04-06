2ee59d358a05c3d07f01ac33664a65c2d51a48f8
Harmon <Harmon758@gmail.com>
2019-04-06 12:31:26 -0500

    [TwitchIO] Rename audiodefine command to pronunciation command
    Add pronounce as an alias and keep audiodefine as an alias

--- Twitch/cogs/words.py
+++ Twitch/cogs/words.py
@@ -10,19 +10,6 @@ class Words:
 		self.bot = bot
 	
 	@commands.command()
-	async def audiodefine(self, ctx, word):
-		url = f"http://api.wordnik.com:80/v4/word.json/{word}/audio"
-		params = {"useCanonical": "false", "limit": 1, "api_key": self.bot.WORDNIK_API_KEY}
-		async with self.bot.aiohttp_session.get(url, params = params) as resp:
-			data = await resp.json()
-			if resp.status == 429:
-				return await ctx.send(f"Error: {data['message']}")
-		if data:
-			await ctx.send(f"{data[0]['word'].capitalize()}: {data[0]['fileUrl']}")
-		else:
-			await ctx.send("Word or audio not found.")
-	
-	@commands.command()
 	async def define(self, ctx, word):
 		url = f"http://api.wordnik.com:80/v4/word.json/{word}/definitions"
 		params = {"limit": 1, "includeRelated": "false", "useCanonical": "false", "includeTags": "false", 
@@ -33,6 +20,19 @@ class Words:
 			await ctx.send(f"{data[0]['word'].capitalize()}: {data[0]['text']}")
 		else:
 			await ctx.send("Definition not found.")
+	
+	@commands.command(aliases = ("audiodefine", "pronounce"))
+	async def pronunciation(self, ctx, word):
+		url = f"http://api.wordnik.com:80/v4/word.json/{word}/audio"
+		params = {"useCanonical": "false", "limit": 1, "api_key": self.bot.WORDNIK_API_KEY}
+		async with self.bot.aiohttp_session.get(url, params = params) as resp:
+			data = await resp.json()
+			if resp.status == 429:
+				return await ctx.send(f"Error: {data['message']}")
+		if data:
+			await ctx.send(f"{data[0]['word'].capitalize()}: {data[0]['fileUrl']}")
+		else:
+			await ctx.send("Word or audio not found.")
 	
 	@commands.command()
 	async def randomword(self, ctx):
