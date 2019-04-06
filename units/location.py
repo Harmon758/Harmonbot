f63c61962170b2113fc3f93123679bdd21521346
Harmon <Harmon758@gmail.com>
2019-04-06 16:00:54 -0500

    [Units] Add wind degrees to direction function

--- units/location.py
+++ units/location.py
@@ -42,4 +42,24 @@ async def get_timezone_data(location = None, latitude = None, longitude = None, 
 		error_message = timezone_data.get("errorMessage", timezone_data["status"])
 		raise UnitOutputError(f"Error: {error_message}")
 	return timezone_data
+	
+def wind_degrees_to_direction(degrees):
+	# http://climate.umn.edu/snow_fence/components/winddirectionanddegreeswithouttable3.htm
+	if 0 <= degrees <= 11.25 or 348.75 <= degrees <= 360: return 'N'
+	elif 11.25 <= degrees <= 33.75: return "NNE"
+	elif 33.75 <= degrees <= 56.25: return "NE"
+	elif 56.25 <= degrees <= 78.75: return "ENE"
+	elif 78.75 <= degrees <= 101.25: return 'E'
+	elif 101.25 <= degrees <= 123.75: return "ESE"
+	elif 123.75 <= degrees <= 146.25: return "SE"
+	elif 146.25 <= degrees <= 168.75: return "SSE"
+	elif 168.75 <= degrees <= 191.25: return 'S'
+	elif 191.25 <= degrees <= 213.75: return "SSW"
+	elif 213.75 <= degrees <= 236.25: return "SW"
+	elif 236.25 <= degrees <= 258.75: return "WSW"
+	elif 258.75 <= degrees <= 281.25: return 'W'
+	elif 281.25 <= degrees <= 303.75: return "WNW"
+	elif 303.75 <= degrees <= 326.25: return "NW"
+	elif 326.25 <= degrees <= 348.75: return "NNW"
+	else: return ""
 
