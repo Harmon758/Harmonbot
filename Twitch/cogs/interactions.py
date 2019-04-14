9061f55daac673943bea456088898aeeda356030
Harmon <Harmon758@gmail.com>
2019-04-14 18:55:18 -0500

    [TwitchIO] Strip slash from start of hello and bye command responses
    Fixes issue with Twitch command execution using input injection for hello and bye command responses

--- Twitch/cogs/interactions.py
+++ Twitch/cogs/interactions.py
@@ -12,14 +12,14 @@ class Interactions:
 		if not user or user.lower() == "harmonbot":
 			await ctx.send(f"Bye, {ctx.author.name.capitalize()}!")
 		else:
-			await ctx.send(f"{user.title()}, {ctx.author.name.capitalize()} says goodbye!")
+			await ctx.send(f"{user.title().lstrip('/')}, {ctx.author.name.capitalize()} says goodbye!")
 	
 	@commands.command(aliases = ("hi",))
 	async def hello(self, ctx, *, user = None):
 		if not user or user.lower() == "harmonbot":
 			await ctx.send(f"Hello, {ctx.author.name.capitalize()}!")
 		else:
-			await ctx.send(f"{user.title()}, {ctx.author.name.capitalize()} says hello!")
+			await ctx.send(f"{user.title().lstrip('/')}, {ctx.author.name.capitalize()} says hello!")
 	
 	@commands.command(aliases = ("congrats", "grats", "gz"))
 	async def congratulations(self, ctx, *, user = None):
