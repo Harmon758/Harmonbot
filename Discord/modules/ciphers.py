f74dc187f51be47441dcd40e3d86db42a98a91b3
Harmon <Harmon758@gmail.com>
2019-04-18 12:49:19 -0500

    [Discord] Improve and normalize ciphers module formatting
    Also remove unnecessary comments and parentheses

--- Discord/modules/ciphers.py
+++ Discord/modules/ciphers.py
@@ -1,38 +1,26 @@
 
 character_to_morse = {
-	'A' : ".-", 'B' : "-...", 'C' : "-.-.", 'D' : "-..", 'E' : '.', 'F' : "..-.", 'G' : "--.", 'H' : "....", 'I' : "..", 'J' : ".---", 'K' : "-.-", 
-	'L' : ".-..", 'M' : "--", 'N' : "-.", 'O' : "---", 'P' : ".--.", 'Q' : "--.-", 'R' : ".-.", 'S' : "...", 'T' : '-', 'U' : "..-", 'V' : "...-", 
-	'W' : ".--", 'X' : "-..-", 'Y' : "-.--", 'Z' : "--..", '0' : "----", '1' : ".----", '2' : "..---", '3' : "...--", '4' : "....-", '5' : ".....", 
-	'6' : "-....", '7' : "--...", '8' : "---..", '9' : "----.", '.' : ".-.-.-", ',' : "--..--", ':' : "---...", '?' : "..--..", "'" : ".---.", 
-	'-' : "-....-", '/' : "-..-.", '!' : "-.-.--", '(' : "-.--.", ')' : "-.--.-", '&' : ".-...", ';' : "-.-.-.", '=' : "-...-", '+' : ".-.-.", 
-	'_' : "..--.-", '"' : ".-..-.", '$' : "...-..-", '@' : ".--.-.", ' ' : "/"
+	'A': ".-", 'B': "-...", 'C': "-.-.", 'D': "-..", 'E': '.', 'F': "..-.", 'G': "--.", 'H': "....", 
+	'I': "..", 'J': ".---", 'K': "-.-", 'L': ".-..", 'M': "--", 'N': "-.", 'O': "---", 'P': ".--.", 
+	'Q': "--.-", 'R': ".-.", 'S': "...", 'T': '-', 'U': "..-", 'V': "...-", 'W': ".--", 'X': "-..-", 
+	'Y': "-.--", 'Z': "--..", '0': "----", '1': ".----", '2': "..---", '3': "...--", '4': "....-", 
+	'5': ".....", '6': "-....", '7': "--...", '8': "---..", '9': "----.", '.': ".-.-.-", ',': "--..--", 
+	':': "---...", '?': "..--..", "'": ".---.", '-': "-....-", '/': "-..-.", '!': "-.-.--", 
+	'(': "-.--.", ')': "-.--.-", '&': ".-...", ';': "-.-.-.", '=': "-...-", '+': ".-.-.", 
+	'_': "..--.-", '"': ".-..-.", '$': "...-..-", '@': ".--.-.", ' ': '/'
 }
 
-morse_to_character = {value : key for key, value in character_to_morse.items()}
+morse_to_character = {value: key for key, value in character_to_morse.items()}
 
 def encode_morse(message):
-	'''
-	encoded_message = ""
-	for character in message.upper():
-		encoded_message += character_to_morse[character] + ' '
-	return encoded_message
-	'''
 	try:
 		return ' '.join(character_to_morse[character] for character in message.upper())
 	except KeyError:
 		return "Error"
 
 def decode_morse(message):
-	'''
-	decoded_message = ""
-	for word in message.split(" / "):
-		for character in word.split(' '):
-			decoded_message += morse_to_character[character]
-		decoded_message += ' '
-	return decoded_message
-	'''
 	try:
-		return ' '.join(''.join((morse_to_character[character]) for character in word.split(' ')) for word in message.split(" / "))
+		return ' '.join(''.join(morse_to_character[character] for character in word.split(' ')) for word in message.split(" / "))
 	except KeyError:
 		return "Error"
 
