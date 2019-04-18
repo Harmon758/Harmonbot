75756f20d4b63daa8425609620e4b32dcb9faab4
Harmon <Harmon758@gmail.com>
2019-04-18 14:38:29 -0500

    [Units] Add cryptography unit with morse code functions

---
+++ units/cryptography.py
@@ -0,0 +1,28 @@
+
+from .errors import UnitOutputError
+
+character_to_morse = {
+	'A': ".-", 'B': "-...", 'C': "-.-.", 'D': "-..", 'E': '.', 'F': "..-.", 'G': "--.", 'H': "....", 
+	'I': "..", 'J': ".---", 'K': "-.-", 'L': ".-..", 'M': "--", 'N': "-.", 'O': "---", 'P': ".--.", 
+	'Q': "--.-", 'R': ".-.", 'S': "...", 'T': '-', 'U': "..-", 'V': "...-", 'W': ".--", 'X': "-..-", 
+	'Y': "-.--", 'Z': "--..", '0': "----", '1': ".----", '2': "..---", '3': "...--", '4': "....-", 
+	'5': ".....", '6': "-....", '7': "--...", '8': "---..", '9': "----.", '.': ".-.-.-", ',': "--..--", 
+	':': "---...", '?': "..--..", "'": ".---.", '-': "-....-", '/': "-..-.", '!': "-.-.--", 
+	'(': "-.--.", ')': "-.--.-", '&': ".-...", ';': "-.-.-.", '=': "-...-", '+': ".-.-.", 
+	'_': "..--.-", '"': ".-..-.", '$': "...-..-", '@': ".--.-.", ' ': '/'
+}
+
+morse_to_character = {value: key for key, value in character_to_morse.items()}
+
+def encode_morse_code(message):
+	try:
+		return ' '.join(character_to_morse[character] for character in message.upper())
+	except KeyError as e:
+		raise UnitOutputError(f"Unable to encode {e}")
+
+def decode_morse_code(message):
+	try:
+		return ' '.join(''.join(morse_to_character[character] for character in word.split(' ')) for word in message.split(" / "))
+	except KeyError as e:
+		raise UnitOutputError(f"Unable to decode {e}")
+
