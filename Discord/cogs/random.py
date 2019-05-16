73710fd5240c80b4a7a57366fa26b71d4b7d325a
Harmon <Harmon758@gmail.com>
2019-05-16 03:41:59 -0500

    [Discord] Improve quote command formatting

--- Discord/cogs/random.py
+++ Discord/cogs/random.py
@@ -475,6 +475,7 @@ class Random(commands.Cog):
 		questions = data.split('\n')
 		await ctx.embed_reply("{}?".format(random.choice(questions).capitalize()))
 	
+	# TODO: separate quote command
 	@commands.command()
 	@checks.not_forbidden()
 	async def quote(self, ctx, message_id: int = None, channel: discord.TextChannel = None):
@@ -488,17 +489,19 @@ class Random(commands.Cog):
 			except discord.NotFound:
 				await ctx.embed_reply(":no_entry: Message not found")
 			else:
-				await ctx.embed_say(message.content, author_name = message.author.display_name, author_icon_url = message.author.avatar_url, footer_text = "Sent", timestamp = message.created_at, attempt_delete = False)
+				await ctx.embed_say(message.content, 
+									author_name = message.author.display_name, author_icon_url = message.author.avatar_url, 
+									footer_text = "Sent", timestamp = message.created_at, 
+									attempt_delete = False)
 		else:
-			async with ctx.bot.aiohttp_session.get("http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en") as resp:
+			url = "http://api.forismatic.com/api/1.0/?method=getQuote&format=json&lang=en"
+			async with ctx.bot.aiohttp_session.get(url) as resp:
 				try:
 					data = await resp.json()
 				except:
 					await ctx.embed_reply(":no_entry: Error")
 					return
 			await ctx.embed_reply(data["quoteText"], footer_text = data["quoteAuthor"]) # quoteLink?
-	
-	# TODO: separate quote command
 	
 	@commands.command()
 	@checks.not_forbidden()
