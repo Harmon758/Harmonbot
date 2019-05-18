043cac22fcbc4c22c52e8066948b711d85528999
Harmon <Harmon758@gmail.com>
2019-05-17 22:14:33 -0500

    [Discord] Use tasks extension for RSS

--- Discord/cogs/rss.py
+++ Discord/cogs/rss.py
@@ -1,6 +1,6 @@
 
 import discord
-from discord.ext import commands
+from discord.ext import commands, tasks
 
 import asyncio
 import datetime
@@ -40,10 +40,10 @@ class RSS(commands.Cog):
 			if len(matching_utc_offsets) == 1:
 				self.tzinfos[timezone_abbreviation] = dateutil.tz.gettz(matching_timezones[0])
 		
-		self.task = self.bot.loop.create_task(self.check_rss_feeds())
+		self.check_rss_feeds.start()
 	
 	def cog_unload(self):
-		self.task.cancel()
+		self.check_rss_feeds.cancel()
 	
 	async def inititalize_database(self):
 		await self.bot.connect_to_database()
@@ -152,12 +152,188 @@ class RSS(commands.Cog):
 		records = await ctx.bot.db.fetch("SELECT feed FROM rss.feeds WHERE channel_id = $1", ctx.channel.id)
 		await ctx.embed_reply('\n'.join(record["feed"] for record in records))
 	
+	@tasks.loop()
 	async def check_rss_feeds(self):
-		offset_aware_task_start_time = datetime.datetime.now(datetime.timezone.utc)
-		## offset_naive_task_start_time = datetime.datetime.utcnow()
+		records = await self.bot.db.fetch(
+			"""
+			SELECT DISTINCT ON (feed) feed, last_checked, ttl
+			FROM rss.feeds
+			ORDER BY feed, last_checked
+			"""
+		)
+		if not records:
+			await asyncio.sleep(60)
+		for record in records:
+			feed = record["feed"]
+			if feed in self.feeds_failed_to_initialize:
+				continue
+			if record["ttl"] and datetime.datetime.now(datetime.timezone.utc) < record["last_checked"] + datetime.timedelta(minutes = record["ttl"]):
+				continue
+			try:
+				with self.bot.suppress_SSLCertVerificationError():
+					async with self.bot.aiohttp_session.get(feed) as resp:
+						feed_text = await resp.text()
+				feed_info = await self.bot.loop.run_in_executor(None, functools.partial(feedparser.parse, io.BytesIO(feed_text.encode("UTF-8")), response_headers = {"Content-Location": feed}))
+				# Still necessary to run in executor?
+				ttl = None
+				if "ttl" in feed_info.feed:
+					ttl = int(feed_info.feed.ttl)
+				await self.bot.db.execute(
+					"""
+					UPDATE rss.feeds
+					SET last_checked = NOW(), 
+						ttl = $1
+					WHERE feed = $2
+					""", 
+					ttl, feed
+				)
+				for entry in feed_info.entries:
+					if "id" not in entry:
+						continue
+					inserted = await self.bot.db.fetchrow(
+						"""
+						INSERT INTO rss.entries (entry, feed)
+						VALUES ($1, $2)
+						ON CONFLICT DO NOTHING
+						RETURNING *
+						""", 
+						entry.id, feed
+					)
+					if not inserted:
+						continue
+					# Get timestamp
+					## if "published_parsed" in entry:
+					##  timestamp = datetime.datetime.fromtimestamp(time.mktime(entry.published_parsed))
+					### inaccurate
+					if "published" in entry and entry.published:
+						timestamp = dateutil.parser.parse(entry.published, tzinfos = self.tzinfos)
+					elif "updated" in entry:  # and entry.updated necessary?; check updated first?
+						timestamp = dateutil.parser.parse(entry.updated, tzinfos = self.tzinfos)
+					else:
+						timestamp = discord.Embed.Empty
+					# TODO: Better method?
+					if timestamp and timestamp.tzinfo and timestamp < self.offset_aware_task_start_time:
+						continue
+					## elif timestamp < self.offset_naive_task_start_time: continue
+					# Get and set description, title, url + set timestamp
+					description = entry.get("summary")
+					if not description and "content" in entry:
+						description = entry["content"][0].get("value")
+					if description:
+						description = BeautifulSoup(description, "lxml").get_text(separator = '\n')
+						description = re.sub("\n\s*\n", '\n', description)
+						if len(description) > self.bot.EMBED_DESCRIPTION_CHARACTER_LIMIT:
+							space_index = description.rfind(' ', 0, self.bot.EDCL - 3)
+							# EDCL: Embed Description Character Limit
+							description = description[:space_index] + "..."
+					title = textwrap.shorten(entry.get("title"), width = self.bot.ETiCL, placeholder = "...")
+					# ETiCL: Embed Title Character Limit
+					embed = discord.Embed(title = html.unescape(title), 
+											url = entry.link, 
+											description = description, 
+											timestamp = timestamp, 
+											color = self.bot.rss_color)
+					# Get and set thumbnail url
+					thumbnail_url = None
+					if "media_thumbnail" in entry:
+						thumbnail_url = entry["media_thumbnail"][0].get("url")
+					if not thumbnail_url and "media_content" in entry:
+						media_image = discord.utils.find(lambda c: "image" in c.get("medium", ""), entry["media_content"])
+						if media_image:
+							thumbnail_url = media_image.get("url")
+					if not thumbnail_url and "links" in entry:
+						image_link = discord.utils.find(lambda l: "image" in l["type"], entry["links"])
+						if image_link:
+							thumbnail_url = image_link.get("href")
+					if not thumbnail_url and "content" in entry:
+						content_value = entry["content"][0].get("value")
+						if content_value:
+							parsed_content_value = BeautifulSoup(content_value, "lxml")
+							content_img = getattr(parsed_content_value, "img")
+							if content_img:
+								thumbnail_url = content_img.get("src")
+					if not thumbnail_url and "media_content" in entry:
+						media_content = discord.utils.find(lambda c: "url" in c, entry["media_content"])
+						if media_content:
+							thumbnail_url = media_content["url"]
+					if not thumbnail_url and "description" in entry:
+						parsed_description = BeautifulSoup(entry.description, "lxml")
+						description_img = getattr(parsed_description, "img")
+						if description_img:
+							thumbnail_url = description_img.get("src")
+					if thumbnail_url:
+						if not urllib.parse.urlparse(thumbnail_url).netloc:
+							thumbnail_url = feed_info.feed.link + thumbnail_url
+						embed.set_thumbnail(url = thumbnail_url)
+					# Get and set footer icon url
+					feed_channel_info = feed_info.feed
+					footer_icon_url = discord.Embed.Empty
+					if "icon" in feed_channel_info:
+						footer_icon_url = feed_channel_info["icon"]
+					if not footer_icon_url and "logo" in feed_channel_info:
+						footer_icon_url = feed_channel_info["logo"]
+					if not footer_icon_url and "image" in feed_channel_info:
+						feed_image = feed_channel_info["image"]
+						if "href" in feed_image:
+							footer_icon_url = feed_image["href"]
+					if not footer_icon_url:
+						parsed_text = BeautifulSoup(feed_text, "lxml")
+						image_parsed = getattr(parsed_text, "image")
+						if image_parsed:
+							image_parsed_values = list(image_parsed.attrs.values())
+							if image_parsed_values:
+								footer_icon_url = image_parsed_values[0]
+					embed.set_footer(text = feed_info.feed.title, icon_url = footer_icon_url)
+					channel_records = await self.bot.db.fetch("SELECT channel_id FROM rss.feeds WHERE feed = $1", feed)
+					for record in channel_records:
+						text_channel = self.bot.get_channel(record["channel_id"])
+						if text_channel:
+							try:
+								await text_channel.send(embed = embed)
+							except discord.Forbidden:
+								pass
+							except discord.HTTPException as e:
+								if e.status == 400 and e.code == 50035:
+									if "In embed.url: Not a well formed URL." in e.text:
+										embed.url = discord.Embed.Empty
+									if "In embed.thumbnail.url: Not a well formed URL." in e.text:
+										embed.set_thumbnail(url = "")
+									await text_channel.send(embed = embed)
+								else:
+									raise
+						# TODO: Remove text channel data if now non-existent
+			except (aiohttp.ClientConnectionError, aiohttp.ClientPayloadError, 
+					asyncio.TimeoutError, UnicodeDecodeError) as e:
+				await self.bot.db.execute(
+					"""
+					INSERT INTO rss.errors (feed, type, message)
+					VALUES ($1, $2, $3)
+					""", 
+					feed, type(e).__name__, str(e)
+				)
+				# Print error?
+				await asyncio.sleep(10)
+				# TODO: Add variable for sleep time
+				'''
+				except asyncio.CancelledError:
+					print(f"{self.bot.console_message_prefix}RSS Task Cancelled @ {datetime.datetime.now().time().isoformat()}")
+					return
+				'''
+				# TODO: Handle canceled error/task cleanup
+			except Exception as e:
+				print("Exception in RSS Task", file = sys.stderr)
+				traceback.print_exception(type(e), e, e.__traceback__, file = sys.stderr)
+				logging.errors_logger.error("Uncaught RSS Task exception\n", exc_info = (type(e), e, e.__traceback__))
+				print(f" (feed: {feed})")
+				await asyncio.sleep(60)
+	
+	@check_rss_feeds.before_loop
+	async def before_check_rss_feeds(self):
+		self.offset_aware_task_start_time = datetime.datetime.now(datetime.timezone.utc)
+		## self.offset_naive_task_start_time = datetime.datetime.utcnow()
 		await self.inititalize_database()
 		records = await self.bot.db.fetch("SELECT DISTINCT feed FROM rss.feeds")
-		feeds_failed_to_initialize = []
+		self.feeds_failed_to_initialize = []
 		for record in records:
 			feed = record["feed"]
 			try:
@@ -179,184 +355,11 @@ class RSS(commands.Cog):
 			except aiohttp.ClientConnectionError as e:
 				print(f"Failed to initialize feed in RSS Task: {feed}\n"
 						f" Connection Error: {type(e).__name__}: {e}", file = sys.stderr)
-				feeds_failed_to_initialize.append(feed)
+				self.feeds_failed_to_initialize.append(feed)
 			except Exception as e:
 				print(f"Failed to initialize feed in RSS Task: {feed}\nException:", file = sys.stderr)
 				traceback.print_exception(type(e), e, e.__traceback__, file = sys.stderr)
 				logging.errors_logger.error("Uncaught RSS Task exception\n", exc_info = (type(e), e, e.__traceback__))
-				feeds_failed_to_initialize.append(feed)
+				self.feeds_failed_to_initialize.append(feed)
 		await self.bot.wait_until_ready()
-		while not self.bot.is_closed():
-			records = await self.bot.db.fetch(
-				"""
-				SELECT DISTINCT ON (feed) feed, last_checked, ttl
-				FROM rss.feeds
-				ORDER BY feed, last_checked
-				"""
-			)
-			if not records:
-				await asyncio.sleep(60)
-			for record in records:
-				feed = record["feed"]
-				if feed in feeds_failed_to_initialize:
-					continue
-				if record["ttl"] and datetime.datetime.now(datetime.timezone.utc) < record["last_checked"] + datetime.timedelta(minutes = record["ttl"]):
-					continue
-				try:
-					with self.bot.suppress_SSLCertVerificationError():
-						async with self.bot.aiohttp_session.get(feed) as resp:
-							feed_text = await resp.text()
-					feed_info = await self.bot.loop.run_in_executor(None, functools.partial(feedparser.parse, io.BytesIO(feed_text.encode("UTF-8")), response_headers = {"Content-Location": feed}))
-					# Still necessary to run in executor?
-					ttl = None
-					if "ttl" in feed_info.feed:
-						ttl = int(feed_info.feed.ttl)
-					await self.bot.db.execute(
-						"""
-						UPDATE rss.feeds
-						SET last_checked = NOW(), 
-							ttl = $1
-						WHERE feed = $2
-						""", 
-						ttl, feed
-					)
-					for entry in feed_info.entries:
-						if "id" not in entry:
-							continue
-						inserted = await self.bot.db.fetchrow(
-							"""
-							INSERT INTO rss.entries (entry, feed)
-							VALUES ($1, $2)
-							ON CONFLICT DO NOTHING
-							RETURNING *
-							""", 
-							entry.id, feed
-						)
-						if not inserted:
-							continue
-						# Get timestamp
-						## if "published_parsed" in entry:
-						##  timestamp = datetime.datetime.fromtimestamp(time.mktime(entry.published_parsed))
-						### inaccurate
-						if "published" in entry and entry.published:
-							timestamp = dateutil.parser.parse(entry.published, tzinfos = self.tzinfos)
-						elif "updated" in entry:  # and entry.updated necessary?; check updated first?
-							timestamp = dateutil.parser.parse(entry.updated, tzinfos = self.tzinfos)
-						else:
-							timestamp = discord.Embed.Empty
-						# TODO: Better method?
-						if timestamp and timestamp.tzinfo and timestamp < offset_aware_task_start_time:
-							continue
-						## elif timestamp < offset_naive_task_start_time: continue
-						# Get and set description, title, url + set timestamp
-						description = entry.get("summary")
-						if not description and "content" in entry:
-							description = entry["content"][0].get("value")
-						if description:
-							description = BeautifulSoup(description, "lxml").get_text(separator = '\n')
-							description = re.sub("\n\s*\n", '\n', description)
-							if len(description) > self.bot.EMBED_DESCRIPTION_CHARACTER_LIMIT:
-								space_index = description.rfind(' ', 0, self.bot.EDCL - 3)
-								# EDCL: Embed Description Character Limit
-								description = description[:space_index] + "..."
-						title = textwrap.shorten(entry.get("title"), width = self.bot.ETiCL, placeholder = "...")
-						# ETiCL: Embed Title Character Limit
-						embed = discord.Embed(title = html.unescape(title), 
-												url = entry.link, 
-												description = description, 
-												timestamp = timestamp, 
-												color = self.bot.rss_color)
-						# Get and set thumbnail url
-						thumbnail_url = None
-						if "media_thumbnail" in entry:
-							thumbnail_url = entry["media_thumbnail"][0].get("url")
-						if not thumbnail_url and "media_content" in entry:
-							media_image = discord.utils.find(lambda c: "image" in c.get("medium", ""), entry["media_content"])
-							if media_image:
-								thumbnail_url = media_image.get("url")
-						if not thumbnail_url and "links" in entry:
-							image_link = discord.utils.find(lambda l: "image" in l["type"], entry["links"])
-							if image_link:
-								thumbnail_url = image_link.get("href")
-						if not thumbnail_url and "content" in entry:
-							content_value = entry["content"][0].get("value")
-							if content_value:
-								parsed_content_value = BeautifulSoup(content_value, "lxml")
-								content_img = getattr(parsed_content_value, "img")
-								if content_img:
-									thumbnail_url = content_img.get("src")
-						if not thumbnail_url and "media_content" in entry:
-							media_content = discord.utils.find(lambda c: "url" in c, entry["media_content"])
-							if media_content:
-								thumbnail_url = media_content["url"]
-						if not thumbnail_url and "description" in entry:
-							parsed_description = BeautifulSoup(entry.description, "lxml")
-							description_img = getattr(parsed_description, "img")
-							if description_img:
-								thumbnail_url = description_img.get("src")
-						if thumbnail_url:
-							if not urllib.parse.urlparse(thumbnail_url).netloc:
-								thumbnail_url = feed_info.feed.link + thumbnail_url
-							embed.set_thumbnail(url = thumbnail_url)
-						# Get and set footer icon url
-						feed_channel_info = feed_info.feed
-						footer_icon_url = discord.Embed.Empty
-						if "icon" in feed_channel_info:
-							footer_icon_url = feed_channel_info["icon"]
-						if not footer_icon_url and "logo" in feed_channel_info:
-							footer_icon_url = feed_channel_info["logo"]
-						if not footer_icon_url and "image" in feed_channel_info:
-							feed_image = feed_channel_info["image"]
-							if "href" in feed_image:
-								footer_icon_url = feed_image["href"]
-						if not footer_icon_url:
-							parsed_text = BeautifulSoup(feed_text, "lxml")
-							image_parsed = getattr(parsed_text, "image")
-							if image_parsed:
-								image_parsed_values = list(image_parsed.attrs.values())
-								if image_parsed_values:
-									footer_icon_url = image_parsed_values[0]
-						embed.set_footer(text = feed_info.feed.title, icon_url = footer_icon_url)
-						channel_records = await self.bot.db.fetch("SELECT channel_id FROM rss.feeds WHERE feed = $1", feed)
-						for record in channel_records:
-							text_channel = self.bot.get_channel(record["channel_id"])
-							if text_channel:
-								try:
-									await text_channel.send(embed = embed)
-								except discord.Forbidden:
-									pass
-								except discord.HTTPException as e:
-									if e.status == 400 and e.code == 50035:
-										if "In embed.url: Not a well formed URL." in e.text:
-											embed.url = discord.Embed.Empty
-										if "In embed.thumbnail.url: Not a well formed URL." in e.text:
-											embed.set_thumbnail(url = "")
-										await text_channel.send(embed = embed)
-									else:
-										raise
-							# TODO: Remove text channel data if now non-existent
-				except (aiohttp.ClientConnectionError, aiohttp.ClientPayloadError, 
-						asyncio.TimeoutError, UnicodeDecodeError) as e:
-					await self.bot.db.execute(
-						"""
-						INSERT INTO rss.errors (feed, type, message)
-						VALUES ($1, $2, $3)
-						""", 
-						feed, type(e).__name__, str(e)
-					)
-					# Print error?
-					await asyncio.sleep(10)
-					# TODO: Add variable for sleep time
-					'''
-					except asyncio.CancelledError:
-						print(f"{self.bot.console_message_prefix}RSS Task Cancelled @ {datetime.datetime.now().time().isoformat()}")
-						return
-					'''
-					# TODO: Handle canceled error/task cleanup
-				except Exception as e:
-					print("Exception in RSS Task", file = sys.stderr)
-					traceback.print_exception(type(e), e, e.__traceback__, file = sys.stderr)
-					logging.errors_logger.error("Uncaught RSS Task exception\n", exc_info = (type(e), e, e.__traceback__))
-					print(f" (feed: {feed})")
-					await asyncio.sleep(60)
 
