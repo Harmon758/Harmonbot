0fbdae545f4f413d8224d59b50fdfd8ee5de5c59
Harmon <Harmon758@gmail.com>
2019-06-09 08:35:28 -0500

    [Discord] Improve comment formatting in poker cog

--- Discord/cogs/poker.py
+++ Discord/cogs/poker.py
@@ -16,7 +16,7 @@ class Poker(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 		self.poker_status, self.poker_players, self.poker_deck, self.poker_hands, self.poker_turn, self.poker_bets, self.poker_current_bet, self.poker_pot, self.poker_community_cards, self.poker_folded = None, [], None, {}, None, {}, None, None, None, []
-		#check default values
+		# check default values
 	
 	@commands.group(invoke_without_command = True, case_insensitive = True)
 	@checks.not_forbidden()
