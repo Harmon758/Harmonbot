21717e9399c08a5b7be1f1e8c3c486d569fb6fb6
Harmon <Harmon758@gmail.com>
2019-06-23 16:16:27 -0500

    [Discord] Use f-strings for conversions command

--- Discord/cogs/meta.py
+++ Discord/cogs/meta.py
@@ -190,8 +190,8 @@ class Meta(commands.Cog):
 	@commands.command()
 	async def conversions(self, ctx):
 		'''All conversion commands'''
-		await ctx.embed_reply("**Temperature Unit Conversions**: {0}[c, f, k, r, de]__to__[c, f, k, r, de, n, re, ro]\n"
-		"**Weight Unit Conversions**: {0}<unit>__to__<unit>\nunits: [amu, me, bagc, bagpc, barge, kt, ct, clove, crith, da, drt, drav, ev, gamma, gr, gv, longcwt, cwt, shcwt, kg, kip, mark, mite, mitem, ozt, ozav, oz, dwt, pwt, point, lb, lbav, lbm, lbt, quarterimp, quarterinf, quarterlinf, q, sap, sheet, slug, st, atl, ats, longtn, ton, shtn, t, wey, g]".format(ctx.prefix), title = "Conversion Commands")
+		await ctx.embed_reply(f"**Temperature Unit Conversions**: {ctx.prefix}[c, f, k, r, de]__to__[c, f, k, r, de, n, re, ro]\n"
+		f"**Weight Unit Conversions**: {ctx.prefix}<unit>__to__<unit>\nunits: [amu, me, bagc, bagpc, barge, kt, ct, clove, crith, da, drt, drav, ev, gamma, gr, gv, longcwt, cwt, shcwt, kg, kip, mark, mite, mitem, ozt, ozav, oz, dwt, pwt, point, lb, lbav, lbm, lbt, quarterimp, quarterinf, quarterlinf, q, sap, sheet, slug, st, atl, ats, longtn, ton, shtn, t, wey, g]", title = "Conversion Commands")
 	
 	@commands.command(aliases = ["oauth"])
 	async def invite(self, ctx):
