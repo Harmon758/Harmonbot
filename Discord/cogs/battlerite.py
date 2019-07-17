374fd56ccbee554d7f508f9cbb9103c10456fc64
Harmon <Harmon758@gmail.com>
2019-07-16 20:45:25 -0500

    [Discord] Update battlerite assets to revision 62165

--- Discord/cogs/battlerite.py
+++ Discord/cogs/battlerite.py
@@ -50,7 +50,7 @@ class Battlerite(commands.Cog):
 			# TODO: get revision dynamically?
 			# https://api.github.com/repos/StunlockStudios/battlerite-assets/contents/mappings
 			url = ("https://raw.githubusercontent.com/StunlockStudios/battlerite-assets/master/mappings/"
-					"61543/stackables.json")
+					"62165/stackables.json")
 			async with self.bot.aiohttp_session.get(url) as resp:
 				data = await resp.content.read()
 			with open(self.bot.data_path + "/battlerite/stackables.json", "wb") as stackables_file:
@@ -59,7 +59,7 @@ class Battlerite(commands.Cog):
 			# TODO: get revision dynamically?
 			# https://api.github.com/repos/StunlockStudios/battlerite-assets/contents/mappings
 			url = ("https://raw.githubusercontent.com/StunlockStudios/battlerite-assets/master/mappings/"
-					"61543/Localization/English.ini")
+					"62165/Localization/English.ini")
 			async with self.bot.aiohttp_session.get(url) as resp:
 				data = await resp.content.read()
 			with open(self.bot.data_path + "/battlerite/English.ini", "wb") as localization_file:
@@ -84,7 +84,7 @@ class Battlerite(commands.Cog):
 	async def battlerite(self, ctx):
 		'''
 		Battlerite
-		Using revision 61543 mappings
+		Using revision 62165 mappings
 		'''
 		await ctx.send_help(ctx.command)
 	
