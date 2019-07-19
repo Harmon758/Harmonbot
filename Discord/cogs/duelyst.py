85f471a63238815b3506f464f9261d2b96751aae
Harmon <Harmon758@gmail.com>
2019-07-18 21:17:49 -0500

    [Discord] Use cog check for duelyst cog

--- Discord/cogs/duelyst.py
+++ Discord/cogs/duelyst.py
@@ -11,14 +11,15 @@ class Duelyst(commands.Cog):
 	def __init__(self, bot):
 		self.bot = bot
 	
+	def cog_check(self, ctx):
+		return checks.not_forbidden_predicate(ctx)
+	
 	@commands.group(invoke_without_command = True, case_insensitive = True)
-	@checks.not_forbidden()
 	async def duelyst(self, ctx):
 		'''Duelyst'''
 		await ctx.send_help(ctx.command)
 	
 	@duelyst.group(case_insensitive = True)
-	@checks.not_forbidden()
 	async def card(self, ctx, *, name : str):
 		'''Details of a specific card'''
 		url = "https://duelyststats.info/scripts/carddata/get.php"
@@ -27,7 +28,6 @@ class Duelyst(commands.Cog):
 		await ctx.embed_reply(data)
 	
 	@card.command()
-	@checks.not_forbidden()
 	async def card_random(self, ctx):
 		'''Details of a random card'''
 		url = "https://duelyststats.info/scripts/carddata/get.php"
