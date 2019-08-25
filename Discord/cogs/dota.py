7bc9987dc891c03f3fd506b0c25b4b9d3b660f5c
Harmon <Harmon758@gmail.com>
2019-08-25 07:32:49 -0500

    [Discord] Add SteamAccount converter

--- Discord/cogs/dota.py
+++ Discord/cogs/dota.py
@@ -11,6 +11,19 @@ class DotA(commands.Cog):
 	def cog_check(self, ctx):
 		return checks.not_forbidden_predicate(ctx)
 	
+	# TODO: Move to converters file
+	class SteamAccount(commands.Converter):
+		async def convert(self, ctx, argument):
+			try:
+				return int(argument) - 76561197960265728
+			except ValueError:
+				url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
+				params = {"key": ctx.bot.STEAM_WEB_API_KEY, "vanityurl": argument}
+				async with ctx.bot.aiohttp_session.get(url, params = params) as resp:
+					# TODO: Handle 429?
+					data = await resp.json()
+				return int(data['response']['steamid']) - 76561197960265728
+	
 	@commands.group(aliases = ["dota2"], invoke_without_command = True, case_insensitive = True)
 	async def dota(self, ctx):
 		'''Defense of the Ancients 2'''
