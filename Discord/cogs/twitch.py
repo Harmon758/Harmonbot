550ab8403eab801db53170a8c63cab28fbf54dd3
Harmon <Harmon758@gmail.com>
2019-09-19 22:16:32 -0500

    [Discord] Improve formatting for twitch add game command

--- Discord/cogs/twitch.py
+++ Discord/cogs/twitch.py
@@ -75,7 +75,7 @@ class Twitch(commands.Cog):
 		with open(clients.data_path + "/twitch_streams.json", 'w') as streams_file:
 			json.dump(self.streams_info, streams_file, indent = 4)
 		await ctx.embed_reply(f"Added the game, [`{game}`](https://www.twitch.tv/directory/game/{game}), to this text channel\n"
-		"I will now announce here when Twitch streams playing this game go live")
+								"I will now announce here when Twitch streams playing this game go live")
 	
 	@twitch_add.command(name = "keyword", aliases = ["query", "search"])
 	@checks.is_permitted()
