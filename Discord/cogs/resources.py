3e4d4c6e4cc58a7fa979edc60daae469a9a95180
Harmon <Harmon758@gmail.com>
2019-10-04 10:08:50 -0500

    [Discord] Use aiohttp session get argument for steam gameinfo command

--- Discord/cogs/resources.py
+++ Discord/cogs/resources.py
@@ -543,8 +543,9 @@ class Resources(commands.Cog):
 			await ctx.embed_reply(":no_entry: Game not found")
 			return
 		appid = str(app["appid"])
-		url = "http://store.steampowered.com/api/appdetails/?appids={}".format(appid)
-		async with ctx.bot.aiohttp_session.get(url) as resp:
+		url = "http://store.steampowered.com/api/appdetails/"
+		params = {"appids": appid}
+		async with ctx.bot.aiohttp_session.get(url, params = params) as resp:
 			data = await resp.json()
 		data = data[appid]["data"]
 		await ctx.embed_reply(data["short_description"], title = data["name"], title_url = data["website"], fields = (("Release Date", data["release_date"]["date"]), ("Free", "Yes" if data["is_free"] else "No"), ("App ID", data["steam_appid"])), image_url = data["header_image"])
