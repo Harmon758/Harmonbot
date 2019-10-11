3e74e225ea49a3bd96504249a000ec68e379196d
Harmon <Harmon758@gmail.com>
2019-10-11 00:32:36 -0500

    [Discord] Optimize setpermission everyone command

--- Discord/cogs/permissions.py
+++ Discord/cogs/permissions.py
@@ -63,7 +63,6 @@ class Permissions(commands.Cog):
 	async def setpermission_everyone(self, ctx, permission : str, setting : bool = None):
 		if permission not in self.bot.all_commands:
 			return await ctx.embed_reply(f"Error: {permission} is not a command")
-		command = self.bot.all_commands[permission].name
 		await self.bot.db.execute(
 			"""
 			INSERT INTO permissions.everyone (guild_id, permission, setting)
@@ -71,7 +70,7 @@ class Permissions(commands.Cog):
 			ON CONFLICT (guild_id, permission) DO
 			UPDATE SET setting = $3
 			""", 
-			ctx.guild.id, command, setting
+			ctx.guild.id, self.bot.all_commands[permission].name, setting
 		)
 		await ctx.embed_reply("Permission updated\n"
 								f"{permission} set to {setting} for everyone")
