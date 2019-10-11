55bb0f2e20e67feec792ca2e44d864a332534e76
Harmon <Harmon758@gmail.com>
2019-10-11 01:32:15 -0500

    [Discord] Use f-strings for getpermission user command

--- Discord/cogs/permissions.py
+++ Discord/cogs/permissions.py
@@ -152,12 +152,12 @@ class Permissions(commands.Cog):
 	@commands.guild_only()
 	@checks.is_permitted()
 	async def getpermission_user(self, ctx, user : str, permission : str):
-		if permission not in self.bot.all_commands: return (await ctx.embed_reply("Error: {} is not a command".format(permission)))
+		if permission not in self.bot.all_commands: return (await ctx.embed_reply(f"Error: {permission} is not a command"))
 		command = self.bot.all_commands[permission].name
 		_user = await utilities.get_user(ctx, user)
 		if not _user: return (await ctx.embed_reply("Error: user not found"))
 		setting = await ctx.get_permission(command, id = _user.id)
-		await ctx.embed_reply("{} is set to {} for {}".format(permission, setting, _user))
+		await ctx.embed_reply(f"{permission} is set to {setting} for {_user}")
 	
 	@commands.group(invoke_without_command = True, case_insensitive = True)
 	@checks.is_permitted()
