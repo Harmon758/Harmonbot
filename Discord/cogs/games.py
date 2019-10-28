e2416b49fc1883adbcab41af0a73b0a447a7f099
Harmon <Harmon758@gmail.com>
2019-10-27 20:13:07 -0500

    [Discord] Fix adventure woodcutting stop command error response

--- Discord/cogs/games.py
+++ Discord/cogs/games.py
@@ -326,7 +326,7 @@ class Games(commands.Cog):
 		if stopped[0]:
 			await ctx.embed_reply(f"\n:evergreen_tree: You were chopping {stopped[0]} for {stopped[1]:.2f} min. and received {stopped[2]} {stopped[0]} and {stopped[3]} xp")
 		elif stopped[1]:
-			await ctx.embed_reply(":no_entry: You're currently {}! You aren't woodcutting right now")
+			await ctx.embed_reply(f":no_entry: You're currently {stopped[1][0]}! You aren't woodcutting right now")
 		else:
 			await ctx.embed_reply(":no_entry: You aren't woodcutting")
 	
