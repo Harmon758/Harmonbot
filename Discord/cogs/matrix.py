f81a68e330b9ee7f9cdfc1aaf5a1ab3ed3096503
Harmon <Harmon758@gmail.com>
2019-12-02 20:21:38 -0600

    [Discord] Improve type hint formatting for matrix sin command

--- Discord/cogs/matrix.py
+++ Discord/cogs/matrix.py
@@ -106,7 +106,7 @@ class Matrix(commands.Cog):
 		await ctx.embed_reply(str(scipy.linalg.signm(matrix)))
 	
 	@matrix.group(aliases = ["sine"], invoke_without_command = True, case_insensitive = True)
-	async def sin(self, ctx, *, matrix : Matrix):
+	async def sin(self, ctx, *, matrix: Matrix):
 		'''Sine of a matrix'''
 		await ctx.embed_reply(str(scipy.linalg.sinm(matrix)))
 	
