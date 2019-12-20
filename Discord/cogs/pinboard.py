6e223027d7efe40b15d7b00d961c4557e3c28b7a
Harmon <Harmon758@gmail.com>
2019-12-19 18:58:58 -0600

    [Discord] Use member attribute of payload for on_raw_reaction_add event
    For on_raw_reaction_add event handler for pinboard system

--- Discord/cogs/pinboard.py
+++ Discord/cogs/pinboard.py
@@ -277,10 +277,9 @@ class Pinboard(commands.Cog):
 		if pin_count < threshold:
 			# Pin count has not reached threshold yet
 			return
-		guild = self.bot.get_guild(payload.guild_id)
 		pinboard_channel = self.bot.get_channel(pinboard_channel_id)
 		pinned_message_channel = self.bot.get_channel(channel_id)
-		if not private_channels_setting and pinned_message_channel.overwrites_for(guild.default_role).read_messages == False:
+		if not private_channels_setting and pinned_message_channel.overwrites_for(payload.member.guild.default_role).read_messages == False:
 			# Set to ignore private channels and message is in private channel
 			return
 		pinned_message = await pinned_message_channel.fetch_message(message_id)
