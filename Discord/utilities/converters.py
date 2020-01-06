e7582e164b034f3f4fa63dc2fca5ae7b6ae78f2e
Harmon <Harmon758@gmail.com>
2020-01-05 21:21:32 -0600

    [Discord] Use constant for SteamID64 base for Steam Account converter

--- Discord/utilities/converters.py
+++ Discord/utilities/converters.py
@@ -13,11 +13,14 @@ class Maptype(commands.Converter):
 			raise commands.BadArgument("Invalid map type")
 		return argument
 
+# https://developer.valvesoftware.com/wiki/SteamID
+STEAM_ID_64_BASE = int("0x110000100000000", 16)  # Assuming Public Individual user account
+
 # TODO: Use for steam gamecount command?
 class SteamAccount(commands.Converter):
 	async def convert(self, ctx, argument):
 		try:
-			return int(argument) - 76561197960265728
+			return int(argument) - STEAM_ID_64_BASE
 		except ValueError:
 			url = "http://api.steampowered.com/ISteamUser/ResolveVanityURL/v0001/"
 			params = {"key": ctx.bot.STEAM_WEB_API_KEY, "vanityurl": argument}
@@ -26,5 +29,5 @@ class SteamAccount(commands.Converter):
 				data = await resp.json()
 			if data["response"]["success"] == 42:  # NoMatch, https://partner.steamgames.com/doc/api/steam_api#EResult
 				raise commands.BadArgument("Account not found")
-			return int(data['response']['steamid']) - 76561197960265728
+			return int(data['response']['steamid']) - STEAM_ID_64_BASE
 
