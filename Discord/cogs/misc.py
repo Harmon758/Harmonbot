56927b591ccb29d36b6138d3e652ca78257a5e16
Harmon <Harmon758@gmail.com>
2020-01-07 08:05:01 -0600

    [Discord] Fix emote commands

--- Discord/cogs/misc.py
+++ Discord/cogs/misc.py
@@ -8,23 +8,31 @@ from modules import utilities
 from utilities import checks
 
 def setup(bot):
-	
-	def emote_wrapper(name, emote = None):
-		if emote is None: emote = name
-		@commands.command(name = name, help = name.capitalize() + " emote")
-		@checks.not_forbidden()
-		async def emote_command(self, ctx):
-			await ctx.embed_reply(":{}:".format(emote))
-		return emote_command
-	
-	for emote in ("fish", "frog", "turtle", "gun", "tomato", "cucumber", "eggplant", "lizard", "minidisc", "horse", "penguin", "dragon", "eagle", "bird"):
-		setattr(Misc, emote, emote_wrapper(emote))
-	setattr(Misc, "cow", emote_wrapper("cow", emote = "cow2"))
-	setattr(Misc, "panda", emote_wrapper("panda", emote = "panda_face"))
-	
 	bot.add_cog(Misc(bot))
 
 class Misc(commands.Cog):
+	
+	def __new__(cls, *args, **kwargs):
+		def emote_wrapper(name, emote = None):
+			if emote is None: emote = name
+			@commands.command(name = name, help = name.capitalize() + " emote")
+			@checks.not_forbidden()
+			async def emote_command(self, ctx):
+				await ctx.embed_reply(":{}:".format(emote))
+			return emote_command
+		
+		for emote in ("fish", "frog", "turtle", "gun", "tomato", "cucumber", "eggplant", "lizard", "minidisc", "horse", "penguin", "dragon", "eagle", "bird"):
+			command = emote_wrapper(emote)
+			setattr(cls, emote, command)
+			cls.__cog_commands__.append(command)
+		command = emote_wrapper("cow", emote = "cow2")
+		setattr(cls, "cow", command)
+		cls.__cog_commands__.append(command)
+		command = emote_wrapper("panda", emote = "panda_face")
+		setattr(cls, "panda", command)
+		cls.__cog_commands__.append(command)
+		
+		return super().__new__(cls, *args, **kwargs)
 	
 	def __init__(self, bot):
 		self.bot = bot
