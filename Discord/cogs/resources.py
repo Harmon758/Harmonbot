5fc1d9bd2a0016d1a0d01d02c6409be785444ffb
Harmon <Harmon758@gmail.com>
2020-01-10 06:57:10 -0600

    [Discord] Improve clarity of longurl command

--- Discord/cogs/resources.py
+++ Discord/cogs/resources.py
@@ -249,8 +249,7 @@ class Resources(commands.Cog):
 		params = {"shortUrl": url, "key": ctx.bot.GOOGLE_API_KEY}
 		async with ctx.bot.aiohttp_session.get(url, params = params) as resp:
 			if resp.status == 400:
-				await ctx.embed_reply(":no_entry: Error")
-				return
+				return await ctx.embed_reply(":no_entry: Error")
 			data = await resp.json()
 		await ctx.embed_reply(data["longUrl"])
 	
