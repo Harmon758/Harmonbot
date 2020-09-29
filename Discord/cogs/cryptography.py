e157b95a3415fba36ac7f11665800c8b0b9f9527
Harmon <Harmon758@gmail.com>
2020-09-29 07:53:33 -0500

    [Discord] Use error emoji Bot attribute in decode gost subcommand
    Use error emoji Bot attribute in decode gost 28147-89 cbc command

--- Discord/cogs/cryptography.py
+++ Discord/cogs/cryptography.py
@@ -80,7 +80,7 @@ class Cryptography(commands.Cog):
 		try:
 			await ctx.embed_reply(pygost.gost28147.cbc_decrypt(key.encode("UTF-8"), bytearray.fromhex(data)).decode("UTF-8"))
 		except ValueError as e:
-			await ctx.embed_reply(f":no_entry: Error: {e}")
+			await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: {e}")
 	
 	@decode_gost_28147_89.command(name = "cfb")
 	async def decode_gost_28147_89_cfb(self, ctx, key: str, *, data: str):
