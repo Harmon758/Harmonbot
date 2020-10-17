148e6863ba3c09fdf6402201f9b58222680c5bba
Harmon <Harmon758@gmail.com>
2020-10-16 22:47:00 -0500

    [Discord] Use error emoji Bot attribute in encode morse command

--- Discord/cogs/cryptography.py
+++ Discord/cogs/cryptography.py
@@ -329,7 +329,7 @@ class Cryptography(commands.Cog):
 		try:
 			await ctx.embed_reply(encode_morse_code(message))
 		except UnitOutputError as e:
-			await ctx.embed_reply(f":no_entry: Error: {e}")
+			await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: {e}")
 	
 	@encode.command(name = "qr")
 	async def encode_qr(self, ctx, *, message: str):
