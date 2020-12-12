7964f241b39e688341b0f8cae1f9c608b5a83660
Harmon <Harmon758@gmail.com>
2020-12-12 15:38:23 -0600

    [Discord] Fix response when calling in poker

--- Discord/cogs/poker.py
+++ Discord/cogs/poker.py
@@ -128,12 +128,12 @@ class Poker(commands.Cog):
 				while True:
 					response = await ctx.bot.wait_for("message", check = check)
 					if response.content.lower() == "call":
-						self.bets[player.id] = self.current_bet
 						if self.current_bet == 0 or (player.id in self.bets and self.bets[player.id] == self.current_bet):
 							embed.description = (f"{player.mention} attempted to call\n"
 													f"Since there's nothing to call, {player.mention} has checked instead")
 						else:
 							embed.description = f"{player.mention} has called"
+						self.bets[player.id] = self.current_bet
 					elif response.content.lower() == "check":
 						if self.current_bet != 0 and (player.id not in self.bets or self.bets[player.id] < self.current_bet):
 							embed_copy = embed.copy()
