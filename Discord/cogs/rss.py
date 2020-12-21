87fcbba06a56098631e518dc7bc911ae3a2ae21c
Harmon <Harmon758@gmail.com>
2020-12-21 16:37:02 -0600

    [Discord] Handle unsupported schemes for RSS embed footer icon URLs

--- Discord/cogs/rss.py
+++ Discord/cogs/rss.py
@@ -287,7 +287,9 @@ class RSS(commands.Cog):
 										embed.url = discord.Embed.Empty
 									if "In embed.thumbnail.url: Not a well formed URL." in e.text:
 										embed.set_thumbnail(url = "")
-									if "In embed.footer.icon_url: Not a well formed URL." in e.text:
+									if ("In embed.footer.icon_url: Not a well formed URL." in e.text or 
+										("In embed.footer.icon_url: Scheme" in e.text and 
+											"is not supported. Scheme must be one of ('http', 'https')." in e.text)):
 										embed.set_footer(text = feed_info.feed.title)
 									await text_channel.send(embed = embed)
 								else:
