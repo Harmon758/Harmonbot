36fc42d63e8582351151f36e2bb2f4bf58c99cb6
Harmon <Harmon758@gmail.com>
2021-01-17 09:55:43 -0600

    [Discord] Use f-strings for rotmg command

--- Discord/cogs/rotmg.py
+++ Discord/cogs/rotmg.py
@@ -18,32 +18,32 @@ class RotMG(commands.Cog):
 	@commands.group(aliases = ["realmofthemadgod"], invoke_without_command = True, case_insensitive = True)
 	async def rotmg(self, ctx, player : str):
 		'''Realm of the Mad God player information'''
-		url = "https://nightfirec.at/realmeye-api/?player={}".format(player)
+		url = f"https://nightfirec.at/realmeye-api/?player={player}"
 		# http://webhost.ischool.uw.edu/~joatwood/realmeye_api/0.3/
 		async with ctx.bot.aiohttp_session.get(url) as resp:
 			data = await resp.json()
 		if "error" in data:
 			await ctx.embed_reply("Error: " + data["error"])
 			return
-		embed = discord.Embed(title = data["player"], url = "https://www.realmeye.com/player/{}".format(player), color = ctx.bot.bot_color)
+		embed = discord.Embed(title = data["player"], url = f"https://www.realmeye.com/player/{player}", color = ctx.bot.bot_color)
 		embed.set_author(name = ctx.author.display_name, icon_url = ctx.author.avatar_url)
 		if data["donator"] == "true": embed.description = "Donator"
 		embed.add_field(name = "Characters", value = data["chars"])
-		embed.add_field(name = "Total Fame", value = "{:,}".format(data["fame"]))
-		embed.add_field(name = "Fame Rank", value = "{:,}".format(data["fame_rank"]))
+		embed.add_field(name = "Total Fame", value = f"{data['fame']:,}")
+		embed.add_field(name = "Fame Rank", value = f"{data['fame_rank']:,}")
 		embed.add_field(name = "Class Quests Completed", value = data["rank"])
-		embed.add_field(name = "Account Fame", value = "{:,}".format(data["account_fame"]))
-		embed.add_field(name = "Account Fame Rank", value = "{:,}".format(data["account_fame_rank"]))
+		embed.add_field(name = "Account Fame", value = f"{data['account_fame']:,}")
+		embed.add_field(name = "Account Fame Rank", value = f"{data['account_fame_rank']:,}")
 		if created := data.get("created"):
 			embed.add_field(name = "Created", value = created)
-		embed.add_field(name = "Total Exp", value = "{:,}".format(data["exp"]))
-		embed.add_field(name = "Exp Rank", value = "{:,}".format(data["exp_rank"]))
+		embed.add_field(name = "Total Exp", value = f"{data['exp']:,}")
+		embed.add_field(name = "Exp Rank", value = f"{data['exp_rank']:,}")
 		embed.add_field(name = "Last Seen", value = data["player_last_seen"])
 		if guild := data.get("guild"):
 			embed.add_field(name = "Guild", value = guild)
 			embed.add_field(name = "Guild Position", value = data["guild_rank"])
 		if data["desc1"] or data["desc2"] or data["desc3"]:
-			embed.add_field(name = "Description", value = "{}\n{}\n{}".format(data["desc1"], data["desc2"], data["desc3"]))
+			embed.add_field(name = "Description", value = f"{data['desc1']}\n{data['desc2']}\n{data['desc3']}")
 		await ctx.send(embed = embed)
 	
 	@rotmg.command(name = "characters")
