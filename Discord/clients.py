60bcd7eab3067a79fc68fb46bef5f2ea426a986e
Harmon <Harmon758@gmail.com>
2021-06-28 18:37:08 -0500

    [Discord] Handle threads when getting command prefix

--- Discord/clients.py
+++ Discord/clients.py
@@ -533,7 +533,7 @@ class Bot(commands.Bot):
 
 	@staticmethod
 	async def get_command_prefix(bot, message):
-		if message.channel.type is discord.ChannelType.private:
+		if not isinstance(message.channel, discord.Thread) and message.channel.type is discord.ChannelType.private:
 			prefixes = await bot.db.fetchval(
 				"""
 				SELECT prefixes
