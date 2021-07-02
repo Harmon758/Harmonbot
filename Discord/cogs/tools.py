ae31bc629142306032c24f39778b35e662577e6a
Harmon <Harmon758@gmail.com>
2021-07-02 10:29:05 -0500

    [Discord] Use error emoji Bot attribute in tag delete command

--- Discord/cogs/tools.py
+++ Discord/cogs/tools.py
@@ -277,7 +277,7 @@ class Tools(commands.Cog):
 			ctx.author.id, tag
 		)
 		if not deleted:
-			return await ctx.embed_reply(":no_entry: Tag not found")
+			return await ctx.embed_reply(f"{ctx.bot.error_emoji} Tag not found")
 		await ctx.embed_reply(f":ok_hand:{ctx.bot.emoji_skin_tone} Your tag has been deleted")
 	
 	@tag.command(name = "expunge")
