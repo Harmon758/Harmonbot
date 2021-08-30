99fb681bcf0aedbbd2b3001a42ea9529bc676f84
Harmon <Harmon758@gmail.com>
2021-08-30 13:02:33 -0500

    [Discord] Improve Paginator formatting

--- Discord/utilities/paginator.py
+++ Discord/utilities/paginator.py
@@ -1,35 +1,36 @@
 
 from discord.ext import commands
 
+
 class Paginator(commands.Paginator):
-	
-	def __init__(self, seperator = "\n", prefix='```', suffix='```', max_size=2000):
-		super().__init__(prefix, suffix, max_size)
-		self.seperator = seperator
-		self._current_page = []
 
-	def add_section(self, section='', *, empty=False):
-		if len(section) > self.max_size - len(self.prefix) - 2:
-			raise RuntimeError('Section exceeds maximum page size %s' % (self.max_size - len(self.prefix) - 2))
+    def __init__(self, seperator = "\n", prefix='```', suffix='```', max_size=2000):
+        super().__init__(prefix, suffix, max_size)
+        self.seperator = seperator
+        self._current_page = []
 
-		if self._count + len(section) + len(self.seperator) > self.max_size:
-			self.close_page()
+    def add_section(self, section='', *, empty=False):
+        if len(section) > self.max_size - len(self.prefix) - 2:
+            raise RuntimeError('Section exceeds maximum page size %s' % (self.max_size - len(self.prefix) - 2))
 
-		self._count += len(section) + len(self.seperator)
-		self._current_page.append(section)
+        if self._count + len(section) + len(self.seperator) > self.max_size:
+            self.close_page()
 
-		if empty:
-			self._current_page.append('')
-			self._count += len(self.seperator)
+        self._count += len(section) + len(self.seperator)
+        self._current_page.append(section)
 
-	def close_page(self):
-		self._pages.append(self.prefix + "\n" + self.seperator.join(self._current_page) + "\n" + self.suffix)
-		self._current_page = []
-		self._count = len(self.prefix) + len(self.seperator)
-	
-	@property
-	def pages(self):
-		if len(self._current_page) > 0:
-			self.close_page()
-		return self._pages
+        if empty:
+            self._current_page.append('')
+            self._count += len(self.seperator)
+
+    def close_page(self):
+        self._pages.append(self.prefix + "\n" + self.seperator.join(self._current_page) + "\n" + self.suffix)
+        self._current_page = []
+        self._count = len(self.prefix) + len(self.seperator)
+
+    @property
+    def pages(self):
+        if len(self._current_page) > 0:
+            self.close_page()
+        return self._pages
 
