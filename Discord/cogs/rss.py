7c47e45ae4d6bf8043b35b04358dc9092238e7f2
Harmon <Harmon758@gmail.com>
2021-09-23 03:39:12 -0500

    [Discord] Add handling for invalid URL schemes in RSS feeds

--- Discord/cogs/rss.py
+++ Discord/cogs/rss.py
@@ -287,7 +287,8 @@ class RSS(commands.Cog):
 								if e.status == 400 and e.code == 50035:
 									if ("In embed.url: Not a well formed URL." in e.text or  # still necessary?
 										"In embeds.0.url: Not a well formed URL." in e.text or
-										("In embed.url: Scheme" in e.text and 
+										(("In embed.url: Scheme" in e.text or  #still necessary?
+											"In embeds.0.url: Scheme" in e.text) and 
 											"is not supported. Scheme must be one of ('http', 'https')." in e.text)):
 										embed.url = discord.Embed.Empty
 									if ("In embed.thumbnail.url: Not a well formed URL." in e.text or 
