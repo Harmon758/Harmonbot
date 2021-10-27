7cefc214d331b99e75c5656d0378e30db7d4ea79
Harmon <Harmon758@gmail.com>
2021-10-27 01:37:16 -0500

    [Discord] Use error emoji Bot attribute in chess command

--- Discord/cogs/chess.py
+++ Discord/cogs/chess.py
@@ -101,7 +101,7 @@ class ChessCog(commands.Cog, name = "Chess"):
 
 		if opponent != ctx.bot.user and self.get_match(ctx.channel, opponent):
 			return await ctx.embed_reply(
-				":no_entry: Your chosen opponent is playing a chess match here"
+				f"{ctx.bot.error_emoji} Your chosen opponent is playing a chess match here"
 			)
 
 		if opponent == ctx.author:
