b80884fbbe84bdc9dfe5474f61b460d88942459f
Harmon <Harmon758@gmail.com>
2022-01-25 03:50:00 -0600

    [Discord] Improve word command formatting

--- Discord/cogs/random.py
+++ Discord/cogs/random.py
@@ -551,5 +551,7 @@ class Random(commands.Cog):
 	@commands.command()
 	async def word(self, ctx):
 		'''Random word'''
-		await ctx.embed_reply(self.bot.wordnik_words_api.getRandomWord().word.capitalize())
+		await ctx.embed_reply(
+			self.bot.wordnik_words_api.getRandomWord().word.capitalize()
+		)
 
