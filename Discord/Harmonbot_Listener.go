7a2fc38f2c0220affc64f1a2629539c2b660b2f9
Harmon <Harmon758@gmail.com>
2022-03-08 22:38:22 -0600

    [Discord Listener] Normalize formatting

--- Discord/Harmonbot_Listener.go
+++ Discord/Harmonbot_Listener.go
@@ -1,28 +1,31 @@
 package main
 
-import "github.com/bwmarrin/dgvoice"
-import "github.com/bwmarrin/discordgo"
-import "github.com/joho/godotenv"
+import (
+	"bufio"
+	"bytes"
+	"encoding/base64"
+	"encoding/binary"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"runtime"
+	"strings"
 
-import "bufio"
-import "bytes"
-import "encoding/base64"
-import "encoding/binary"
-import "io/ioutil"
-import "fmt"
-import "os"
-import "runtime"
-import "strings"
+	"github.com/bwmarrin/dgvoice"
+	"github.com/bwmarrin/discordgo"
+	"github.com/joho/godotenv"
+)
 
 const owner_id string = "115691005197549570"
 
-var _continue		chan bool
-var _listen			bool
-var dgv				*discordgo.VoiceConnection
-var recv			chan *discordgo.Packet
-var send			chan []int16
-var me				*discordgo.User
-var token			string
+var _continue chan bool
+var _listen bool
+var dgv *discordgo.VoiceConnection
+var recv chan *discordgo.Packet
+var send chan []int16
+var me *discordgo.User
+var token string
+
 // var response		*discordgo.Message
 
 func check(e error) {
@@ -34,42 +37,42 @@ func check(e error) {
 func main() {
 	fmt.Println("Starting up Discord Harmonbot Listener...")
 	_continue = make(chan bool)
-	
+
 	// Load Credentials
 	godotenv.Load("../.env")
 	token = os.Getenv("DISCORD_LISTENER_BOT_TOKEN")
-	
+
 	// Connect to Discord
 	dg, err := discordgo.New(token)
 	check(err)
-	
+
 	// Register ready as a callback for the ready events.
 	dg.AddHandler(ready)
 	// Register messageCreate as a callback for the messageCreate events.
 	dg.AddHandler(messageCreate)
-	
+
 	// Open Websocket
 	err = dg.Open()
 	check(err)
-	
+
 	me, err = dg.User("@me")
 	check(err)
-	
+
 	fmt.Printf("Started up %s#%s (%s)\n", me.Username, me.Discriminator, me.ID)
-	
+
 	if os.Getenv("CI") == "" && os.Getenv("GITHUB_ACTION") == "" {
-		<- _continue
+		<-_continue
 	}
-	
+
 	fmt.Println("Shutting down Discord Harmonbot Listener...")
-	
+
 	// Close connections
 	if dgv != nil {
 		dgv.Close()
 	}
 	dg.Logout()
 	dg.Close()
-	
+
 	return
 }
 
@@ -80,51 +83,51 @@ func ready(s *discordgo.Session, event *discordgo.Ready) {
 func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 	var err error
 	switch m.Content {
-		case ">test":
-			s.ChannelMessageSend(m.ChannelID, "Hello, World!")
-		case "!help":
-			s.ChannelMessageSend(m.ChannelID, "My prefix is `>`\nSee `>help`")
-		case ">help":
-			s.ChannelMessageSend(m.ChannelID, "WIP\n```>test\n>join (voice channel ID)\n>leave\n>listen\n>stoplistening [>stopl]\n>restart```")
-		case ">listen":
-			if dgv == nil {
-				s.ChannelMessageSend(m.ChannelID, "I'm not in a voice channel\nUse `>join (voice channel ID)`")
-			} else {
-				_listen = true
-				s.ChannelMessageSend(m.ChannelID, ":ear::skin-tone-2: I'm listening..")
-				Listen(dgv)
-				s.ChannelMessageSend(m.ChannelID, ":stop_sign: I stopped listening")
-				// s.ChannelMessageEdit(m.ChannelID, response.ID, "I stopped listening")
-			}
-		case ">stopl", ">stoplistening":
-			// response, err = s.ChannelMessageSend(m.ChannelID, "Processing..")
-			// check(err)
-			_listen = false
-		case ">leave":
-			err = dgv.Disconnect()
-			check(err)
-			dgv = nil
-			s.ChannelMessageSend(m.ChannelID, ":door: I've left the voice channel")
-		case ">restart":
-			// if m.Author.ID == owner_id {
-			s.ChannelMessageSend(m.ChannelID, ":ok_hand::skin-tone-2: Restarting...")
-			close(_continue)
-			// }
-		case ">updateavatar":
-			if m.Author.ID == owner_id {
-				changeAvatar(s)
-				s.ChannelMessageSend(m.ChannelID, "Avatar Updated")
-			}
-		case ">version go":
-			s.ChannelMessageSend(m.ChannelID, runtime.Version())
+	case ">test":
+		s.ChannelMessageSend(m.ChannelID, "Hello, World!")
+	case "!help":
+		s.ChannelMessageSend(m.ChannelID, "My prefix is `>`\nSee `>help`")
+	case ">help":
+		s.ChannelMessageSend(m.ChannelID, "WIP\n```>test\n>join (voice channel ID)\n>leave\n>listen\n>stoplistening [>stopl]\n>restart```")
+	case ">listen":
+		if dgv == nil {
+			s.ChannelMessageSend(m.ChannelID, "I'm not in a voice channel\nUse `>join (voice channel ID)`")
+		} else {
+			_listen = true
+			s.ChannelMessageSend(m.ChannelID, ":ear::skin-tone-2: I'm listening..")
+			Listen(dgv)
+			s.ChannelMessageSend(m.ChannelID, ":stop_sign: I stopped listening")
+			// s.ChannelMessageEdit(m.ChannelID, response.ID, "I stopped listening")
+		}
+	case ">stopl", ">stoplistening":
+		// response, err = s.ChannelMessageSend(m.ChannelID, "Processing..")
+		// check(err)
+		_listen = false
+	case ">leave":
+		err = dgv.Disconnect()
+		check(err)
+		dgv = nil
+		s.ChannelMessageSend(m.ChannelID, ":door: I've left the voice channel")
+	case ">restart":
+		// if m.Author.ID == owner_id {
+		s.ChannelMessageSend(m.ChannelID, ":ok_hand::skin-tone-2: Restarting...")
+		close(_continue)
+		// }
+	case ">updateavatar":
+		if m.Author.ID == owner_id {
+			changeAvatar(s)
+			s.ChannelMessageSend(m.ChannelID, "Avatar Updated")
+		}
+	case ">version go":
+		s.ChannelMessageSend(m.ChannelID, runtime.Version())
 	}
 	if strings.HasPrefix(m.Content, ">join") {
-		
+
 		if recv != nil || send != nil {
 			s.ChannelMessageSend(m.ChannelID, ":warning: Error: Please `>restart` me to have me rejoin")
 			return
 		}
-		
+
 		channel, err := s.Channel(m.ChannelID)
 		check(err)
 		if len(strings.Split(m.Content, " ")) == 1 {
@@ -136,48 +139,48 @@ func messageCreate(s *discordgo.Session, m *discordgo.MessageCreate) {
 			s.ChannelMessageSend(m.ChannelID, ":warning: Error")
 			return
 		}
-		
+
 		recv = make(chan *discordgo.Packet, 2)
 		go dgvoice.ReceivePCM(dgv, recv)
 		send = make(chan []int16, 2)
 		go dgvoice.SendPCM(dgv, send)
-		
+
 		s.ChannelMessageSend(m.ChannelID, "I've joined the voice channel")
 	}
 }
 
 func Listen(v *discordgo.VoiceConnection) {
 	// fmt.Println("Echoing")
-	
+
 	fo, err := os.Create("data/temp/heard.pcm")
 	check(err)
-	
+
 	// v.Speaking(true)
 	// defer v.Speaking(false)
-	
+
 	w := bufio.NewWriter(fo)
 	buf := new(bytes.Buffer)
-	
+
 	for _listen {
 		p, ok := <-recv
 		if !ok {
 			return
 		}
-		
+
 		// echo for debugging
 		// send <- p.PCM
 		err := binary.Write(buf, binary.LittleEndian, p.PCM)
 		check(err)
 	}
-	
+
 	_, err = w.Write(buf.Bytes())
 	check(err)
-	
+
 	err = w.Flush()
 	check(err)
-	
+
 	fo.Sync()
-	
+
 	// fmt.Println("Stopping")
 }
 
