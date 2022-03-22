0b351bc5dc81b48aa13be9b76712d4b13095053a
Harmon <Harmon758@gmail.com>
2022-03-22 15:03:38 -0500

    [Discord] Disallow combining numbers as digits in 24 command

--- Discord/cogs/games.py
+++ Discord/cogs/games.py
@@ -121,6 +121,8 @@ class Games(commands.Cog):
 				return False
 			if any(character not in numbers + [' ', '+', '-', '*', '/', '(', ')'] for character in message.content):
 				return False
+			if len(re.split("\W+", message.content)) != 4:
+				return False
 			for number in set(numbers):
 				if message.content.count(number) != numbers.count(number):
 					return False
