5f656a934b5319e953f848556dc18e5daac09c3e
Harmon <Harmon758@gmail.com>
2022-04-13 06:11:56 -0500

    [Discord] Improve Context.embed_send formatting

--- Discord/utilities/context.py
+++ Discord/utilities/context.py
@@ -25,16 +25,23 @@ class Context(commands.Context):
 		return message
 	
 	# TODO: optimize/improve clarity
-	async def embed_send(self, description = None, *args, 
-							title = None, title_url = None, 
-							author_name = "", author_url = None, author_icon_url = None, 
-							image_url = None, thumbnail_url = None, 
-							footer_text = None, footer_icon_url = None, 
-							timestamp = None, fields = [], color = None, **kwargs):
-		embed = discord.Embed(title = title, url = title_url, timestamp = timestamp, color = color or self.bot.bot_color)
+	async def embed_send(
+		self, description = None, *args, title = None, title_url = None,
+		author_name = "", author_url = None, author_icon_url = None,
+		image_url = None, thumbnail_url = None, footer_text = None,
+		footer_icon_url = None, timestamp = None, fields = [], color = None,
+		**kwargs
+	):
+		embed = discord.Embed(
+			title = title, url = title_url, timestamp = timestamp,
+			color = color or self.bot.bot_color
+		)
 		embed.description = str(description) if description else None
 		if author_name:
-			embed.set_author(name = author_name, url = author_url, icon_url = author_icon_url)
+			embed.set_author(
+				name = author_name, url = author_url,
+				icon_url = author_icon_url
+			)
 		if image_url:
 			embed.set_image(url = image_url)
 		if thumbnail_url:
@@ -42,13 +49,30 @@ class Context(commands.Context):
 		embed.set_footer(text = footer_text, icon_url = footer_icon_url)
 		for field in fields:
 			if len(field) >= 3:
-				embed.add_field(name = field[0], value = field[1], inline = field[2])
+				embed.add_field(
+					name = field[0], value = field[1], inline = field[2]
+				)
 			else:
 				embed.add_field(name = field[0], value = field[1])
-		if self.channel.type is discord.ChannelType.private or getattr(self.channel.permissions_for(self.channel.guild.me), "embed_links", None):
+		if (
+			self.channel.type is discord.ChannelType.private or
+			getattr(
+				self.channel.permissions_for(self.channel.guild.me),
+				"embed_links", None
+			)
+		):
 			return await self.send(*args, embed = embed, **kwargs)
-		elif not (title or title_url or image_url or thumbnail_url or footer_icon_url or timestamp or fields) and (not footer_text or footer_text.startswith("In response to")):
-			return await self.reply(discord.utils.escape_mentions(str(description)))
+		elif (
+			not (
+				title or title_url or image_url or thumbnail_url or
+				footer_icon_url or timestamp or fields
+			) and (
+				not footer_text or footer_text.startswith("In response to")
+			)
+		):
+			return await self.reply(
+				discord.utils.escape_mentions(str(description))
+			)
 			# TODO: Clean role + user mentions, etc.?
 		else:
 			raise commands.BotMissingPermissions(["embed_links"])
