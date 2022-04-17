8d18af76a0faaa6265d633f0df19365b513a197c
Harmon <Harmon758@gmail.com>
2022-04-17 06:42:48 -0500

    [Discord] Update blackjack command to use buttons

--- Discord/cogs/blackjack.py
+++ Discord/cogs/blackjack.py
@@ -1,4 +1,5 @@
 
+from discord import ui
 from discord.ext import commands
 
 import asyncio
@@ -31,79 +32,63 @@ class Blackjack(commands.Cog):
         # TODO: S17
         game = BlackjackGame()
 
+        view = BlackjackView(game = game)
         response = await ctx.embed_reply(
             title = "Blackjack",
             description = (
                 f"Dealer: {game.dealer_string} (?)"
                 f"\n{ctx.author.display_name}: {game.player_string} ({game.player_total})\n"
             ),
-            footer_text = "Hit or Stay?"
+            footer_text = "Hit or Stay?",
+            view = view
         )
         embed = response.embeds[0]
 
-        while True:
-            action = await ctx.bot.wait_for(
-                "message",
-                check = (lambda message: (
-                    message.author == ctx.author and
-                    message.content.lower().strip('!') in ("hit", "stay")
-                ))
+        view.message = response
+        ctx.bot.views.append(view)
+        await view.wait()
+
+        if game.player_total <= 21:
+            game.dealer_turn = True
+
+            embed.description = (
+                f"Dealer: {game.dealer_string} ({game.dealer_total})\n"
+                f"{ctx.author.display_name}: {game.player_string} ({game.player_total})\n"
             )
-            await ctx.bot.attempt_delete_message(action)
-
-            if action.content.lower().strip('!') == "hit":
-                game.player_hit()
-
-                embed.description = (
-                    f"Dealer: {game.dealer_string} (?)\n"
-                    f"{ctx.author.display_name}: {game.player_string} ({game.player_total})\n"
-                )
+            if game.dealer_total > 21:
+                embed.description += ":boom: The dealer busted"
+                embed.set_footer(text = "You win!")
                 await response.edit(embed = embed)
+                return
+            elif game.dealer_total > game.player_total:
+                embed.description += "The dealer beat you"
+                embed.set_footer(text = "You lost :(")
+                await response.edit(embed = embed)
+                return
+            embed.set_footer(text = "Dealer's turn..")
+            await response.edit(embed = embed)
 
-                if game.player_total > 21:
-                    embed.description += ":boom: You have busted"
-                    embed.set_footer(text = "You lost :(")
-                    break
-            else:
-                game.dealer_turn = True
+            while game.dealer_total < 21 and game.dealer_total <= game.player_total:
+                await asyncio.sleep(5)
+
+                game.dealer_hit()
 
                 embed.description = (
                     f"Dealer: {game.dealer_string} ({game.dealer_total})\n"
                     f"{ctx.author.display_name}: {game.player_string} ({game.player_total})\n"
                 )
-                if game.dealer_total > 21:
-                    embed.description += ":boom: The dealer busted"
-                    embed.set_footer(text = "You win!")
-                    break
-                elif game.dealer_total > game.player_total:
-                    embed.description += "The dealer beat you"
-                    embed.set_footer(text = "You lost :(")
-                    break
-                embed.set_footer(text = "Dealer's turn..")
                 await response.edit(embed = embed)
 
-                while game.dealer_total < 21 and game.dealer_total <= game.player_total:
-                    await asyncio.sleep(5)
+            if game.dealer_total > 21:
+                embed.description += ":boom: The dealer busted"
+                embed.set_footer(text = "You win!")
+            elif game.dealer_total > game.player_total:
+                embed.description += "The dealer beat you"
+                embed.set_footer(text = "You lost :(")
+            elif game.dealer_total == game.player_total == 21:
+                embed.set_footer(text = "It's a push (tie)")
 
-                    game.dealer_hit()
-
-                    embed.description = (
-                        f"Dealer: {game.dealer_string} ({game.dealer_total})\n"
-                        f"{ctx.author.display_name}: {game.player_string} ({game.player_total})\n"
-                    )
-                    await response.edit(embed = embed)
-
-                if game.dealer_total > 21:
-                    embed.description += ":boom: The dealer busted"
-                    embed.set_footer(text = "You win!")
-                elif game.dealer_total > game.player_total:
-                    embed.description += "The dealer beat you"
-                    embed.set_footer(text = "You lost :(")
-                elif game.dealer_total == game.player_total == 21:
-                    embed.set_footer(text = "It's a push (tie)")
-                break
-
-        await response.edit(embed = embed)
+            await response.edit(embed = embed)
 
 
 class BlackjackGame:
@@ -150,3 +135,50 @@ def calculate_total(cards):
 
 def cards_to_string(cards):
     return "".join(f":{card.suit.lower()}: {card.value} " for card in cards)
+
+
+class BlackjackView(ui.View):
+
+    def __init__(self, *, game):
+        super().__init__(timeout = None)
+
+        self.game = game
+
+        self.message = None
+
+    @ui.button(label = "Hit")
+    async def hit(self, interaction, button):
+        self.game.player_hit()
+
+        embed = interaction.message.embeds[0]
+        embed.description = (
+            f"Dealer: {self.game.dealer_string} (?)\n"
+            f"{interaction.user.display_name}: {self.game.player_string} ({self.game.player_total})\n"
+        )
+
+        if self.game.player_total > 21:
+            embed.description += ":boom: You have busted"
+            embed.set_footer(text = "You lost :(")
+            await self.stop(interaction = interaction, embed = embed)
+        else:
+            await interaction.response.edit_message(embed = embed)
+
+    @ui.button(label = "Stay")
+    async def stay(self, interaction, button):
+        await self.stop(interaction = interaction)
+
+    async def stop(self, *, interaction = None, embed = None):
+        self.hit.disabled = True
+        self.stay.disabled = True
+
+        if interaction:
+            if embed:
+                await interaction.response.edit_message(
+                    embed = embed, view = self
+                )
+            else:
+                await interaction.response.edit_message(view = self)
+        elif self.message:
+            await self.bot.attempt_edit_message(self.message, view = self)
+
+        super().stop()
