02963598909036bd7da1cd91cf6781c5ac41efff
Harmon <Harmon758@gmail.com>
2022-06-10 08:38:45 -0500

    [Discord] Normalize google command docstring quotation mark type

--- Discord/cogs/search.py
+++ Discord/cogs/search.py
@@ -108,7 +108,7 @@ class Search(commands.Cog):
 	
 	@commands.group(case_insensitive = True, invoke_without_command = True)
 	async def google(self, ctx, *, search: str):
-		'''Google search'''
+		"""Google search"""
 		await ctx.embed_reply(
 			f"[Google search for \"{search}\"]"
 			f"(https://www.google.com/search?q={search.replace(' ', '+')})"
