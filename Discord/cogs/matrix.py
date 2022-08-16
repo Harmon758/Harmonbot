37a194ec75366ace0823dca98b1c0109ee18e4cf
Harmon <Harmon758@gmail.com>
2022-08-16 14:16:17 -0500

    [Discord] Improve matrix tan command consistency

--- Discord/cogs/matrix.py
+++ Discord/cogs/matrix.py
@@ -148,7 +148,7 @@ class Matrix(commands.Cog):
 			str(numpy.matrix(matrix_a) - numpy.matrix(matrix_b))
 		)
 	
-	@matrix.group(aliases = ["tangent"], invoke_without_command = True, case_insensitive = True)
+	@matrix.group(aliases = ["tangent"], case_insensitive = True, invoke_without_command = True)
 	async def tan(self, ctx, *, matrix: Matrix):
 		"""Tangent of a matrix"""
 		await ctx.embed_reply(str(scipy.linalg.tanm(matrix)))
