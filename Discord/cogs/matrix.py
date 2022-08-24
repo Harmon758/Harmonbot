3b71ed36d5444da1f7020d46862d46a848dd9e1c
Harmon <Harmon758@gmail.com>
2022-08-24 14:43:33 -0500

    [Discord] Improve matrix transpose command consistency

--- Discord/cogs/matrix.py
+++ Discord/cogs/matrix.py
@@ -161,7 +161,7 @@ class Matrix(commands.Cog):
 		"""Hyperbolic tangent of a matrix"""
 		await ctx.embed_reply(str(scipy.linalg.tanhm(matrix)))
 	
-	@matrix.group(aliases = ["transposition"], invoke_without_command = True, case_insensitive = True)
+	@matrix.group(aliases = ["transposition"], case_insensitive = True, invoke_without_command = True)
 	async def transpose(self, ctx, *, matrix: Matrix):
 		"""Transpose of a matrix"""
 		await ctx.embed_reply(str(numpy.matrix(matrix).T))
