e60ba48ed41c375f1bdd32bfe6df6076f2a5b543
Harmon <Harmon758@gmail.com>
2022-09-23 15:34:47 -0500

    [Units] Handle wrong encoding for trivia answers

--- units/trivia.py
+++ units/trivia.py
@@ -35,6 +35,8 @@ def check_answer(answer, response, inflect_engine = None):
             answer = answer.replace(character, "")
         if character in response:
             response = response.replace(character, "")
+    # Fix wrong encoding
+    answer = answer.encode("ISO-8859-1").decode("UTF-8")
     # Remove diacritics
     answer = "".join(
         character for character in unicodedata.normalize("NFD", answer) 
