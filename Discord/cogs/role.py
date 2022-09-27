33615f859533e8147850aaca27a60b08165fac34
Harmon <Harmon758@gmail.com>
2022-09-26 20:20:22 -0500

    [Discord] Change role command to hybrid group command

--- Discord/cogs/role.py
+++ Discord/cogs/role.py
@@ -20,7 +20,7 @@ class Role(commands.Cog):
 		not_forbidden = await checks.not_forbidden().predicate(ctx)
 		return guild_only and not_forbidden
 	
-	@commands.group(aliases = ["roles"], invoke_without_command = True, case_insensitive = True)
+	@commands.hybrid_group(aliases = ["roles"], case_insensitive = True)
 	async def role(self, ctx):
 		"""Role"""
 		await ctx.send_help(ctx.command)
@@ -28,7 +28,7 @@ class Role(commands.Cog):
 	# TODO: check role hierarchy
 	# TODO: reason options?
 	
-	@role.command(aliases = ["colour"])
+	@role.command(aliases = ["colour"], with_app_command = False)
 	async def color(self, ctx, role: discord.Role, *, color: discord.Color = None):
 		'''The color of a role'''
 		if color:
@@ -39,7 +39,7 @@ class Role(commands.Cog):
 		else:
 			await ctx.embed_reply(role.mention + "'s color is {}".format(role.color))
 	
-	@role.command(aliases = ["make", "new"])
+	@role.command(aliases = ["make", "new"], with_app_command = False)
 	@commands.bot_has_guild_permissions(manage_roles = True)
 	@commands.check_any(commands.has_guild_permissions(manage_roles = True), commands.is_owner())
 	async def create(self, ctx, *, name: str = ""):
@@ -48,12 +48,12 @@ class Role(commands.Cog):
 		role = await ctx.guild.create_role(name = name)
 		await ctx.embed_reply(role.mention + " created")
 	
-	@role.command()
+	@role.command(with_app_command = False)
 	async def default(self, ctx, *, role: discord.Role):
 		'''Whether a role is the default role or not'''
 		await ctx.embed_reply(role.mention + " is {}the default role".format("" if role.is_default() else "not "))
 	
-	@role.command(aliases = ["hoist"])
+	@role.command(aliases = ["hoist"], with_app_command = False)
 	async def hoisted(self, ctx, role: discord.Role, hoist: bool = None):
 		'''Whether a role is displayed separately from other members or not'''
 		if hoist is not None:
@@ -64,12 +64,12 @@ class Role(commands.Cog):
 		else:
 			await ctx.embed_reply(role.mention + " is {}hoisted".format("" if role.hoist else "not "))
 	
-	@role.command()
+	@role.command(with_app_command = False)
 	async def id(self, ctx, *, role: discord.Role):
 		'''The ID of a role'''
 		await ctx.embed_reply(role.id)
 	
-	@role.command(aliases = ["info"])
+	@role.command(aliases = ["info"], with_app_command = False)
 	async def information(self, ctx, *, role: discord.Role):
 		"""Information about a role"""
 		if command := ctx.bot.get_command("information role"):
@@ -80,12 +80,12 @@ class Role(commands.Cog):
 				"when role information command invoked"
 			)
 	
-	@role.command()
+	@role.command(with_app_command = False)
 	async def managed(self, ctx, *, role: discord.Role):
 		'''Indicates if the role is managed by the guild through some form of integrations such as Twitch'''
 		await ctx.embed_reply(role.mention + " is {}managed".format("" if role.managed else "not "))
 	
-	@role.command()
+	@role.command(with_app_command = False)
 	async def mentionable(self, ctx, role: discord.Role, mentionable: bool = None):
 		'''Whether a role is mentionable or not'''
 		if mentionable is not None:
@@ -96,7 +96,7 @@ class Role(commands.Cog):
 		else:
 			await ctx.embed_reply(role.mention + " is {}mentionable".format("" if role.mentionable else "not "))
 	
-	@role.command()
+	@role.command(with_app_command = False)
 	async def name(self, ctx, role: discord.Role, *, name: str = ""):
 		'''The name of a role'''
 		if name:
@@ -107,7 +107,7 @@ class Role(commands.Cog):
 		else:
 			await ctx.embed_reply(role.name)
 	
-	@role.command()
+	@role.command(with_app_command = False)
 	async def position(self, ctx, role: discord.Role, position: int = None):
 		'''
 		The position of a role
@@ -123,7 +123,7 @@ class Role(commands.Cog):
 			await ctx.embed_reply(role.mention + "'s position is {}".format(role.position))
 	
 	# TODO: move to server cog
-	@role.command()
+	@role.command(with_app_command = False)
 	async def positions(self, ctx):
 		'''
 		WIP
