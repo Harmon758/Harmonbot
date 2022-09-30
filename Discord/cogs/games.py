af82308e82a89697ab9d0d77d28f0c6e1d5b464f
Harmon <Harmon758@gmail.com>
2022-09-30 11:52:36 -0500

    [Discord] Set default for question parameter for 8-ball command

--- Discord/cogs/games.py
+++ Discord/cogs/games.py
@@ -94,7 +94,7 @@ class Games(commands.Cog):
 		name = "8-ball", aliases = ["8ball", "eightball", '\N{BILLIARDS}']
 	)
 	@checks.not_forbidden()
-	async def eightball(self, ctx, *, question: Optional[str]):
+	async def eightball(self, ctx, *, question: Optional[str] = None):
 		"""
 		Ask 8-ball a yes or no question
 		Also triggers on \N{BILLIARDS} without prefix
