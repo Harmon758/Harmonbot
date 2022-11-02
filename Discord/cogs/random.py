c9de5bc1748b8d27eacf135aebefbc3eccc5e098
Harmon <Harmon758@gmail.com>
2022-11-02 05:00:34 -0500

    [Discord] Improve random fact command clarity

--- Discord/cogs/random.py
+++ Discord/cogs/random.py
@@ -273,7 +273,8 @@ class Random(commands.GroupCog, group_name = "random"):
 		# uses page, limit, and cb parameters, seemingly to no effect
 		async with ctx.bot.aiohttp_session.get(url) as resp:
 			if resp.status == 503:
-				return await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: API Service Unavailable")
+				await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: API Service Unavailable")
+				return
 			data = await resp.json(content_type = "text/plain")
 		await ctx.embed_reply(BeautifulSoup(data[0]["fact"], "lxml").text, 
 								image_url = data[0]["primaryImage"])
