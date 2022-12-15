fb130ab76df86541001c10e27add07bca6ce965b
Harmon <Harmon758@gmail.com>
2022-12-15 04:45:59 -0600

    [Discord] Mention active trivia question on max concurrency reached
    Mention active trivia question response on max concurrency reached
    Optimize Trivia.cog_command_error

--- Discord/cogs/trivia.py
+++ Discord/cogs/trivia.py
@@ -56,19 +56,18 @@ class Trivia(commands.Cog):
 	
 	async def cog_command_error(self, ctx, error):
 		if isinstance(error, commands.MaxConcurrencyReached):
-			if ctx.guild.id in self.trivia_questions:
-				game = "trivia"
-				channel_id = self.trivia_questions[ctx.guild.id].channel_id
+			if trivia_question := self.trivia_questions[ctx.guild.id]:
+				await ctx.embed_reply(f"[There's already an active trivia question here]({trivia_question.response.jump_url})")
+				return
 			elif ctx.guild.id in self.active_jeopardy:
-				game = "jeopardy"
 				channel_id = self.active_jeopardy[ctx.guild.id].channel_id
+				if ctx.channel.id == channel_id:
+					return await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: There is already an ongoing game of jeopardy here")
+				else:
+					channel = ctx.guild.get_channel_or_thread(channel_id)
+					return await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: There is already an ongoing game of jeopardy in {channel.mention}")
 			else:
 				raise RuntimeError("Trivia max concurrency reached, but neither active trivia nor jeopardy found.")
-			if ctx.channel.id == channel_id:
-				return await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: There is already an ongoing game of {game} here")
-			else:
-				channel = ctx.guild.get_channel_or_thread(channel_id)
-				return await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: There is already an ongoing game of {game} in {channel.mention}")
 	
 	@commands.group(
 		case_insensitive = True, invoke_without_command = True,
