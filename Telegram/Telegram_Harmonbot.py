012f070464ab8200d3322841e25e909c7062d761
Harmon <Harmon758@gmail.com>
2023-01-04 04:27:07 -0600

    [Telegram] Update python-telegram-bot usage

--- Telegram/Telegram_Harmonbot.py
+++ Telegram/Telegram_Harmonbot.py
@@ -2,12 +2,13 @@
 import telegram
 import telegram.ext
 
+import asyncio
 import datetime
 import os
 
 import dotenv
 
-version = "0.2.2"
+version = "0.3.0"
 
 # TODO: set up logging and/or make Beta bot for CI
 
@@ -15,22 +16,13 @@ version = "0.2.2"
 dotenv.load_dotenv()
 token = os.getenv("TELEGRAM_BOT_API_TOKEN")
 
-bot = telegram.Bot(token = token)
-updater = telegram.ext.Updater(token = token)
+async def test(update, context):
+	await context.bot.sendMessage(chat_id = update.message.chat_id, text = "Hello, World!")
 
-def test(update, context):
-	context.bot.sendMessage(chat_id = update.message.chat_id, text = "Hello, World!")
+async def ping(update, context):
+	await context.bot.sendMessage(chat_id = update.message.chat_id, text = "pong")
 
-def ping(update, context):
-	context.bot.sendMessage(chat_id = update.message.chat_id, text = "pong")
-
-test_handler = telegram.ext.CommandHandler("test", test)
-updater.dispatcher.add_handler(test_handler)
-
-ping_handler = telegram.ext.CommandHandler("ping", ping)
-updater.dispatcher.add_handler(ping_handler)
-
-def error_handler(update, context):
+async def error_handler(update, context):
 	if isinstance(context.error, telegram.error.Conflict):
 		print(f"Conflict @ {datetime.datetime.now().isoformat()}")  # probably CI
 	elif isinstance(context.error, telegram.error.NetworkError):
@@ -38,13 +30,33 @@ def error_handler(update, context):
 	else:
 		raise context.error
 
-updater.dispatcher.add_error_handler(error_handler)
+async def post_init(application):
+	asyncio.create_task(post_start(application), name = "post_start")
 
-updater.start_polling()
+async def post_start(application):
+	while not application.updater.running:
+		await asyncio.sleep(1)
 
-bot_info = bot.getMe()
-print(f"Started up Telegram Harmonbot ({bot_info['username']}) ({bot_info['id']})")
+	bot = telegram.Bot(token = token)
+	bot_info = await bot.getMe()
+	print(f"Started up Telegram Harmonbot ({bot_info['username']}) ({bot_info['id']})")
 
-if os.getenv("CI") or os.getenv("GITHUB_ACTION"):
-	updater.stop()
+	if os.getenv("CI") or os.getenv("GITHUB_ACTION"):
+		application.stop()
+
+def main():
+	application = telegram.ext.Application.builder().token(token).post_init(post_init).build()
+
+	test_handler = telegram.ext.CommandHandler("test", test)
+	application.add_handler(test_handler)
+
+	ping_handler = telegram.ext.CommandHandler("ping", ping)
+	application.add_handler(ping_handler)
+
+	application.add_error_handler(error_handler)
+
+	application.run_polling()
+
+if __name__ == "__main__":
+	main()
 
