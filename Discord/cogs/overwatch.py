6c8800defd5f50171cd8cbf79a8f04d61b479d19
Harmon <Harmon758@gmail.com>
2023-01-25 23:18:28 -0600

    [Discord] Normalize overwatch subcommand docstring quotation mark type
    Normalize overwatch achievement command docstring quotation mark type

--- Discord/cogs/overwatch.py
+++ Discord/cogs/overwatch.py
@@ -40,12 +40,12 @@ class Overwatch(commands.Cog):
 	
 	@overwatch.command(hidden = True)
 	async def achievement(self, ctx):
-		'''
+		"""
 		Overwatch Achievements
 		Deprecated, as the API this command used to use does not exist anymore
 		https://overwatch-api.net/
 		https://github.com/jamesmcfadden/overwatch-api
-		'''
+		"""
 		await ctx.send_help(ctx.command)
 	
 	@overwatch.command(hidden = True)
