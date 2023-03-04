55c4d100bdef28e9418b3e2d069341102ea40537
Harmon <Harmon758@gmail.com>
2023-03-03 22:48:26 -0600

    [Discord] Improve decode gost 34.12-2015 command clarity

--- Discord/cogs/cryptography.py
+++ Discord/cogs/cryptography.py
@@ -114,9 +114,11 @@ class Cryptography(commands.Cog):
         '''
         # TODO: Add decode kuznyechik alias
         if len(key) < 32:
-            return await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: key length must be at least 32")
+            await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: key length must be at least 32")
+            return
         if len(data) < 16:
-            return await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: data length must be at least 16")
+            await ctx.embed_reply(f"{ctx.bot.error_emoji} Error: data length must be at least 16")
+            return
         await ctx.embed_reply(pygost.gost3412.GOST3412Kuznechik(key.encode("UTF-8")).decrypt(bytearray.fromhex(data)).decode("UTF-8"))
 
     @decode.command(name = "morse")
