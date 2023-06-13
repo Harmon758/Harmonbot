4844ed86cfe52e4029b55db7909dbd815dcc78ff
Harmon <Harmon758@gmail.com>
2023-06-12 22:38:18 -0500

    [Discord] Add Jamspace to activity command

--- Discord/cogs/discord.py
+++ Discord/cogs/discord.py
@@ -24,6 +24,7 @@ ACTIVITES = {
 	# "Doodle Crew": 878067389634314250,  # Now Sketch Hands
 	"Fishington.io": 814288819477020702,
 	"Gartic Phone": 1007373802981822582,
+	"Jamspace": 1070087967294631976,
 	"Know What I Meme": 950505761862189096,
 	"Land-io": 903769130790969345,
 	"Letter League": 879863686565621790,
@@ -64,10 +65,10 @@ class Discord(commands.Cog):
 		activity: Literal[
 			"Ask Away", "Bash Out", "Betrayal.io", "Blazing 8s",
 			"Bobble League", "Checkers In The Park", "Chess In The Park",
-			"Doodle Crew", "Fishington.io", "Gartic Phone", "Know What I Meme",
-			"Land-io", "Letter League", "Ocho", "Poker Night", "Putt Party",
-			"Sketch Hands", "SpellCast", "Watch Together", "Word Snacks",
-			"YouTube Together"
+			"Doodle Crew", "Fishington.io", "Gartic Phone", "Jamspace",
+			"Know What I Meme", "Land-io", "Letter League", "Ocho",
+			"Poker Night", "Putt Party", "Sketch Hands", "SpellCast",
+			"Watch Together", "Word Snacks", "YouTube Together"
 		]
 	):
 		"""
