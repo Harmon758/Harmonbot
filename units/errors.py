adcc7b82d27d0cce35b187c7c3d43f9e281180da
Harmon <Harmon758@gmail.com>
2023-06-17 15:40:49 -0500

    [Units] Remove unnecessary passes in exceptions

--- units/errors.py
+++ units/errors.py
@@ -1,13 +1,10 @@
 
 class UnitError(Exception):
     '''Unit Error'''
-    pass
 
 class UnitExecutionError(UnitError):
     '''Unit Execution Error'''
-    pass
 
 class UnitOutputError(UnitError):
     '''Unit Output Error'''
-    pass
 
