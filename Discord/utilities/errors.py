5eb7d6ba4d7a4f1622ecabd6baf8d3fe84a518f9
Harmon <Harmon758@gmail.com>
2023-06-17 15:29:22 -0500

    [Discord] Remove unnecessary passes in exceptions

--- Discord/utilities/errors.py
+++ Discord/utilities/errors.py
@@ -3,25 +3,19 @@ from discord.ext.commands.errors import CheckFailure
 
 class NotGuildOwner(CheckFailure):
 	'''Not Guild Owner'''
-	pass
 
 class VoiceNotConnected(CheckFailure):
 	'''Voice Not Connected'''
-	pass
 
 class PermittedVoiceNotConnected(VoiceNotConnected):
 	'''Permitted, but Voice Not Connected'''
-	pass
 
 class NotPermittedVoiceNotConnected(VoiceNotConnected):
 	'''Voice Not Connected, and Not Permitted'''
-	pass
 
 class NotPermitted(CheckFailure):
 	'''Not Permitted'''
-	pass
 
 class AudioError(CheckFailure):
 	'''Audio Error'''
-	pass
 
