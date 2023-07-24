1165ff3decfeb4d7be763040d3b6edb73b774ee7
Harmon <Harmon758@gmail.com>
2023-07-24 04:08:58 -0500

    [Units] Allow not passing aiohttp session to get_monster_data function

--- units/runescape.py
+++ units/runescape.py
@@ -1,7 +1,7 @@
 
 import aiohttp
 
-from .errors import UnitExecutionError, UnitOutputError
+from .errors import UnitOutputError
 
 
 async def get_item_id(
@@ -73,20 +73,23 @@ async def get_ge_data(
 
 
 async def get_monster_data(monster, aiohttp_session = None):
-    if not aiohttp_session:
-        raise UnitExecutionError("aiohttp session required")
-        # TODO: Default aiohttp session?
-    async with aiohttp_session.get(
-        "http://services.runescape.com/m=itemdb_rs/bestiary/beastSearch.json",
-        params = {"term": monster}
-    ) as resp:
-        data = await resp.json(content_type = "text/html")
-    if "value" not in data[0]:
-        raise UnitOutputError("Monster not found")
-    async with aiohttp_session.get(
-        "http://services.runescape.com/m=itemdb_rs/bestiary/beastData.json",
-        params = {"beastid": data[0]["value"]}
-    ) as resp:
-        data = await resp.json(content_type = "text/html")
-    return data
+    if aiohttp_session_not_passed := (aiohttp_session is None):
+        aiohttp_session = aiohttp.ClientSession()
+    try:
+        async with aiohttp_session.get(
+            "http://services.runescape.com/m=itemdb_rs/bestiary/beastSearch.json",
+            params = {"term": monster}
+        ) as resp:
+            data = await resp.json(content_type = "text/html")
+        if "value" not in data[0]:
+            raise UnitOutputError("Monster not found")
+        async with aiohttp_session.get(
+            "http://services.runescape.com/m=itemdb_rs/bestiary/beastData.json",
+            params = {"beastid": data[0]["value"]}
+        ) as resp:
+            data = await resp.json(content_type = "text/html")
+        return data
+    finally:
+        if aiohttp_session_not_passed:
+            await aiohttp_session.close()
 
