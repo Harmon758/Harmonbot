6337ab72843a5cea3dba453c6ebfc610244099b4
Harmon <Harmon758@gmail.com>
2023-07-24 01:07:33 -0500

    [Units] Improve clarity of get_monster_data function

--- units/runescape.py
+++ units/runescape.py
@@ -47,15 +47,17 @@ async def get_monster_data(monster, aiohttp_session = None):
     if not aiohttp_session:
         raise UnitExecutionError("aiohttp session required")
         # TODO: Default aiohttp session?
-    url = "http://services.runescape.com/m=itemdb_rs/bestiary/beastSearch.json"
-    params = {"term": monster}
-    async with aiohttp_session.get(url, params = params) as resp:
+    async with aiohttp_session.get(
+        "http://services.runescape.com/m=itemdb_rs/bestiary/beastSearch.json",
+        params = {"term": monster}
+    ) as resp:
         data = await resp.json(content_type = "text/html")
     if "value" not in data[0]:
         raise UnitOutputError("Monster not found")
-    url = "http://services.runescape.com/m=itemdb_rs/bestiary/beastData.json"
-    params = {"beastid": data[0]["value"]}
-    async with aiohttp_session.get(url, params = params) as resp:
+    async with aiohttp_session.get(
+        "http://services.runescape.com/m=itemdb_rs/bestiary/beastData.json",
+        params = {"beastid": data[0]["value"]}
+    ) as resp:
         data = await resp.json(content_type = "text/html")
     return data
 
