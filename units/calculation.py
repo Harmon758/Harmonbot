5b84e9b5df639bc94ba10e57c32bd101928c145c
Harmon <Harmon758@gmail.com>
2023-07-30 02:34:49 -0500

    [Units] Improve formatting of calculation.py

--- units/calculation.py
+++ units/calculation.py
@@ -3,10 +3,11 @@ import operator
 
 from pyparsing import Forward, Group, Literal, nums, Suppress, Word
 
+
 expression_stack = []
 
 def push_first(tokens):
-	expression_stack.append(tokens[0])
+    expression_stack.append(tokens[0])
 
 """
 atom       :: '0'..'9'+ | '(' expression ')'
@@ -19,24 +20,24 @@ term = atom + ((Literal('*') | Literal('/')) + atom).setParseAction(push_first)[
 expression <<= term + ((Literal('+') | Literal('-')) + term).setParseAction(push_first)[...]
 
 operations = {
-	'+': operator.add,
-	'-': operator.sub,
-	'*': operator.mul,
-	'/': operator.truediv
+    '+': operator.add,
+    '-': operator.sub,
+    '*': operator.mul,
+    '/': operator.truediv
 }
 
 def evaluate_stack(stack):
-	token = stack.pop()
-	if token in "+-*/":
-		# Operands are pushed onto the stack in reverse order
-		operand_2 = evaluate_stack(stack)
-		operand_1 = evaluate_stack(stack)
-		return operations[token](operand_1, operand_2)
-	else:
-		return int(token)
+    token = stack.pop()
+    if token in "+-*/":
+        # Operands are pushed onto the stack in reverse order
+        operand_2 = evaluate_stack(stack)
+        operand_1 = evaluate_stack(stack)
+        return operations[token](operand_1, operand_2)
+    else:
+        return int(token)
 
 def calculate(input_string: str):
-	expression_stack.clear()
-	expression.parseString(input_string, parseAll=True)  # can raise pyparsing.ParseException
-	return evaluate_stack(expression_stack)  # can raise ZeroDivisionError
+    expression_stack.clear()
+    expression.parseString(input_string, parseAll=True)  # can raise pyparsing.ParseException
+    return evaluate_stack(expression_stack)  # can raise ZeroDivisionError
 
