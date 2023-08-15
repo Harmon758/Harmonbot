d74b04208d506aa3498f4394705a70c3ae6a47e2
Harmon <Harmon758@gmail.com>
2023-08-15 11:17:54 -0500

    [Units] Allow ignoring of kwargs for async_cache
    Add aiocache as requirement

--- units/cache.py
+++ units/cache.py
@@ -1,7 +1,37 @@
 
 # https://github.com/python/cpython/issues/90780
 
+from aiocache import cached
 from async_lru import alru_cache
 
-async_cache = alru_cache(maxsize = None)
+
+def async_cache(function = None, *, ignore_kwargs = None):
+    if callable(function):
+        return alru_cache(maxsize = None)(function)
+    elif ignore_kwargs:
+        return cached(
+            key_builder = custom_key_builder(ignore_kwargs = ignore_kwargs)
+        )
+    else:
+        return alru_cache(maxsize = None)
+
+
+def custom_key_builder(ignore_kwargs = ()):
+    if isinstance(ignore_kwargs, str):
+        ignore_kwargs = (ignore_kwargs,)
+
+    def key_builder(func, *args, **kwargs):
+        for ignore_kwarg in ignore_kwargs:
+            kwargs.pop(ignore_kwarg, None)
+
+        # aiocache.cached default
+        ordered_kwargs = sorted(kwargs.items())
+        return (
+            (func.__module__ or "")
+            + func.__name__
+            + str(args)  # noself = False
+            + str(ordered_kwargs)
+        )
+
+    return key_builder
 
