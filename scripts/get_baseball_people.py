47aa5b7fd2690d3d54858e89a5a22f4bad867781
Harmon <Harmon758@gmail.com>
2023-08-19 06:32:39 -0500

    [Scripts] Use INT instead of BIGINT to store years for baseball people
    Use INT instead of BIGINT to store integers representing years, months, and days for information about baseball people
    
    https://github.com/pandas-dev/pandas/issues/35347

--- scripts/get_baseball_people.py
+++ scripts/get_baseball_people.py
@@ -49,6 +49,31 @@ engine = sqlalchemy.create_engine(
 )
 
 dataframe.to_sql(
-    "people", engine, schema = "baseball", if_exists = "replace", index = False
+    "people", engine,
+    schema = "baseball", if_exists = "replace", index = False,
+    dtype = {  # https://github.com/pandas-dev/pandas/issues/35347
+        "birth_year": sqlalchemy.INT,
+        "birth_month": sqlalchemy.INT,
+        "birth_day": sqlalchemy.INT,
+        "death_year": sqlalchemy.INT,
+        "death_month": sqlalchemy.INT,
+        "death_day": sqlalchemy.INT,
+        "pro_played_first": sqlalchemy.INT,
+        "pro_played_last": sqlalchemy.INT,
+        "mlb_played_first": sqlalchemy.INT,
+        "mlb_played_last": sqlalchemy.INT,
+        "col_played_first": sqlalchemy.INT,
+        "col_played_last": sqlalchemy.INT,
+        "pro_managed_first": sqlalchemy.INT,
+        "pro_managed_last": sqlalchemy.INT,
+        "mlb_managed_first": sqlalchemy.INT,
+        "mlb_managed_last": sqlalchemy.INT,
+        "col_managed_first": sqlalchemy.INT,
+        "col_managed_last": sqlalchemy.INT,
+        "pro_umpired_first": sqlalchemy.INT,
+        "pro_umpired_last": sqlalchemy.INT,
+        "mlb_umpired_first": sqlalchemy.INT,
+        "mlb_umpired_last": sqlalchemy.INT
+    }
 )
 
