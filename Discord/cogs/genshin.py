8dceb0bff7261559f9df2b3c2219389d9db4ba02
Harmon <Harmon758@gmail.com>
2023-08-21 04:45:24 -0500

    [Discord] Add genshin_impact character command

--- Discord/cogs/genshin.py
+++ Discord/cogs/genshin.py
@@ -1,7 +1,17 @@
 
+from discord import app_commands
 from discord.ext import commands
 
+import calendar
+import sys
+
 from utilities import checks
+
+sys.path.insert(0, "..")
+from units.genshin_impact import (
+    API_BASE_URL, get_character, get_character_images, get_characters
+)
+sys.path.pop(0)
 
 
 async def setup(bot):
@@ -18,6 +28,83 @@ class GenshinImpact(commands.Cog, name = "Genshin Impact"):
     async def genshin_impact(self, ctx):
         """Genshin Impact"""
         await ctx.send_help(ctx.command)
+
+    @genshin_impact.command()
+    async def character(self, ctx, name: str):
+        """
+        Show information about a Genshin Impact character
+
+        Parameters
+        ----------
+        name
+            Name of character to show information about
+        """
+        await ctx.defer()
+
+        try:
+            character = await get_character(
+                name, aiohttp_session = ctx.bot.aiohttp_session
+            )
+        except ValueError as e:
+            await ctx.embed_reply(f"{ctx.bot.error_emoji} {e}")
+            return
+
+        images = await get_character_images(
+            name, aiohttp_session = ctx.bot.aiohttp_session
+        )
+
+        birth_year, birth_month, birth_day = map(
+            int, character.birthday.split('-')
+        )
+
+        await ctx.embed_reply(
+            title = character.name,
+            description = character.description,
+            thumbnail_url = (
+                f"{API_BASE_URL}/characters/{name.lower().replace(' ', '-')}/icon"
+                if "icon" in images else None
+            ),
+            fields = (
+                ("Title", character.title),
+                ("Vision", character.vision),
+                ("Weapon", character.weapon),
+                ("Nation", character.nation),
+                ("Affiliation", character.affiliation),
+                ("Rarity", '\N{WHITE MEDIUM STAR}' * character.rarity),
+                ("Constellation", character.constellation),
+                (
+                    "Birthday",
+                    f"{calendar.month_name[birth_month]} {birth_day}"
+                )
+            )
+            # TODO: combat talents, passive talents, constellations
+            # TODO: other images
+        )
+
+    @character.autocomplete(name = "name")
+    async def character_autocomplete(self, interaction, current):
+        current = current.lower()
+
+        characters = await get_characters(
+            aiohttp_session = interaction.client.aiohttp_session
+        )
+        characters = [character.replace('-', ' ') for character in characters]
+
+        primary_matches = set()
+        secondary_matches = set()
+
+        for character in characters:
+            if character.startswith(current):
+                primary_matches.add(character)
+            elif current in character:
+                secondary_matches.add(character)
+
+        matches = sorted(primary_matches) + sorted(secondary_matches)
+
+        return [
+            app_commands.Choice(name = match.title(), value = match)
+            for match in matches[:25]
+        ]
 
     @genshin_impact.command(aliases = ["fandom", "wikia", "wikicities"])
     async def wiki(self, ctx, *, query: str):
