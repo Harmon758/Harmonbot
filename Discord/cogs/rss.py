cebf345f9abbfd271e1254dfbb7de96390301aa1
Harmon <Harmon758@gmail.com>
2023-10-07 17:11:39 -0500

    [Discord] Improve rss feeds command formatting

--- Discord/cogs/rss.py
+++ Discord/cogs/rss.py
@@ -194,9 +194,13 @@ class RSS(commands.Cog):
 	@checks.not_forbidden()
 	async def feeds(self, ctx):
 		'''Show feeds being followed in this channel'''
-		records = await ctx.bot.db.fetch("SELECT feed FROM rss.feeds WHERE channel_id = $1", ctx.channel.id)
-		await ctx.embed_reply('\n'.join(record["feed"] for record in records), 
-								title = "RSS feeds being followed in this channel")
+		records = await ctx.bot.db.fetch(
+			"SELECT feed FROM rss.feeds WHERE channel_id = $1", ctx.channel.id
+		)
+		await ctx.embed_reply(
+			'\n'.join(record["feed"] for record in records),
+			title = "RSS feeds being followed in this channel"
+		)
 	
 	# R/PT60S
 	@tasks.loop(seconds = 60)
