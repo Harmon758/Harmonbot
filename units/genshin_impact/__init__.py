d2421f72a40d78d44d65eed4c997b150bb725169
Harmon <Harmon758@gmail.com>
2023-10-18 11:20:36 -0500

    [Units] Use redundant aliases for genshin_impact function exports
    Explicitly export genshin_impact.characters functions using redundant aliases in genshin_impact/__init__.py

--- units/genshin_impact/__init__.py
+++ units/genshin_impact/__init__.py
@@ -3,7 +3,8 @@
 # https://github.com/genshindev/api
 API_BASE_URL = "https://api.genshin.dev"
 
-from .characters import (
-    get_all_characters, get_character, get_character_images, get_characters
-)
+from .characters import get_all_characters as get_all_characters
+from .characters import get_character as get_character
+from .characters import get_character_images as get_character_images
+from .characters import get_characters as get_characters
 
