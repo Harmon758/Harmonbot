5d738222db8c7a25e043536604127b04433f6fe9
Harmon <Harmon758@gmail.com>
2023-10-19 09:40:37 -0500

    [Units] Ignore module-import-not-at-top-of-file violations
    Ignore module-import-not-at-top-of-file violations in genshin_impact/__init__.py

--- units/genshin_impact/__init__.py
+++ units/genshin_impact/__init__.py
@@ -3,8 +3,8 @@
 # https://github.com/genshindev/api
 API_BASE_URL = "https://api.genshin.dev"
 
-from .characters import get_all_characters as get_all_characters
-from .characters import get_character as get_character
-from .characters import get_character_images as get_character_images
-from .characters import get_characters as get_characters
+from .characters import get_all_characters as get_all_characters  # noqa: E402 (module-import-not-at-top-of-file)
+from .characters import get_character as get_character  # noqa: E402 (module-import-not-at-top-of-file)
+from .characters import get_character_images as get_character_images  # noqa: E402 (module-import-not-at-top-of-file)
+from .characters import get_characters as get_characters  # noqa: E402 (module-import-not-at-top-of-file)
 
