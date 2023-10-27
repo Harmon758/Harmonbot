6106f02d9691139a84b66c9ceb449886f2684790
Harmon <Harmon758@gmail.com>
2023-10-27 11:21:45 -0500

    [TwitchIO] Improve birthday command formatting

--- Twitch/cogs/time.py
+++ Twitch/cogs/time.py
@@ -38,24 +38,38 @@ class Time:
 			)
 			await ctx.send(f"Birthday set to {date.strftime('%B %#d')}")
 			return
-			# %#d for removal of leading zero on Windows with native Python executable
-		record = await self.bot.db.fetchrow("SELECT month, day FROM twitch.birthdays WHERE channel = $1", ctx.channel.name)
+			# %#d for removal of leading zero on Windows with native Python
+			# executable
+		record = await self.bot.db.fetchrow(
+			"SELECT month, day FROM twitch.birthdays WHERE channel = $1",
+			ctx.channel.name
+		)
 		if not record or not record["month"] or not record["day"]:
 			await ctx.send("Error: Birthday not set")
 			return
-		location = await self.bot.db.fetchval("SELECT location FROM twitch.locations WHERE channel = $1", ctx.channel.name)
+		location = await self.bot.db.fetchval(
+			"SELECT location FROM twitch.locations WHERE channel = $1",
+			ctx.channel.name
+		)
 		if location:
 			try:
-				timezone_data = await get_timezone_data(location = location, aiohttp_session = self.bot.aiohttp_session)
+				timezone_data = await get_timezone_data(
+					location = location,
+					aiohttp_session = self.bot.aiohttp_session
+				)
 			except ValueError as e:
 				await ctx.send(f"Error: {e}")
 				return
-			now = datetime.datetime.fromtimestamp(datetime.datetime.utcnow().timestamp() + 
-													timezone_data["dstOffset"] + timezone_data["rawOffset"])
+			now = datetime.datetime.fromtimestamp(
+				datetime.datetime.utcnow().timestamp() +
+				timezone_data["dstOffset"] + timezone_data["rawOffset"]
+			)
 		birthday = datetime.datetime(now.year, record["month"], record["day"])
 		if now > birthday:
 			birthday = birthday.replace(year = birthday.year + 1)
-		await ctx.send(f"{duration_to_string(birthday - now)} until {ctx.channel.name.capitalize()}'s birthday!")
+		await ctx.send(
+			f"{duration_to_string(birthday - now)} until {ctx.channel.name.capitalize()}'s birthday!"
+		)
 	
 	@commands.command()
 	async def christmas(self, ctx):
