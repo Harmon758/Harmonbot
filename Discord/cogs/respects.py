2469d16a74b6ba1d385474481513f6f39a31227a
Harmon <Harmon758@gmail.com>
2023-10-29 10:58:11 -0500

    [Discord] Improve respects stats command formatting

--- Discord/cogs/respects.py
+++ Discord/cogs/respects.py
@@ -146,7 +146,9 @@ class Respects(commands.Cog):
 	@respects.command(aliases = ["statistics"])
 	async def stats(self, ctx):
 		'''Statistics'''
-		total_respects = await ctx.bot.db.fetchval("SELECT value FROM respects.stats WHERE stat = 'total'")
+		total_respects = await ctx.bot.db.fetchval(
+			"SELECT value FROM respects.stats WHERE stat = 'total'"
+		)
 		respects_paid = []
 		async with (
 			ctx.bot.database_connection_pool.acquire() as connection,
@@ -154,41 +156,64 @@ class Respects(commands.Cog):
 			# Postgres requires non-scrollable cursors to be created
 			# and used in a transaction.
 		):
-			async for record in connection.cursor("SELECT * FROM respects.users"):
+			async for record in connection.cursor(
+				"SELECT * FROM respects.users"
+			):
 				respects_paid.append(record["respects"])
 		# TODO: Optimize
 		# TODO: Fit curve
 		## n, bins, _ = matplotlib.pyplot.hist(respects_paid, log = True, 
 		figure = matplotlib.figure.Figure()
-		axes = figure.add_subplot(xscale = "log", xlabel = "Respects Paid", ylabel = "People")
+		axes = figure.add_subplot(
+			xscale = "log", xlabel = "Respects Paid", ylabel = "People"
+		)
 		last_power_of_10 = math.ceil(numpy.log10(max(respects_paid)))
-		bins = (10 ** numpy.arange(last_power_of_10))[:, numpy.newaxis] * numpy.arange(1, 10)
-		axes.hist(respects_paid, bins = bins.flatten(), log = True, ec = "black")
+		bins = (
+			(10 ** numpy.arange(last_power_of_10))[:, numpy.newaxis] *
+			numpy.arange(1, 10)
+		)
+		axes.hist(
+			respects_paid, bins = bins.flatten(), log = True, ec = "black"
+		)
 		## bin_centers = bins[:-1] + numpy.diff(bins) / 2
 		## def func(x, a, b, c):
 		##	return a * numpy.exp(-b * x) + c
-		## popt, _ = scipy.optimize.curve_fit(func, bin_centers[1:], n[1:], p0 = (1000, 1, 1), maxfev = 100000)
+		## popt, _ = scipy.optimize.curve_fit(
+		## 	func, bin_centers[1:], n[1:], p0 = (1000, 1, 1), maxfev = 100000
+		## )
 		## t_init = astropy.modeling.models.Gaussian1D()
 		## fit_t = astropy.modeling.fitting.LevMarLSQFitter()
 		## t = fit_t(t_init, bin_centers, n)
 		## x_interval_for_fit = numpy.linspace(bins[0], bins[-1], 10000)
-		## matplotlib.pyplot.plot(x_interval_for_fit, func(x_interval_for_fit, *popt), color = "red")
+		## matplotlib.pyplot.plot(
+		## 	x_interval_for_fit, func(x_interval_for_fit, *popt), color = "red"
+		## )
 		## axes.set_ylim(0.8)
 		formatter = matplotlib.ticker.ScalarFormatter()
 		formatter.set_scientific(False)
 		axes.get_xaxis().set_major_formatter(formatter)
 		axes.get_yaxis().set_major_formatter(formatter)
 		# Remove minor ticks < 1
-		axes.set_xticks([tick for tick in axes.get_xticks(minor = True) if tick >= 1], minor = True)
-		axes.set_yticks([tick for tick in axes.get_yticks(minor = True) if tick >= 1], minor = True)
+		axes.set_xticks(
+			[tick for tick in axes.get_xticks(minor = True) if tick >= 1],
+			minor = True
+		)
+		axes.set_yticks(
+			[tick for tick in axes.get_yticks(minor = True) if tick >= 1],
+			minor = True
+		)
 		axes.autoscale(enable = True)
 		buffer = io.BytesIO()
 		figure.savefig(buffer, format = "PNG")
 		buffer.seek(0)
-		await ctx.embed_reply(fields = (("Total respects paid", f"{total_respects:,}"), 
-										("People who paid respects", f"{len(respects_paid):,}")),
-								image_url = "attachment://respects.png", 
-								file = discord.File(buffer, filename = "respects.png"))
+		await ctx.embed_reply(
+			fields = (
+				("Total respects paid", f"{total_respects:,}"),
+				("People who paid respects", f"{len(respects_paid):,}")
+			),
+			image_url = "attachment://respects.png",
+			file = discord.File(buffer, filename = "respects.png")
+		)
 	
 	@respects.command(aliases = ["most"])
 	async def top(self, ctx, number: int = 10):
