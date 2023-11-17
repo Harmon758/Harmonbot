fdc344afe40e8246a703c912499a2a1c63964b4c
Harmon <Harmon758@gmail.com>
2023-11-17 10:05:15 -0600

    [TwitchIO] Improve Search cog formatting

--- Twitch/cogs/search.py
+++ Twitch/cogs/search.py
@@ -1,25 +1,26 @@
 
 from twitchio.ext import commands
 
+
 @commands.cog()
 class Search:
-	
-	def __init__(self, bot):
-		self.bot = bot
-	
-	@commands.command()
-	async def google(self, ctx, *search):
-		await ctx.send("https://google.com/search?q=" + '+'.join(search))
-	
-	@commands.command()
-	async def imfeelinglucky(self, ctx, *search):
-		await ctx.send("https://google.com/search?btnI&q=" + '+'.join(search))
-	
-	@commands.command()
-	async def lmgtfy(self, ctx, *search):
-		await ctx.send("https://lmgtfy.com/?q=" + '+'.join(search))
-	
-	@commands.command(aliases = ("wiki",))
-	async def wikipedia(self, ctx, *search):
-		await ctx.send("https://wikipedia.org/wiki/" + '_'.join(search))
+
+    def __init__(self, bot):
+        self.bot = bot
+
+    @commands.command()
+    async def google(self, ctx, *search):
+        await ctx.send("https://google.com/search?q=" + '+'.join(search))
+
+    @commands.command()
+    async def imfeelinglucky(self, ctx, *search):
+        await ctx.send("https://google.com/search?btnI&q=" + '+'.join(search))
+
+    @commands.command()
+    async def lmgtfy(self, ctx, *search):
+        await ctx.send("https://lmgtfy.com/?q=" + '+'.join(search))
+
+    @commands.command(aliases = ("wiki",))
+    async def wikipedia(self, ctx, *search):
+        await ctx.send("https://wikipedia.org/wiki/" + '_'.join(search))
 
