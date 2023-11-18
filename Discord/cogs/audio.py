ccd9fd0b6b6d1e5b9ae651253b467e6f4a878aae
Harmon <Harmon758@gmail.com>
2023-11-18 12:30:46 -0600

    [Discord] Improve undeafen command clarity

--- Discord/cogs/audio.py
+++ Discord/cogs/audio.py
@@ -775,7 +775,8 @@ class Audio(commands.Cog):
     async def undeafen(self, ctx):
         '''Undeafen'''
         if not ctx.guild.me.voice.self_deaf:
-            return await ctx.embed_reply(f"{ctx.bot.error_emoji} I'm not deafened")
+            await ctx.embed_reply(f"{ctx.bot.error_emoji} I'm not deafened")
+            return
         await ctx.guild.change_voice_state(channel = ctx.guild.voice_client.channel, self_deaf = False, self_mute = ctx.guild.me.voice.self_mute)
         await ctx.embed_reply("I've undeafened myself")
 
