7ee1fb7ed2fed6a7736633c73ece666261de36ba
Harmon <Harmon758@gmail.com>
2023-11-18 12:25:13 -0600

    [Discord] Improve mute command formatting

--- Discord/cogs/audio.py
+++ Discord/cogs/audio.py
@@ -763,7 +763,10 @@ class Audio(commands.Cog):
         if ctx.guild.me.voice.self_mute:
             await ctx.embed_reply(f"{ctx.bot.error_emoji} I'm already muted")
             return
-        await ctx.guild.change_voice_state(channel = ctx.guild.voice_client.channel, self_mute = True, self_deaf = ctx.guild.me.voice.self_deaf)
+        await ctx.guild.change_voice_state(
+            channel = ctx.guild.voice_client.channel, self_mute = True,
+            self_deaf = ctx.guild.me.voice.self_deaf
+        )
         await ctx.embed_reply("I've muted myself")
 
     @commands.command()
