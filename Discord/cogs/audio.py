0a0e8147bb2f06856f2a847a285f75a6fc399655
Harmon <Harmon758@gmail.com>
2023-11-19 00:57:42 -0600

    [Discord] Use character name escape sequences in volume command
    Use character name escape sequences for speaker with one sound wave emoji in volume command response

--- Discord/cogs/audio.py
+++ Discord/cogs/audio.py
@@ -647,17 +647,17 @@ class Audio(commands.Cog):
             (Defaults to False — change/show the volume for the current song)
         '''  # noqa: RUF002 (ambiguous-unicode-character-docstring)
         if default and volume_setting is None:
-            await ctx.embed_reply(f":sound: Current default volume: {self.players[ctx.guild.id].default_volume:g}")
+            await ctx.embed_reply(f"\N{SPEAKER WITH ONE SOUND WAVE} Current default volume: {self.players[ctx.guild.id].default_volume:g}")
         elif default:
             volume_setting = min(max(0, volume_setting), 2000)
             self.players[ctx.guild.id].default_volume = volume_setting
-            await ctx.embed_reply(f":sound: Set default volume to {volume_setting:g}")
+            await ctx.embed_reply(f"\N{SPEAKER WITH ONE SOUND WAVE} Set default volume to {volume_setting:g}")
         elif volume_setting is None and ctx.guild.voice_client.is_playing():
-            await ctx.embed_reply(f":sound: Current volume: {ctx.guild.voice_client.source.volume:g}")
+            await ctx.embed_reply(f"\N{SPEAKER WITH ONE SOUND WAVE} Current volume: {ctx.guild.voice_client.source.volume:g}")
         elif ctx.guild.voice_client.is_playing():
             ctx.guild.voice_client.source.volume = volume_setting
             volume_setting = min(max(0, volume_setting), 2000)
-            await ctx.embed_reply(f":sound: Set volume to {volume_setting:g}")
+            await ctx.embed_reply(f"\N{SPEAKER WITH ONE SOUND WAVE} Set volume to {volume_setting:g}")
         else:
             await ctx.embed_reply(f"{ctx.bot.error_emoji} There's nothing playing right now")
 
