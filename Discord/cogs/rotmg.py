9cea108bf5e4ee35c51a7f1c0323c2dc4ba7673b
Harmon <Harmon758@gmail.com>
2023-11-28 18:22:14 -0600

    [Discord] Improve rotmg command formatting

--- Discord/cogs/rotmg.py
+++ Discord/cogs/rotmg.py
@@ -15,7 +15,10 @@ class RotMG(commands.Cog):
 	async def cog_check(self, ctx):
 		return await checks.not_forbidden().predicate(ctx)
 	
-	@commands.group(aliases = ["realmofthemadgod"], invoke_without_command = True, case_insensitive = True)
+	@commands.group(
+		aliases = ["realmofthemadgod"],
+		invoke_without_command = True, case_insensitive = True
+	)
 	async def rotmg(self, ctx, player: str):
 		'''Realm of the Mad God player information'''
 		# http://webhost.ischool.uw.edu/~joatwood/realmeye_api/0.3/
@@ -24,24 +27,42 @@ class RotMG(commands.Cog):
 			params = {"player": player}
 		) as resp:
 			data = await resp.json()
+		
 		if "error" in data:
 			await ctx.embed_reply("Error: " + data["error"])
 			return
-		fields = [("Characters", data["chars"]), ("Total Fame", f"{data['fame']:,}"), 
-					("Fame Rank", f"{data['fame_rank']:,}"), ("Class Quests Completed", data["rank"]), 
-					("Account Fame", f"{data['account_fame']:,}"), 
-					("Account Fame Rank", f"{data['account_fame_rank']:,}")]
+		
+		fields = [
+			("Characters", data["chars"]),
+			("Total Fame", f"{data['fame']:,}"),
+			("Fame Rank", f"{data['fame_rank']:,}"),
+			("Class Quests Completed", data["rank"]),
+			("Account Fame", f"{data['account_fame']:,}"),
+			("Account Fame Rank", f"{data['account_fame_rank']:,}")
+		]
 		if created := data.get("created"):
 			fields.append(("Created", created))
-		fields.extend((("Total Exp", f"{data['exp']:,}"), ("Exp Rank", f"{data['exp_rank']:,}"), 
-						("Last Seen", data["player_last_seen"])))
+		fields.extend((
+			("Total Exp", f"{data['exp']:,}"),
+			("Exp Rank", f"{data['exp_rank']:,}"),
+			("Last Seen", data["player_last_seen"])
+		))
 		if guild := data.get("guild"):
-			fields.extend((("Guild", guild), ("Guild Position", data["guild_rank"])))
+			fields.extend((
+				("Guild", guild),
+				("Guild Position", data["guild_rank"])
+			))
 		if data["desc1"] or data["desc2"] or data["desc3"]:
-			fields.append(("Description", f"{data['desc1']}\n{data['desc2']}\n{data['desc3']}"))
-		await ctx.embed_reply(title = data["player"], title_url = f"https://www.realmeye.com/player/{player}", 
-								description = "Donator" if data["donator"] == "true" else None,
-								fields = fields)
+			fields.append((
+				"Description",
+				f"{data['desc1']}\n{data['desc2']}\n{data['desc3']}"
+			))
+		await ctx.embed_reply(
+			title = data["player"],
+			title_url = f"https://www.realmeye.com/player/{player}",
+			description = "Donator" if data["donator"] == "true" else None,
+			fields = fields
+		)
 	
 	@rotmg.command(name = "characters")
 	async def rotmg_characters(self, ctx, player : str):
