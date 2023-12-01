1383ec192ac63e5aefd5384d0b6ea3e875d290ca
Harmon <Harmon758@gmail.com>
2023-12-01 01:07:34 -0600

    [Discord] Improve Reactions cog formatting

--- Discord/cogs/reactions.py
+++ Discord/cogs/reactions.py
@@ -3,18 +3,19 @@ from discord.ext import commands
 
 from utilities import checks
 
+
 async def setup(bot):
-	await bot.add_cog(Reactions(bot))
+    await bot.add_cog(Reactions(bot))
 
 # meta.stats reaction_responses column:
 #  Fixed to stop counting own reactions on 2019-10-25
 #  Deprecated on 2020-01-04 in favor of menu_reactions
 
 class Reactions(commands.Cog):
-	
-	@commands.command(aliases = ["reaction", "menus", "menu"])
-	@checks.not_forbidden()
-	async def reactions(self, ctx):
-		'''Menu versions of commands'''
-		await ctx.send_help(ctx.command)
+
+    @commands.command(aliases = ["reaction", "menus", "menu"])
+    @checks.not_forbidden()
+    async def reactions(self, ctx):
+        '''Menu versions of commands'''
+        await ctx.send_help(ctx.command)
 
