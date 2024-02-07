172809f6e9d0a60a811b8f1563e8d31dd299c732
Harmon <Harmon758@gmail.com>
2024-02-07 03:02:22 -0600

    [Discord] Improve astronomy people command formatting

--- Discord/cogs/astronomy.py
+++ Discord/cogs/astronomy.py
@@ -420,9 +420,18 @@ class Astronomy(commands.Cog):
 	async def people(self, ctx):
 		"""Current people in space"""
 		# TODO: add input/search option
-		async with ctx.bot.aiohttp_session.get("http://api.open-notify.org/astros.json") as resp:
+		async with ctx.bot.aiohttp_session.get(
+			"http://api.open-notify.org/astros.json"
+		) as resp:
 			data = await resp.json()
-		await ctx.embed_reply('\n'.join(f"{person['name']} ({person['craft']})" for person in data["people"]), title = "Current People In Space ({})".format(data["number"]))
+		
+		await ctx.embed_reply(
+			'\n'.join(
+				f"{person['name']} ({person['craft']})"
+				for person in data["people"]
+			),
+			title = "Current People In Space ({})".format(data["number"])
+		)
 	
 	@astronomy.command()
 	async def publication(self, ctx, *, bibcode: str):
