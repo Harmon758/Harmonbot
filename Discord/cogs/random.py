46b0b1f0c9b06bc1547063053347fc020fcab5ce
Harmon <Harmon758@gmail.com>
2024-03-22 07:26:19 -0500

    [Discord] Normalize random coin command docstring quotation mark type

--- Discord/cogs/random.py
+++ Discord/cogs/random.py
@@ -228,7 +228,7 @@ class Random(commands.Cog):
 
     @random.command(name = "coin", aliases = ["flip"], with_app_command = False)
     async def random_coin(self, ctx):
-        '''Flip a coin'''
+        """Flip a coin"""
         # Note: coin command invokes this command
         await ctx.embed_reply(random.choice(("Heads!", "Tails!")))
 
