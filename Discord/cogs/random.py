c04e39342d58197a477ea5169b3be7926f139486
Harmon <Harmon758@gmail.com>
2024-03-23 07:27:34 -0500

    [Discord] Improve random coin command formatting

--- Discord/cogs/random.py
+++ Discord/cogs/random.py
@@ -226,7 +226,9 @@ class Random(commands.Cog):
                 "random choose command not found when choose command invoked"
             )
 
-    @random.command(name = "coin", aliases = ["flip"], with_app_command = False)
+    @random.command(
+        name = "coin", aliases = ["flip"], with_app_command = False
+    )
     async def random_coin(self, ctx):
         """Flip a coin"""
         # Note: coin command invokes this command
