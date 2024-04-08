a37cb256cad45856913cc45ec2ab2586a5de3d84
Harmon <Harmon758@gmail.com>
2024-04-08 10:36:44 -0500

    [Discord] Normalize shards subcommand docstring quotation mark type
    Normalize shards recommended command docstring quotation mark type

--- Discord/cogs/meta.py
+++ Discord/cogs/meta.py
@@ -173,7 +173,7 @@ class Meta(commands.Cog):
 
     @shards.command(aliases = ["recommend"])
     async def recommended(self, ctx):
-        '''Recommended number of shards to use by Discord API'''
+        """Recommended number of shards to use by Discord API"""
         count, _ = await ctx.bot.http.get_bot_gateway()
         await ctx.embed_reply(count)
 
