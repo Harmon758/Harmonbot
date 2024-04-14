0a1ee3f691b4504054111632d1f1252d14c34e17
Harmon <Harmon758@gmail.com>
2024-04-14 10:48:04 -0500

    [Discord] Improve harmonbot nickname command formatting

--- Discord/cogs/meta.py
+++ Discord/cogs/meta.py
@@ -553,7 +553,7 @@ class Meta(commands.Cog):
     @harmonbot.command(name = "nickname")
     @commands.guild_only()
     @checks.not_forbidden()
-    async def harmonbot_nickname(self, ctx, *, nickname : str = ""):
+    async def harmonbot_nickname(self, ctx, *, nickname: str = ""):
         """My nickname"""
         if not nickname:
             await ctx.embed_reply(ctx.me.nick)
