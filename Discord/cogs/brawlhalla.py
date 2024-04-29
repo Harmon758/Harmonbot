7a875c55568aee3e4242693964581d5bbe791fc8
Harmon <Harmon758@gmail.com>
2024-04-29 08:25:03 -0500

    [Discord] Move SteamID64 import into type-checking block

--- Discord/cogs/brawlhalla.py
+++ Discord/cogs/brawlhalla.py
@@ -8,10 +8,10 @@ from typing import Optional, TYPE_CHECKING
 
 from units.cache import async_cache
 from utilities import checks
-from utilities.converters import SteamID64
 
 if TYPE_CHECKING:
     from utilities.context import Context
+    from utilities.converters import SteamID64
 
 
 async def setup(bot):
