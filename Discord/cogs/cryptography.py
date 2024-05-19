5fd6d2598b24a174452e7f6f3fd78c2589e6b24a
Harmon <Harmon758@gmail.com>
2024-05-19 16:48:12 -0500

    [Discord] Add Context type hint for encode gost command

--- Discord/cogs/cryptography.py
+++ Discord/cogs/cryptography.py
@@ -294,7 +294,7 @@ class Cryptography(commands.Cog):
         name = "gost", aliases = ["гост"],
         case_insensitive = True, hidden = True
     )
-    async def encode_gost(self, ctx):
+    async def encode_gost(self, ctx: Context):
         """
         Russian Federation/Soviet Union GOST
         Межгосударственный стандарт
