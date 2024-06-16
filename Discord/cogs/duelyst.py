f73519805fa229f8d4bb06c93b32277a3a4cbb0e
Harmon <Harmon758@gmail.com>
2024-06-16 06:23:47 -0500

    [Discord] Add Context type hint for duelyst card random command

--- Discord/cogs/duelyst.py
+++ Discord/cogs/duelyst.py
@@ -56,7 +56,7 @@ class Duelyst(commands.Cog):
         await ctx.send_help(ctx.command)
 
     @card.command(hidden = True)
-    async def card_random(self, ctx):
+    async def card_random(self, ctx: Context):
         """
         This command is deprecated now that Duelyst is closed/shut down:
         https://duelyst.com/news/farewell-duelyst
