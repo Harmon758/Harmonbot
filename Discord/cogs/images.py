7d9c4163efe1dcdcb4e1fca3079be40c0df4d739
Harmon <Harmon758@gmail.com>
2024-07-02 08:03:14 -0500

    [Discord] Move discord import into type-checking block for Images cog

--- Discord/cogs/images.py
+++ Discord/cogs/images.py
@@ -1,7 +1,6 @@
 
 from __future__ import annotations
 
-import discord
 from discord.ext import commands
 
 import inspect
@@ -14,6 +13,7 @@ from units import clarifai
 from utilities import checks
 
 if TYPE_CHECKING:
+    import discord
     from utilities.context import Context
 
 
