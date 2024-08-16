299a154931c3ba76f521475658b60b57373b8e94
Harmon <Harmon758@gmail.com>
2024-08-16 16:54:51 -0500

    [Discord] Add Context type hint for sinh command

--- Discord/cogs/math.py
+++ Discord/cogs/math.py
@@ -288,7 +288,7 @@ class Math(commands.Cog):
         await ctx.embed_reply(math.sin(value))
 
     @commands.command(alises = ["sineh"])
-    async def sinh(self, ctx, value: float):
+    async def sinh(self, ctx: Context, value: float):
         """Hyperbolic sine function"""
         await ctx.embed_reply(math.sinh(value))
 
