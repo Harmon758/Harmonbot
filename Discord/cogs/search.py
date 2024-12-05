07dd6aa194da8ed0b3d7918ba284ae2cc7a35aee
Harmon <Harmon758@gmail.com>
2024-12-05 07:36:47 -0600

    [Discord] Add Context type hint for search aol command

--- Discord/cogs/search.py
+++ Discord/cogs/search.py
@@ -75,7 +75,7 @@ class Search(commands.GroupCog, group_name = "search"):
             )
 
     @search.command(name = "aol")
-    async def search_aol(self, ctx, *search: str):
+    async def search_aol(self, ctx: Context, *search: str):
         """Search with AOL"""
         # Note: aol command invokes this command
         await ctx.embed_reply(
