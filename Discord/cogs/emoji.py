6184d30050f39c56211957a93347fa898ac8a213
Harmon <Harmon758@gmail.com>
2025-05-13 22:02:10 -0500

    [Discord] Update Ruff rule code in error suppression for Emoji cog

--- Discord/cogs/emoji.py
+++ Discord/cogs/emoji.py
@@ -10,7 +10,7 @@ from operator import attrgetter
 from typing import TYPE_CHECKING
 
 from utilities import checks
-from utilities.transformers import PartialEmojiTransformer  # noqa: TCH002
+from utilities.transformers import PartialEmojiTransformer  # noqa: TC002
 
 if TYPE_CHECKING:
     from utilities.context import Context
