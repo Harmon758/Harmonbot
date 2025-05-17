a0a1d17c441257e769574261bfbc06cfaa7b0490
Harmon <Harmon758@gmail.com>
2025-05-16 22:08:57 -0500

    [Discord] Update Ruff rule code in error suppression for Images cog

--- Discord/cogs/images.py
+++ Discord/cogs/images.py
@@ -1,7 +1,7 @@
 
 from __future__ import annotations
 
-from discord import Attachment  # noqa: TCH001
+from discord import Attachment  # noqa: TC001
 from discord.ext import commands
 
 import inspect
