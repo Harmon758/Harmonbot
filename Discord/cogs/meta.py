e3c9dc76db91138a4b0feedeee9457fe3431546e
Harmon <Harmon758@gmail.com>
2025-07-15 20:32:55 -0500

    [Discord] Use Optional type hint for harmonbot activity command

--- Discord/cogs/meta.py
+++ Discord/cogs/meta.py
@@ -427,7 +427,7 @@ class Meta(commands.Cog):
                         invoke_without_command = True, case_insensitive = True)
     @commands.guild_only()
     @checks.not_forbidden()
-    async def harmonbot_activity(self, ctx, *, name: str = ""):
+    async def harmonbot_activity(self, ctx, *, name: Optional[str]):  # noqa: UP045 (non-pep604-annotation-optional)
         '''My activity'''
         # TODO: Handle in DMs
         activity = ctx.me.activity
