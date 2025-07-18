040112168b80793aba53521023ee042fabc6eef3
Harmon <Harmon758@gmail.com>
2025-07-17 20:37:46 -0500

    [Discord] Use Optional type hint for harmonbot activity type command

--- Discord/cogs/meta.py
+++ Discord/cogs/meta.py
@@ -474,7 +474,7 @@ class Meta(commands.Cog):
     @harmonbot_activity.command(name = "type")
     @commands.guild_only()
     @checks.not_forbidden()
-    async def harmonbot_activity_type(self, ctx, type: str = ""):
+    async def harmonbot_activity_type(self, ctx, type: Optional[str]):  # noqa: UP045 (non-pep604-annotation-optional)
         '''
         My activity type
         Valid types: playing, streaming, listening, watching
