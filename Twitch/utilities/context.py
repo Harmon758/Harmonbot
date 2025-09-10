f08200c1c8813d30f014221a02d07485870a1318
Harmon <Harmon758@gmail.com>
2025-09-10 06:58:49 -0500

    [TwitchIO] Improve Context formatting

--- Twitch/utilities/context.py
+++ Twitch/utilities/context.py
@@ -3,12 +3,13 @@ from twitchio.ext import commands
 
 import random
 
+
 class Context(commands.Context):
-	
-	def __init__(self, *args, **kwargs):
-		super().__init__(*args, **kwargs)
-		self.channel_command = None
-	
-	def random_viewer(self):
-		return random.choice(self.channel.chatters)
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.channel_command = None
+
+    def random_viewer(self):
+        return random.choice(self.channel.chatters)
 
