ef9979debc9dfec4ba616b5070709f4293e81f93
Harmon <Harmon758@gmail.com>
2025-09-24 01:46:35 -0500

    [Discord] Fix handling of ephemeral user integration interactions
    Fix handling of ephemeral user integration interactions in guilds

--- Discord/Harmonbot.py
+++ Discord/Harmonbot.py
@@ -131,53 +131,54 @@ if __name__ == "__main__":
 		
 		# Forward DMs
 		if channel.type is discord.ChannelType.private:
-			if not channel.recipient:
-				try:
+			try:
+				if not channel.recipient:
 					channel = (
 						ctx.bot.get_channel(channel.id) or
 						await ctx.bot.fetch_channel(channel.id)
 					)
-				except discord.Forbidden:
-					# Forbidden from fetching channel here means this is a user
-					# integration interaction in a guild and this is an
-					# ephemeral message that is an instance of the below issue.
-					ctx.bot.print(
-						f"Ephemeral message with erroneous DMChannel channel attribute: {message.id}"
-					)
-			# message.channel is a DMChannel here for an ephemeral message even
-			# if it was not sent as a DM. This is an issue with discord.py
-			# caused by Discord's API not providing guild_id for ephemeral
-			# messages for the MESSAGE_CREATE event. This might eventually be
-			# fixed by Discord.
-			# https://github.com/Rapptz/discord.py/issues/7370
-			# https://discord.com/channels/336642139381301249/336642776609456130/875382864919797760
-			if channel.type is not discord.ChannelType.private:
+			except discord.Forbidden:
+				# Forbidden from fetching channel here means this is a user
+				# integration interaction in a guild and this is an ephemeral
+				# message that is an instance of the below issue.
 				ctx.bot.print(
 					f"Ephemeral message with erroneous DMChannel channel attribute: {message.id}"
 				)
-			elif channel.recipient.id != ctx.bot.owner_id:
-				if not (
-					owner := discord.utils.get(
-						ctx.bot.get_all_members(), id = ctx.bot.owner_id
+			else:
+				# message.channel is a DMChannel here for an ephemeral message
+				# even if it was not sent as a DM. This is an issue with
+				# discord.py caused by Discord's API not providing guild_id for
+				# ephemeral messages for the MESSAGE_CREATE event. This might
+				# eventually be fixed by Discord.
+				# https://github.com/Rapptz/discord.py/issues/7370
+				# https://discord.com/channels/336642139381301249/336642776609456130/875382864919797760
+				if channel.type is not discord.ChannelType.private:
+					ctx.bot.print(
+						f"Ephemeral message with erroneous DMChannel channel attribute: {message.id}"
 					)
-				):
-					owner = await ctx.bot.fetch_user(ctx.bot.owner_id)
-				if author == ctx.bot.user:
-					try:
+				elif channel.recipient.id != ctx.bot.owner_id:
+					if not (
+						owner := discord.utils.get(
+							ctx.bot.get_all_members(), id = ctx.bot.owner_id
+						)
+					):
+						owner = await ctx.bot.fetch_user(ctx.bot.owner_id)
+					if author == ctx.bot.user:
+						try:
+							await owner.send(
+								f"To {channel.recipient.mention} ({channel.recipient}): {message.content}",
+								embed = message.embeds[0] if message.embeds else None
+							)
+						except discord.HTTPException:
+							# TODO: use textwrap/paginate
+							await owner.send(
+								f"To {channel.recipient.mention} ({channel.recipient}): `DM too long to forward`"
+							)
+					else:
 						await owner.send(
-							f"To {channel.recipient.mention} ({channel.recipient}): {message.content}",
+							f"From {author.mention} ({author}): {message.content}",
 							embed = message.embeds[0] if message.embeds else None
 						)
-					except discord.HTTPException:
-						# TODO: use textwrap/paginate
-						await owner.send(
-							f"To {channel.recipient.mention} ({channel.recipient}): `DM too long to forward`"
-						)
-				else:
-					await owner.send(
-						f"From {author.mention} ({author}): {message.content}",
-						embed = message.embeds[0] if message.embeds else None
-					)
 		
 		# Ignore own and blank messages
 		if author == ctx.bot.user or not message.content:
