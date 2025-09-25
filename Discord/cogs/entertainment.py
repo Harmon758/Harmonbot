ce2c470539405f46c6e213ea15c5fee3656b9e5a
Harmon <Harmon758@gmail.com>
2025-09-25 09:13:15 -0500

    [Discord] Use embed for xkcd slash command response
    When comic not found

--- Discord/cogs/entertainment.py
+++ Discord/cogs/entertainment.py
@@ -352,7 +352,7 @@ class Entertainment(commands.Cog):
 			if not (
 				initial_page := await self.search_for_xkcd(ctx, query)
 			):
-				await interaction.response.send_message("xkcd comic not found")
+				await ctx.embed_reply("xkcd comic not found")
 				return
 		else:
 			initial_page = inf
