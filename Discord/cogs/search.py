ac7eb575f1fd64f2ab7a2e5d99fe48d965f7bc2d
Harmon <Harmon758@gmail.com>
2025-09-26 09:04:22 -0500

    [Discord] Allow search commands in (G)DMs

--- Discord/cogs/search.py
+++ Discord/cogs/search.py
@@ -49,6 +49,9 @@ class Search(commands.Cog):
 
     @commands.hybrid_group(case_insensitive = True)
     @app_commands.allowed_installs(guilds = True, users = True)
+    @app_commands.allowed_contexts(
+        guilds = True, dms = True, private_channels = True
+    )
     async def search(self, ctx: Context):
         """
         Search things
