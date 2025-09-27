e47059014f3bbc9ae1b993400b0f2e6e445fba86
Harmon <Harmon758@gmail.com>
2025-09-27 11:58:54 -0500

    [Discord] Remove get_random_article import for Search cog
    Remove now unnecessary get_random_article import for Search cog

--- Discord/cogs/search.py
+++ Discord/cogs/search.py
@@ -9,7 +9,7 @@ from typing import Optional, TYPE_CHECKING
 
 import youtube_dl
 
-from units.wikis import get_random_article, search_wiki, WIKIS, OUTDATED_WIKIS
+from units.wikis import search_wiki, WIKIS, OUTDATED_WIKIS
 from units import wolfram_alpha
 from utilities import checks
 from utilities.menu_sources import WolframAlphaSource
