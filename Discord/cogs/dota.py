05f8490fd2f339640589904156800a93b4c54dcf
Harmon <Harmon758@gmail.com>
2025-10-02 07:17:45 -0500

    [Discord] Correct dota command documentation

--- Discord/cogs/dota.py
+++ Discord/cogs/dota.py
@@ -24,7 +24,7 @@ class DotA(commands.Cog):
         case_insensitive = True, invoke_without_command = True
     )
     async def dota(self, ctx):
-        '''Defense of the Ancients 2'''
+        '''Dota 2'''
         await ctx.send_help(ctx.command)
 
     # TODO: Add dota buff subcommand alias
