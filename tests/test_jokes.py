ce39cbeab03fe24c1e9c21511cacae4c299e2856
Harmon <Harmon758@gmail.com>
2025-10-09 07:20:07 -0500

    [Tests] Use pytest to conditionally not expect failure for CI
    For test to get random dad joke
    Since CI uses existing HTTP interactions in cassette instead of recording

--- tests/test_jokes.py
+++ tests/test_jokes.py
@@ -3,6 +3,7 @@ import unittest
 
 import asyncio
 import os
+import pytest
 
 from tests import vcr
 from units.jokes import get_dad_joke, DadJokeError
@@ -13,7 +14,11 @@ class TestGetDadJoke(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
         asyncio.get_running_loop().slow_callback_duration = 1
 
-    @unittest.expectedFailure  # https://github.com/aio-libs/aiohttp/pull/11604
+    @pytest.mark.xfail(
+        not os.getenv("CI"),
+        reason = "zstd error from aiohttp"
+        # https://github.com/aio-libs/aiohttp/pull/11604
+    )
     @vcr.use_cassette(
         "jokes/get_dad_joke/get_random_dad_joke.yaml",
         record_mode = "none" if os.getenv("CI") else "all"
