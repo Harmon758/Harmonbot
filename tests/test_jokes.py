933576ca9d8fa6379e5e72c40933af9e09b819ca
Harmon <Harmon758@gmail.com>
2025-10-11 21:44:02 -0500

    [Tests] Remove expected failure for test to get random dad joke
    As it should no longer fail with the zstandard dependency removed

--- tests/test_jokes.py
+++ tests/test_jokes.py
@@ -14,11 +14,6 @@ class TestGetDadJoke(unittest.IsolatedAsyncioTestCase):
     async def asyncSetUp(self):
         asyncio.get_running_loop().slow_callback_duration = 1
 
-    @pytest.mark.xfail(
-        not os.getenv("CI"),
-        reason = "zstd error from aiohttp"
-        # https://github.com/aio-libs/aiohttp/pull/11604
-    )
     @vcr.use_cassette(
         "jokes/get_dad_joke/get_random_dad_joke.yaml",
         record_mode = "none" if os.getenv("CI") else "all"
