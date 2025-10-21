d9d51e9192af7a26c8efa75097a2dcaaa9c0bb0e
Harmon <Harmon758@gmail.com>
2025-10-20 19:39:36 -0500

    [Discord] Optimize LichessUserView initialization

--- Discord/cogs/lichess.py
+++ Discord/cogs/lichess.py
@@ -512,9 +512,9 @@ class LichessUserView(ui.View):
                 continue
             if mode_data.get("games", 0):
                 prov = ""
-                if lichess_user["perfs"][mode].get("prov"):
+                if mode_data.get("prov"):
                     prov = '?'
-                if lichess_user["perfs"][mode]["prog"] >= 0:
+                if mode_data["prog"] >= 0:
                     arrow = self.uprightarrow_emoji
                 else:
                     arrow = self.downrightarrow_emoji
