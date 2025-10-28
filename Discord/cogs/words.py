46c650d1cfdb46d141442c4e2460ba0d8cb4f90d
Harmon <Harmon758@gmail.com>
2025-10-27 20:28:03 -0500

    [Discord] Allow define command for user installations

--- Discord/cogs/words.py
+++ Discord/cogs/words.py
@@ -71,7 +71,6 @@ class Words(commands.Cog):
     @commands.hybrid_command(
         aliases = ["definition", "definitions", "dictionary"]
     )
-    @app_commands.allowed_installs(guilds = True, users = False)
     @app_commands.allowed_contexts(
         guilds = True, dms = False, private_channels = False
     )
