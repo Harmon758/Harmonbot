394d599ce95df5579a0531995cbf85e4ab79c30a
Harmon <Harmon758@gmail.com>
2025-11-24 09:39:46 -0600

    [Discord] Improve adventure forage start command formatting

--- Discord/cogs/adventure.py
+++ Discord/cogs/adventure.py
@@ -196,12 +196,19 @@ class Adventure(commands.Cog):
         player = await self.get_adventure_player(ctx.author.id)
         started = await player.start_foraging(item)
         if started is True:
-            await ctx.embed_reply(f"\N{HERB} You have started foraging for {item}")
+            await ctx.embed_reply(
+                f"\N{HERB} You have started foraging for {item}"
+            )
             # TODO: active option?
         elif started is False:
-            await ctx.embed_reply(f"{ctx.bot.error_emoji} That item type doesn't exist")
+            await ctx.embed_reply(
+                f"{ctx.bot.error_emoji} That item type doesn't exist"
+            )
         else:
-            await ctx.embed_reply(f"{ctx.bot.error_emoji} You're currently {started}! You can't start foraging right now")
+            await ctx.embed_reply(
+                f"{ctx.bot.error_emoji} You're currently {started}! "
+                "You can't start foraging right now"
+            )
 
     @forage.command(name = "stop", aliases = ["off"])
     async def forage_stop(self, ctx):
