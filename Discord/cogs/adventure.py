f5e52b72e1c826416fbc5cad52a8cdcc20069f00
Harmon <Harmon758@gmail.com>
2025-12-13 17:08:17 -0600

    [Discord] Use character name escape sequence in adventure subcommand
    Use character name escape sequence for evergreen tree emoji in adventure stats woodcutting command response

--- Discord/cogs/adventure.py
+++ Discord/cogs/adventure.py
@@ -283,7 +283,7 @@ class Adventure(commands.Cog):
         player = await self.get_adventure_player(ctx.author.id)
         woodcutting_xp = player.woodcutting_xp
         await ctx.embed_reply(
-            f":evergreen_tree: Woodcutting xp: {woodcutting_xp:,}\n"
+            f"\N{EVERGREEN TREE} Woodcutting xp: {woodcutting_xp:,}\n"
             f"{self.level_bar(woodcutting_xp)}\n"
             f"{xp_left_to_next_lvl(woodcutting_xp):,} xp to next level"
         )
