a6df7d79a1111511d0b99758baf1e703af331db1
Harmon <Harmon758@gmail.com>
2025-12-20 19:12:40 -0600

    [Discord] Use error emoji Bot attribute in adventure subcommand
    Use error emoji Bot attribute in adventure woodcutting rate command

--- Discord/cogs/adventure.py
+++ Discord/cogs/adventure.py
@@ -340,7 +340,7 @@ class Adventure(commands.Cog):
         if wood_type in WOOD_TYPES:
             await ctx.embed_reply(f"You will get {player.wood_rate(wood_type) * player.woodcutting_rate:.2f} {wood_type}/min. at your current level")
         else:
-            await ctx.embed_reply(":no_entry: That wood type doesn't exist")
+            await ctx.embed_reply(f"{ctx.bot.error_emoji} That wood type doesn't exist")
 
     @woodcutting.command(name = "start", aliases = ["on"])
     async def woodcutting_start(self, ctx, *, wood_type: str):
