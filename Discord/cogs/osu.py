6c791e81f528ee7eee73efc8c58011ece342727f
Harmon <Harmon758@gmail.com>
2026-02-16 11:59:08 -0600

    [Discord] Assign reference to asyncio task in Osu cog

--- Discord/cogs/osu.py
+++ Discord/cogs/osu.py
@@ -36,7 +36,7 @@ class Osu(commands.Cog):
         self.bot = bot
 
     async def cog_load(self):
-        asyncio.create_task(
+        self.application_emojis_initialization_task = asyncio.create_task(
             self.initialize_application_emojis(),
             name = "Initialize osu! application emojis"
         )
